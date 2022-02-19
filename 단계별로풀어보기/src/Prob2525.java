757d5c1fbefad3e57ae064ef0601cfd1944659ed
yurrim <yurimkang20@gmail.com>
2022-02-19 19:01:19 +0900

    Step2. if문

---
+++ 단계별로풀어보기/src/Prob2525.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Prob2525 {
+    public static void main(String[] args) throws IOException {
+        
+        // OPTION 1 : 128ms 14296KB
+        
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer (br.readLine(), " ");
+
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(br.readLine());
+
+        h += (m+t) / 60;
+        m = (m+t) % 60;
+
+        if (h >= 24) {
+            h -= 24;
+        } 
+
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+
+        bw.write(Integer.toString(h) + " ");
+        bw.write(Integer.toString(m));
+
+        bw.flush();
+        bw.close();
+
+        br.close();
+    }
+}
---
+++ 단계별로풀어보기/src/Prob2884.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Prob2884 {
+    public static void main(String[] args) throws IOException {
+        
+        // OPTION 1 : 136ms 14492KB
+
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer (br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if(a==0) {
+            a = 24;
+        }
+
+        int sum = (60*a + b) - 45;
+
+        int hour = sum / 60;
+        int minute = sum % 60;
+
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+
+        if(hour==24) {
+            hour = 0;
+        }
+
+        bw.write(Integer.toString(hour) + " ");
+        bw.write(Integer.toString(minute));
+
+        // OPTION 2 : 140ms 14300KB
+
+        if (a==0) {
+            if (b >= 45) {
+                b -= 45;
+            } else {
+                a = 23;
+                b += 15;
+            }
+        } else {
+            if (b >= 45) {
+                b -= 45;
+            } else {
+                a -= 1;
+                b += 15;
+            }
+        }
+
+        bw.write(Integer.toString(a) + " ");
+        bw.write(Integer.toString(b));
+
+        bw.flush();
+        bw.close();
+
+        br.close();
+
+    }
+}
