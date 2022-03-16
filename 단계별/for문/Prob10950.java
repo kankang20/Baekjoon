07c52eaf1a09dd422bda61ec38a5ad9b335c5df8
yurrim <yurimkang20@gmail.com>
2022-03-16 23:38:10 +0900

    step3

---
+++ 단계별/for문/Prob10950.java
@@ -0,0 +1,40 @@
+package 단계별.for문;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Prob10950 {
+    public static void main(String[] args) throws IOException {
+
+        // OPTION 1 : 132ms 14296KB
+        
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+
+        for (int i = 0; i < num; i++) {
+            
+            StringTokenizer st = new StringTokenizer (br.readLine(), " ");
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            bw.write(Integer.toString(a+b));
+            bw.newLine();
+            bw.flush();
+              
+        }
+
+        // OPTION 2 : 128ms 14192KB
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
---
+++ 단계별/for문/Prob11022.java
@@ -0,0 +1,39 @@
+package 단계별.for문;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Prob11022 {
+    public static void main(String[] args) throws IOException {
+        
+        // OPTION 1 : 180ms 16576KB
+        
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+
+        for (int i = 1; i <= num; i++) {
+            
+            StringTokenizer st = new StringTokenizer (br.readLine(), " ");
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            bw.write("Case #" + Integer.toString(i) + ": " 
+                        + Integer.toString(a) + " + " 
+                        + Integer.toString(b) + " = " + Integer.toString(a+b));
+            bw.newLine();
+        }
+
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
