a79aee86b22a1b12157db09551dbec37c42120f0
yurrim <yurimkang20@gmail.com>
2022-03-16 23:39:55 +0900

    step6

---
+++ 단계별/함수/Prob15596.java
@@ -0,0 +1,34 @@
+package 단계별.함수;
+
+import java.io.IOException;
+
+public class Prob15596 {
+    static long sum(int[] a) {
+
+        long ans = 0;
+        
+        // OPTION 1 : 36ms 422800KB
+        
+        for(long i : a) {
+            ans += i;
+        }
+
+        // OPTION 2 : 24ms 423296KB
+
+        for(int i = 0; i <a.length; i++) {
+            ans += a[i];
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) throws IOException {
+        
+        StringBuilder sb = new StringBuilder();
+
+        int[] test = {1, 2, 3, 4, 5};
+
+        sb.append(sum(test));
+        System.out.println(sb);
+    }
+}
