cd6e6e4300f940956856d3386b309968da4d72bc
yurrim <yurimkang20@gmail.com>
2022-08-14 04:49:17 +0900

    몫과 나머지 출력하기

---
+++ SWEA/Problem/D1/Prob2029.java
@@ -0,0 +1,30 @@
+package SWEA.Problem.D1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 97ms 18708KB
+
+public class Prob2029 {
+    public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < T; i++) {
+
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			sb.append("#" + (i+1) + " " + (a/b) + " " + (a%b) + "\n");
+		}
+
+		System.out.println(sb);
+		br.close();
+	}
+}
