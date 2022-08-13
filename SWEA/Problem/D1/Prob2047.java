6fa3cdb92cade7092577002f5ff4faff31662380
yurrim <yurimkang20@gmail.com>
2022-08-14 04:49:56 +0900

    신문 헤드라인

---
+++ SWEA/Problem/D1/Prob2047.java
@@ -0,0 +1,19 @@
+package SWEA.Problem.D1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// 103ms 18388KB
+
+public class Prob2047 {
+    public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+        
+		sb.append(br.readLine().toUpperCase());
+
+		System.out.println(sb);
+		br.close();
+	}
+}
