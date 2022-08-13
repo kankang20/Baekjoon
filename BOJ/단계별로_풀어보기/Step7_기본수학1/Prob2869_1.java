5522ff66f36c6b893356dff52b4636dddd6f709b
yurrim <yurimkang20@gmail.com>
2022-08-14 04:05:53 +0900

    달팽이는 올라가고 싶다

---
+++ BOJ/단계별로_풀어보기/Step7_기본수학1/Prob2869_1.java
@@ -0,0 +1,31 @@
+package BOJ.단계별로_풀어보기.Step7_기본수학1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 124ms 14112KB
+
+public class Prob2869_1 {
+    public static void main(String[] args) throws IOException {
+       
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        int count = (v - b) / (a - b);
+
+        if ((v - b) % (a - b) != 0) count++;
+
+        sb.append(count);
+
+        System.out.println(sb);
+        br.close();
+    }
+}
