235be943fc9bcc4ef252cd40ba02d41abd0d8241
yurrim <yurimkang20@gmail.com>
2022-08-14 01:55:20 +0900

    오븐 시계

---
+++ BOJ/단계별로_풀어보기/Step2_조건문/Prob2525_1.java
@@ -0,0 +1,41 @@
+package BOJ.단계별로_풀어보기.Step2_조건문;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// 128ms 14296KB
+
+public class Prob2525_1 {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer (br.readLine(), " ");
+
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(br.readLine());
+
+        h += (m+t) / 60;
+        m = (m+t) % 60;
+
+        if (h >= 24) {
+            h -= 24;
+        } 
+
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+
+        bw.write(Integer.toString(h) + " ");
+        bw.write(Integer.toString(m));
+
+        bw.flush();
+        bw.close();
+
+        br.close();
+
+    }
+}
