b2ae5e727203fbc5d634add0d0866441f471fa8c
yurrim <yurimkang20@gmail.com>
2022-08-14 04:49:03 +0900

    대각선 출력하기

---
+++ SWEA/Problem/D1/Prob2027.java
@@ -0,0 +1,18 @@
+package SWEA.Problem.D1;
+
+// 97ms 19356KB
+
+public class Prob2027 {
+    public static void main(String args[]) throws Exception
+	{
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("#++++\n");
+		sb.append("+#+++\n");
+		sb.append("++#++\n");
+		sb.append("+++#+\n");
+		sb.append("++++#\n");
+
+		System.out.println(sb);
+	}
+}
