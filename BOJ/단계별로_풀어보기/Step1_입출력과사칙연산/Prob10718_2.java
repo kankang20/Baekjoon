e382ea5688c09fed481e90a8de27947f69d9145b
yurrim <yurimkang20@gmail.com>
2022-08-14 01:24:15 +0900

    We love kriii

---
+++ BOJ/단계별로_풀어보기/Step1_입출력과사칙연산/Prob10718_2.java
@@ -0,0 +1,18 @@
+package BOJ.단계별로_풀어보기.Step1_입출력과사칙연산;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+// 128ms 13964KB
+
+public class Prob10718_2 {
+    public static void main(String[] args) throws IOException{
+
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter (System.out));
+
+        bw.write("강한친구 대한육군\n강한친구 대한육군");
+        bw.flush();
+        bw.close();
+    }
+}
