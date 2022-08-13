6e574ccb2cff7587b9397b3a39049f2778ead3e0
yurrim <yurimkang20@gmail.com>
2022-08-14 02:11:48 +0900

    합

---
+++ BOJ/단계별로_풀어보기/Step3_반복문/Prob8393_1.java
@@ -0,0 +1,31 @@
+package BOJ.단계별로_풀어보기.Step3_반복문;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// 124ms 14248KB
+
+public class Prob8393_1 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+
+        for (int i = 1; i <= num; i++) {
+            sum += i;
+        }
+
+        bw.write(Integer.toString(sum));
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
