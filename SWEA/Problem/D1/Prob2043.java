75f140a3ad19b0a71d2bc59777ef4512468058ae
yurrim <yurimkang20@gmail.com>
2022-08-14 04:49:31 +0900

    서랍의 비밀번호

---
+++ SWEA/Problem/D1/Prob2043.java
@@ -0,0 +1,25 @@
+package SWEA.Problem.D1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 107ms 18452KB
+
+public class Prob2043 {
+    public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int P = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		sb.append(P-K+1);
+
+		System.out.println(sb);
+		br.close();
+	}
+}
