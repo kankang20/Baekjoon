42cbaabcd0ede715ebb15d7a81b58422c00a54bb
yurrim <yurimkang20@gmail.com>
2022-08-14 04:51:24 +0900

    큰 놈, 작은 놈, 같은 놈

---
+++ SWEA/Problem/D1/Prob2070.java
@@ -0,0 +1,32 @@
+package SWEA.Problem.D1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 95ms 18380KB
+
+public class Prob2070 {
+    public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < T; i++) {
+
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			if (a > b) sb.append("#" + (i+1) + " >\n");
+			else if (a < b) sb.append("#" + (i+1) + " <\n");
+			else sb.append("#" + (i+1) + " =\n");
+		}
+
+		System.out.println(sb);
+		br.close();
+	}
+}
