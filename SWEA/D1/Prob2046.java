c6552f7db80423a0e3e6ec9a6962344dc5343399
yurrim <yurimkang20@gmail.com>
2022-08-30 23:49:43 +0900

    스탬프 찍기

---
+++ SWEA/D1/Prob2046.java
@@ -0,0 +1,23 @@
+package SWEA.D1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// 스탬프 찍기
+// 98ms 18340KB
+
+public class Prob2046 {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < T; i++) {
+			sb.append("#");
+		}
+
+		System.out.println(sb);
+		br.close();
+	}
+}
