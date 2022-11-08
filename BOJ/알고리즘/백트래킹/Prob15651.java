f528ff18dfa496a6eeb543b739d90534cc8ae2ce
yurrim <yurimkang20@gmail.com>
2022-11-08 22:13:20 +0900

    N과 M (3)

---
+++ BOJ/알고리즘/백트래킹/Prob15651.java
@@ -0,0 +1,53 @@
+package BOJ.알고리즘.백트래킹;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// N과 M (3) 608ms 207272KB (java11) 516ms 262508KB (java8)
+// 백트래킹
+
+public class Prob15651 {
+
+    /** 1부터 N까지의 자연수 중에서 M개를 고른 수열 */
+
+    static int N;
+    static int M;
+
+    static int[] numbers;
+
+    static StringBuilder sb = new StringBuilder();
+
+    private static void permutation(int cnt) {
+
+        if (cnt == M) {
+            for (int num : numbers)
+                sb.append(num + " ");
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+
+            numbers[cnt] = i;
+            permutation(cnt + 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        numbers = new int[M];
+
+        permutation(0);
+
+        System.out.println(sb);
+    }
+}
