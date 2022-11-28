46852c8715a54ddaab531ffe2eb7d1b18345f1f5
yurrim <yurimkang20@gmail.com>
2022-11-28 20:53:54 +0900

    ??!

---
+++ BOJ/단계별로_풀어보기/입출력과사칙연산/Prob10926_1.java
@@ -0,0 +1,17 @@
+package BOJ.단계별로_풀어보기.입출력과사칙연산;
+
+import java.util.Scanner;
+
+// ??! 216ms 17764KB
+
+public class Prob10926_1 {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.next();
+        System.out.println(name + "??!");
+
+        sc.close();
+    }
+}
