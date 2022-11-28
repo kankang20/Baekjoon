3e983fb455cef4712d4e2f5aefddc1064c443e9b
yurrim <yurimkang20@gmail.com>
2022-11-28 20:53:33 +0900

    사칙연산

---
+++ BOJ/단계별로_풀어보기/입출력과사칙연산/Prob10869_1.java
@@ -0,0 +1,23 @@
+package BOJ.단계별로_풀어보기.입출력과사칙연산;
+
+import java.util.Scanner;
+
+// 사칙연산 220ms 17744KB
+
+public class Prob10869_1 {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
+
+        sc.close();
+    }
+}
