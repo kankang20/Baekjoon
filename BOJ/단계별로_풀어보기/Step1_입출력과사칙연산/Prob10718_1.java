b136301c90f7208de2f8752a51fb258217ad60db
yurrim <yurimkang20@gmail.com>
2022-11-28 19:19:37 +0900

    정리중

--- BOJ/단계별로_풀어보기/Step1_입출력과사칙연산/Prob10718_1.java
+++
@@ -1,9 +0,0 @@
-package BOJ.단계별로_풀어보기.Step1_입출력과사칙연산;
-
-// 112ms 13856KB
-
-public class Prob10718_1 {
-    public static void main(String[] args) {
-        System.out.println("강한친구 대한육군\n강한친구 대한육군");
-    }
-}
--- BOJ/단계별로_풀어보기/Step1_입출력과사칙연산/Prob2557_1.java
+++
@@ -1,9 +0,0 @@
-package BOJ.단계별로_풀어보기.Step1_입출력과사칙연산;
-
-// 112ms 13896KB
-
-public class Prob2557_1 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
