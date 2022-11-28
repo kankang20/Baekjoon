5c63f1c08cdc370da17904df2449d9b3e4ce66b3
yurrim <yurimkang20@gmail.com>
2022-11-28 19:22:14 +0900

    A+B

---
+++ BOJ/단계별로_풀어보기/입출력과사칙연산/Prob1000_1.java
@@ -0,0 +1,19 @@
+package BOJ.단계별로_풀어보기.입출력과사칙연산;
+
+import java.util.Scanner;
+
+// A+B 212ms 17720KB
+
+public class Prob1000_1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println(num1 + num2);
+
+        sc.close();
+    }
+}
---
+++ BOJ/단계별로_풀어보기/입출력과사칙연산/Prob1000_2.java
@@ -0,0 +1,22 @@
+package BOJ.단계별로_풀어보기.입출력과사칙연산;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// A+B 132ms 14188KB
+
+public class Prob1000_2 {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(str, " ");
+
+        int a_token = Integer.parseInt(st.nextToken());
+        int b_token = Integer.parseInt(st.nextToken());
+
+        System.out.println(a_token + b_token);
+    }
+}
