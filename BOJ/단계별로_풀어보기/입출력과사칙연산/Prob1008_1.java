8b84fbd7a3fca13d290beabf1649b9740f5323dc
yurrim <yurimkang20@gmail.com>
2022-11-28 19:23:15 +0900

    A/B

---
+++ BOJ/단계별로_풀어보기/입출력과사칙연산/Prob1008_1.java
@@ -0,0 +1,19 @@
+package BOJ.단계별로_풀어보기.입출력과사칙연산;
+
+import java.util.Scanner;
+
+// A/B 228ms 18032KB
+
+public class Prob1008_1 {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        double num1 = sc.nextDouble();
+        double num2 = sc.nextDouble();
+
+        System.out.println(num1 / num2);
+
+        sc.close();
+    }
+}
