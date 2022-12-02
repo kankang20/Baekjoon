f4c70d6a56978bcd5d66b15b9701e9b46eb5b558
yurrim <yurimkang20@gmail.com>
2022-12-03 02:26:03 +0900

    부녀회장이 될테야

---
+++ BOJ/단계별로_풀어보기/기본수학1/Prob2775_1.java
@@ -0,0 +1,41 @@
+package BOJ.단계별로_풀어보기.기본수학1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 부녀회장이 될테야 136ms 14552KB
+
+public class Prob2775_1 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int[][] count = new int[15][15];
+
+        for (int i = 0; i < 15; i++) {
+            count[i][1] = 1;
+            count[0][i] = i;
+        }
+
+        for (int i = 1; i < 15; i++) {
+            for (int j = 2; j < 15; j++) {
+                count[i][j] = count[i - 1][j] + count[i][j - 1];
+            }
+        }
+
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < num; i++) {
+
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+
+            sb.append(count[k][n] + "\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
