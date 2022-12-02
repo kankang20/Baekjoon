5af88d8ec475910fa63a2077a074909c039eed1d
yurrim <yurimkang20@gmail.com>
2022-12-03 02:26:46 +0900

    ACM 호텔

---
+++ BOJ/단계별로_풀어보기/기본수학1/Prob10250_1.java
@@ -0,0 +1,40 @@
+package BOJ.단계별로_풀어보기.기본수학1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// ACM 호텔 132ms 14288KB
+
+public class Prob10250_1 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        int x = 0, y = 0;
+
+        for (int i = 0; i < num; i++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int height = Integer.parseInt(st.nextToken());
+            st.nextToken();
+            int number = Integer.parseInt(st.nextToken());
+
+            x = number / height;
+            y = number % height;
+
+            if (y == 0) {
+                y = height;
+            } else {
+                x++;
+            }
+
+            System.out.println(y * 100 + x);
+        }
+
+        br.close();
+    }
+}
