0338af4df13e19c86fc316c857aa2ad9a7e81ddb
yurrim <yurimkang20@gmail.com>
2022-12-03 02:21:37 +0900

    두 수 비교하기

---
+++ BOJ/단계별로_풀어보기/조건문/Prob1330_1.java
@@ -0,0 +1,36 @@
+package BOJ.단계별로_풀어보기.조건문;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// 두 수 비교하기 128ms 14280KB
+
+public class Prob1330_1 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer str = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(str.nextToken());
+        int b = Integer.parseInt(str.nextToken());
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        if (a > b) {
+            bw.write(">");
+        } else if (a < b) {
+            bw.write("<");
+        } else {
+            bw.write("==");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
