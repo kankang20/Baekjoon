ce2a08101d04b02bffb397a5a1e070033689e306
yurrim <yurimkang20@gmail.com>
2023-01-10 23:18:50 +0900

    정리

--- BOJ/단계별로_풀어보기/입출력과사칙연산/Prob2557_1.java
+++
@@ -1,9 +0,0 @@
-package BOJ.단계별로_풀어보기.입출력과사칙연산;
-
-// Hello World 112ms 13896KB
-
-public class Prob2557_1 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
