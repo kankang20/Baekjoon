151b41fe9a7872627fbd8cf2fae8ba81c88c8f22
yuuforest <yurimkang20@gmail.com>
2023-03-10 09:55:24 +0900

    Lv 1

--- src/PROGRAMMERS/완전탐색/최소직사각형.java
+++ src/PROGRAMMERS/완전탐색/최소직사각형.java
@@ -1,8 +1,5 @@
 package PROGRAMMERS.완전탐색;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class 최소직사각형 {
     public static void main(String[] args) {
 
@@ -22,49 +19,14 @@ public class 최소직사각형 {
 
     public static int solution(int[][] sizes) {
 
-        int answer = Integer.MAX_VALUE;
-
-        // 중복되는 길이가 있을 수 있기 때문에 우선은 중복 제거
-        List<Integer> width = new ArrayList<>();
-        List<Integer> height = new ArrayList<>();
+        int small = 0, big = 0;
 
         for (int[] size : sizes) {
-            if (!width.contains(size[0])) {
-                width.add(size[0]);
-            }
+
+            small = Math.max(Math.min(size[0], size[1]), small);
+            big = Math.max(Math.max(size[0], size[1]), big);
         }
 
-        for (int[] size : sizes) {
-            if (!height.contains(size[1])) {
-                height.add(size[1]);
-            }
-        }
-
-        // 이제 하나씩 해보면 돼
-        for (Integer w : width) {
-            for (Integer h : height) {
-                if(check(w, h, sizes)) { // 최소값 갱신하기
-                    answer = Math.min(answer, w*h);
-                }
-            }
-        }
-        
-        return answer;
+        return small * big;
     } // end solution
-
-    private static boolean check(int w, int h, int[][] sizes) {
-
-        int width = 0, height = 0;
-
-        for (int[] size: sizes) {
-
-            width = size[0];
-            height = size[1];
-
-            if(width <= w && height <= h) continue;
-            if(width <= h && height <= w) continue;
-            return false;
-        }
-        return true;
-    } // end check
 }
