249dab5a33c733f04a7210fdd1a689dd0bd8a02d
yurrim <yurimkang20@gmail.com>
2023-06-27 22:52:09 +0900

    Upload. 2217 로프

---
+++ python/그리디/Prob2217.py
@@ -0,0 +1,14 @@
+""" 실버 4. 로프 """
+
+N = int(input())    # 로프 갯수
+
+rope = [int(input()) for _ in range(N)]     # 각 로프가 버틸 수 있는 최대 중량
+
+rope.sort()     # 작은 수 부터 확인하기 위함
+
+answer = 0
+
+for num in range(N):
+    answer = max(answer, rope[num] * (N - num))
+
+print(answer)
