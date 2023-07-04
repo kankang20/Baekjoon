9345003f2e3f842efc5235c62b2b904c83eedd06
yurrim <yurimkang20@gmail.com>
2023-07-04 15:06:47 +0900

    Upload. 13305 주유소

--- python/그리디/Prob13305.py
+++ python/그리디/Prob13305.py
@@ -1,21 +1,15 @@
 
 """ 실버 3. 주유소 """
 
-N = int(input())    # 도시의 개수
+N = int(input())                                # 도시 개수
+roads = list(map(int, input().split()))         # 도로의 길이
+costs = list(map(int, input().split()))         # 주유소 리터당 가격
 
-# 도로의 길이
-Length = list(map(int, input().split()))    # 도로의 길이
+min_cost = max(costs)
+answer = 0      # 이동하는 최소 비용
 
-# 주유소의 리터랑 가격
-E1 = [num for num in range(N)]
-E2 = list(map(int, input().split()))   
+for num, road in enumerate(roads):
+    min_cost = min(min_cost, costs[num])
+    answer += min_cost * road
 
-Expense = dict(zip(E1, E2))
-Expense = sorted(Expense.items(), key = lambda item: item[1])
-
-# 지불 가능한 도로인지 확인 
-check = [False for _ in range(N-1)]   
-
-for key, value in Expense:
-    print(key, value)
-
+print(answer)
