d22e76f865c01626d00d401fa66d655844596f61
yurrim <yurimkang20@gmail.com>
2023-07-13 22:19:44 +0900

    Upload. 1244 스위치 켜고 끄기

--- python/구현/Prob1244.py
+++ python/구현/Prob1244.py
@@ -1,33 +1,26 @@
 
 """ 실버 4. 스위치 켜고 끄기 """
 
-# 스위치 개수
-switch_count = int(input())                         
-# 각 스위치의 상태 (1 켜져있음 0 꺼져있음)
-switch_state = list(map(int, input().split()))
-switch_state.insert(0, 0)  
-# 학생 수   
-student_count = int(input())                     
-# 학생의 성별, 학생이 받은 수
-student_switch = [list(map(int, input().split())) for _ in range(student_count)]
+switch_count = int(input())                         # 스위치 개수                 
+switch_state = list(map(int, input().split()))      # 각 스위치의 상태 (1 켜져있음 0 꺼져있음)
+switch_state.insert(0, 0)    
+student_count = int(input())                        # 학생 수                      
 
-for gender, number in student_switch:
-    
+for student in range(student_count):
+    gender, number = map(int, input().split())      # 입력받으면서 스위치 변경
+    s = 1
+
     if gender == 1:     # 남학생
-        number_up = number
-        while number_up < len(switch_state):
-            switch_state[number_up] = 1 if switch_state[number_up] == 0 else 0
-            number_up += number
+        while number * s < len(switch_state):
+            switch_state[number * s] = abs(switch_state[number * s] - 1)
+            s += 1
 
     else:               # 여학생
-        switch_state[number] = 1 if switch_state[number] == 0 else 0
-        left = number - 1
-        right = number + 1 
-        while left > 0 and right < switch_count+1 and switch_state[left] == switch_state[right]:
-            switch_state[left] = 1 if switch_state[left] == 0 else 0
-            switch_state[right] = 1 if switch_state[right] == 0 else 0
-            left -= 1
-            right += 1
+        switch_state[number] = abs(switch_state[number] - 1)
+        while number - s > 0 and number + s < switch_count+1 and switch_state[number - s] == switch_state[number + s]:
+            switch_state[number - s] = abs(switch_state[number - s] - 1)
+            switch_state[number + s] = abs(switch_state[number + s] - 1)
+            s += 1
 
 for num in range(1, len(switch_state)):
     print(switch_state[num], end = " ")
