09026f42cb5acee5ae4417d0ba70cc6e2253c531
yurrim <yurimkang20@gmail.com>
2023-07-17 12:51:11 +0900

    Upload. 1244 스위치 켜고 끄기

--- python/구현/Prob1244.py
+++ python/구현/Prob1244.py
@@ -1,27 +1,26 @@
 
 """ 실버 4. 스위치 켜고 끄기 """
 
-switch_count = int(input())                         # 스위치 개수                 
-switch_state = list(map(int, input().split()))      # 각 스위치의 상태 (1 켜져있음 0 꺼져있음)
-switch_state.insert(0, 0)    
-student_count = int(input())                        # 학생 수                      
+sc = int(input())                                # 스위치 개수                 
+state = [0] + list(map(int, input().split()))    # 각 스위치의 상태 (1 켜져있음 0 꺼져있음)   
+student = int(input())                           # 학생 수                      
 
-for student in range(student_count):
-    gender, number = map(int, input().split())      # 입력받으면서 스위치 변경
+for num in range(student):
+    ge, sw = map(int, input().split())      # 입력받으면서 스위치 변경
     s = 1
 
-    if gender == 1:     # 남학생
-        while number * s < len(switch_state):
-            switch_state[number * s] = abs(switch_state[number * s] - 1)
+    if ge == 1:         # 남학생
+        while sw * s < sc + 1:
+            state[sw * s] = abs(state[sw * s] - 1)
             s += 1
 
     else:               # 여학생
-        switch_state[number] = abs(switch_state[number] - 1)
-        while number - s > 0 and number + s < switch_count+1 and switch_state[number - s] == switch_state[number + s]:
-            switch_state[number - s] = abs(switch_state[number - s] - 1)
-            switch_state[number + s] = abs(switch_state[number + s] - 1)
+        state[sw] = abs(state[sw] - 1)
+        while sw - s > 0 and sw + s < sc + 1 and state[sw - s] == state[sw + s]:
+            state[sw - s] = abs(state[sw - s] - 1)
+            state[sw + s] = abs(state[sw + s] - 1)
             s += 1
 
-for num in range(1, len(switch_state)):
-    print(switch_state[num], end = " ")
+for num in range(1, sc+1):
+    print(state[num], end = " ")
     if num % 20 == 0:print()
