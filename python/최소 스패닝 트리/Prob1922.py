319eb67b6e7a288c037b841b2c544da20717dcd9
yurrim <yurimkang20@gmail.com>
2023-07-25 00:58:25 +0900

    Upload. 1922 네트워크 연결

---
+++ python/최소 스패닝 트리/Prob1922.py
@@ -0,0 +1,36 @@
+
+""" 골드 4. 네트워크 연결 """
+
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+def union(parent, a, b):
+    a = find(parent, a)
+    b = find(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+N = int(input())    # 컴퓨터의 수
+M = int(input())    # 연결할 수 있는 선의 수
+parent = [num for num in range(N+1)]
+
+edges = []
+
+for _ in range(M):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+edges.sort()
+answer = 0      # 최소 비용
+
+for edge in edges:
+    cost, a, b = edge
+    if find(parent, a) != find(parent, b):
+        union(parent, a, b)
+        answer += cost
+
+print(answer)
