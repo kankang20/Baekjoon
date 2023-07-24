9cbf85943d3402ef4df8b730cb0a5be7c32377bd
yurrim <yurimkang20@gmail.com>
2023-07-24 18:14:08 +0900

    Upload. 1976 여행 가자

---
+++ python/분리집합/Prob1976.py
@@ -0,0 +1,38 @@
+
+""" 골드 4. 여행 가자 """
+
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+def union(parent, a, b):
+    a = find(parent, a)
+    b = find(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+N = int(input())    # 도시 수
+M = int(input())    # 여행 계획에 속한 도시들의 수
+
+parent = [num for num in range(N)]      
+
+for i in range(N):
+    temp = list(map(int, input().split()))
+    for j in range(N):
+        if i <= j or temp[j] == 0: 
+            continue
+        union(parent, i, j)
+
+plan = list(map(int, input().split()))
+root = find(parent, plan[0]-1)
+
+for city in plan[1:]:
+    if root != find(parent, city-1):
+        print("NO")
+        break
+
+else:
+    print("YES")
