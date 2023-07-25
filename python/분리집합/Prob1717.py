231bb04597dd44c62e061425b23b1f0a8955ada2
yurrim <yurimkang20@gmail.com>
2023-07-25 13:55:42 +0900

    Upload. 1717 집합의 표현

---
+++ python/분리집합/Prob1717.py
@@ -0,0 +1,33 @@
+
+""" 골드 5. 집합의 표현 """
+
+import sys
+sys.setrecursionlimit(10**6)
+
+N, M = map(int, sys.stdin.readline().split())       # 원소 개수, 연산 개수
+parent = [num for num in range(N+1)]                # 부모 테이블
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+for i in range(M):
+
+    check, a, b = map(int, sys.stdin.readline().split())
+
+    if check == 0:
+        union(a, b)
+    else:
+        if find(a) == find(b):
+            print("YES")
+        else:
+            print("NO")
