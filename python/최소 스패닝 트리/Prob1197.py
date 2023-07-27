70f1cedf7caea775bbbd0a8602119baa1e29c889
yurrim <yurimkang20@gmail.com>
2023-07-27 23:07:52 +0900

    Upload. 1197 최소 스패닝 트리

--- python/최소 스패닝 트리/Prob1197.py
+++ python/최소 스패닝 트리/Prob1197.py
@@ -1,8 +1,10 @@
 
 """ 골드 4. 최소 스패닝 트리 """
 
-V, E = map(int, input().split())            # 정점의 개수, 간선의 개수
-parent = [num for num in range(V+1)]        # 부모 테이블
+import sys
+
+V, E = map(int, sys.stdin.readline().split())           # 정점의 개수, 간선의 개수
+parent = [num for num in range(V+1)]                    # 부모 테이블
 
 def find(x):
     if parent[x] != x:
@@ -17,19 +19,23 @@ def union(a, b):
     else:
         parent[a] = b
 
-edges = []
+def solution():
+    edges = []
 
-for _ in range(E):
-    a, b, cost = map(int, input().split())
-    edges.append((cost, a, b))
+    for _ in range(E):
+        a, b, cost = map(int, sys.stdin.readline().split())
+        edges.append((cost, a, b))
 
-edges.sort()
-answer = 0
+    edges.sort()
+    answer = 0
 
-for edge in edges:
-    cost, a, b = edge
-    if find(a) != find(b):
-        union(a, b)
-        answer += cost
+    for edge in edges:
+        cost, a, b = edge
+        if find(a) != find(b):
+            union(a, b)
+            answer += cost
 
-print(answer)
+    print(answer)
+
+if __name__ == '__main__':
+    solution()
