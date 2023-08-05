510aef49d3c2e6f926199c6d237b3a9c35ed646c
yurrim <yurimkang20@gmail.com>
2023-08-05 15:51:07 +0900

    Upload. 2470 두 용액

--- python/투 포인터/Prob2470.py
+++ python/투 포인터/Prob2470.py
@@ -6,5 +6,26 @@ import sys
 N = int(sys.stdin.readline())                       # 전체 용액의 수
 P = list(map(int, sys.stdin.readline().split()))    # 각 용액의 특성값
 
-print(N)
-print(P)
+answer = sys.maxsize
+number = ()
+
+for idx in range(N-1):
+
+    num1 = P[idx]
+
+    i = idx + 1
+
+    while i < N:
+        num2 = P[i]
+        sum = abs(num1 + num2)
+
+        if sum < answer:
+            answer = sum
+            number = (num1, num2)
+
+        i += 1
+
+if number[0] < number[1]:
+    print(number[0], number[1])
+else:
+    print(number[1], number[0])
