3297ea378597efa917432c5c6a4c1fe3d6e8d37a
yurrim <yurimkang20@gmail.com>
2023-08-05 15:11:05 +0900

    Upload. 2470 두 용액

---
+++ python/투 포인터/Prob2470.py
@@ -0,0 +1,10 @@
+
+""" 골드 5. 두 용액 """
+
+import sys
+
+N = int(sys.stdin.readline())                       # 전체 용액의 수
+P = list(map(int, sys.stdin.readline().split()))    # 각 용액의 특성값
+
+print(N)
+print(P)
