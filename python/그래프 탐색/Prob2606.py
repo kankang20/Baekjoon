d8b56344079e526e07bf9e8689b756d4c4501a13
yurrim <yurimkang20@gmail.com>
2023-08-26 00:39:02 +0900

    Upload. 2606 바이러스

--- python/그래프 탐색/Prob2606.py
+++ python/그래프 탐색/Prob2606.py
@@ -5,21 +5,22 @@
 N = int(input())    # 컴퓨터 수
 M = int(input())    # 네트워크 상에서 직접 연결되어 있는 컴퓨터 쌍의 수
 
-computer = [[0] * (N+1) for _ in range(N+1)]
+computer = [[] for _ in range(N+1)] 
 visited = [0] * (N+1)
 
 for _ in range(M):
     i, j = map(int, input().split())
-    computer[i][j] = computer[j][i] = 1
+    computer[i].append(j)
+    computer[j].append(i)
 
 
 # 함수
 def dfs(start):
 
     visited[start] = 1
-
-    for i in range(1, N+1):
-        if not visited[i] and computer[start][i]:
+    
+    for i in computer[start]:
+        if not visited[i]:
             dfs(i)
 
 
