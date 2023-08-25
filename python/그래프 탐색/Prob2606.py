db856c5c843c5cb2ebc79e567d848fcb29576585
yurrim <yurimkang20@gmail.com>
2023-08-26 00:58:05 +0900

    Upload. 2606 바이러스

--- python/그래프 탐색/Prob2606.py
+++ python/그래프 탐색/Prob2606.py
@@ -1,29 +1,41 @@
 
 """ 실버 3. 바이러스 """
 
+from collections import deque
+
 # 입력
 N = int(input())    # 컴퓨터 수
 M = int(input())    # 네트워크 상에서 직접 연결되어 있는 컴퓨터 쌍의 수
 
-computer = [[] for _ in range(N+1)] 
-visited = [0] * (N+1)
+computer = [[0] * (N+1) for _ in range(N+1)]
 
 for _ in range(M):
     i, j = map(int, input().split())
-    computer[i].append(j)
-    computer[j].append(i)
+    computer[i][j] = computer[j][i] = 1
 
 
 # 함수
-def dfs(start):
+def bfs(start):
 
+    visited = [0] * (N+1)
+
+    queue = deque()
+
+    queue.append(start)
     visited[start] = 1
-    
-    for i in computer[start]:
-        if not visited[i]:
-            dfs(i)
+
+    while queue:
+
+        now = queue.popleft()
+
+        for i in range(N+1):
+
+            if not visited[i] and computer[now][i]:
+                queue.append(i)
+                visited[i] = 1
+
+    return visited.count(1) - 1
 
 
 # 실행
-dfs(1)
-print(visited.count(1) - 1)
+print(bfs(1))
