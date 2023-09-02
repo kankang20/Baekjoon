a9aa0800b6162ee99168ed26eac0b3aceebbf62c
yurrim <yurimkang20@gmail.com>
2023-09-02 23:55:31 +0900

    Upload. 11725 트리의 부모 찾기

--- python/그래프 탐색/Prob11725.py
+++ python/그래프 탐색/Prob11725.py
@@ -7,9 +7,8 @@ import sys
 ### 입력
 input = sys.stdin.readline
 
-N = int(input())    # 노드의 개수
-# 인접 리스트
-nodes = [[] for _ in range(N+1)]
+N = int(input())                        # 노드의 개수
+nodes = [[] for _ in range(N+1)]        # 인접 리스트    
 for _ in range(N-1):
     i, j = map(int, input().split())
     nodes[i].append(j)
@@ -17,43 +16,26 @@ for _ in range(N-1):
 
 
 ### 함수
+
 def solution():
-
-    for idx in range(2, N+1):
-
-        if len(nodes[idx]) == 1:
-            print(nodes[idx][0])
-            continue
-
-        for nn in nodes[idx]:
-            if bfs(nn):
-                print(nn)
-                break
-        
-
-def bfs(start):
-
-    visited = [False] * (N+1)
+    parent = [-1] * (N+1)
 
     queue = deque()
-    
-    queue.append(start)
-    visited[start] = True
+
+    queue.append(1)
+    parent[1] = 1
 
     while queue:
+        now = queue.popleft()
 
-        node = queue.popleft()
-
-        if node == 1:
-            return True
-
-        for nn in nodes[node]:
-            if not visited[nn]:
+        for nn in nodes[now]:
+            if parent[nn] == -1:
+                parent[nn] = now
                 queue.append(nn)
-                visited[nn] = True
 
-    return False
-
+    return parent[2:]
 
 ### 실행
-solution()
+parent = solution()
+for p in parent:
+    print(p)
