be9c4937483f5fb16f10b74bbb6a7d51a0ccb728
yurrim <yurimkang20@gmail.com>
2023-09-04 14:58:38 +0900

    Upload. 2667 단지번호붙이기

--- python/그래프 탐색/Prob2667.py
+++ python/그래프 탐색/Prob2667.py
@@ -1,6 +1,7 @@
 
 """ 실버 1. 단지번호붙이기 """
 
+from collections import deque
 import sys
 
 ### 입력
@@ -9,25 +10,35 @@ N = int(input())    # 지도의 크기
 graph = [list(map(int, input().rstrip())) for _ in range(N)]
 
 visited = [[False] * N for _ in range(N)]    # 방문 처리
-count = 0
-
 dr = [0, 0, -1, 1]
 dc = [1, -1, 0, 0]
 
 ### 함수
-def dfs(r, c):
+def bfs(r, c):
+
+    q = deque()
+
+    q.append((r, c))
     visited[r][c] = True
-    global count
-    count += 1
 
-    for d in range(4):
-        nr = r + dr[d]
-        nc = c + dc[d]
+    count = 1
 
-        if nr < 0 or nc < 0 or nr >= N or nc >= N or visited[nr][nc] or not graph[nr][nc]:
-            continue
+    while q:
+        nowr, nowc = q.popleft()
 
-        dfs(nr, nc)
+        for d in range(4):
+            nr = nowr + dr[d]
+            nc = nowc + dc[d]
+
+            if nr < 0 or nc < 0 or nr >= N or nc >= N or visited[nr][nc] or not graph[nr][nc]:
+                continue
+            
+            count += 1
+
+            q.append((nr, nc))
+            visited[nr][nc] = True
+
+    return count
 
 
 ### 실행
@@ -37,10 +48,8 @@ ans = 0
 for i in range(N):
     for j in range(N):
         if graph[i][j] and not visited[i][j]:
-            dfs(i, j)
-            answers.append(count)
             ans += 1
-            count = 0
+            answers.append(bfs(i, j))
 
 print(ans)
 for a in sorted(answers):
