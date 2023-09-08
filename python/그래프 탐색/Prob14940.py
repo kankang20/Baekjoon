41cb2210e2d20cf1ee57359901616d3f8252b7bd
yurrim <yurimkang20@gmail.com>
2023-09-08 14:22:41 +0900

    Upload. 14940 쉬운 최단거리

--- python/그래프 탐색/Prob14940.py
+++ python/그래프 탐색/Prob14940.py
@@ -13,33 +13,33 @@ dc = [1, -1, 0, 0]
 
 
 def solution():
-
+    answer = [[0] * C for _ in range(R)]
     start = [(r, c) for r in range(R) for c in range(C) if graph[r][c] == 2]    # 탐색 시작 위치 찾기 (2)
 
     queue = deque()
 
-    queue.append((start[0][0], start[0][1]))
-    graph[start[0][0]][start[0][1]] = 2
+    queue.append((start[0][0], start[0][1], 0))
+    graph[start[0][0]][start[0][1]] = 0
 
     while queue:
 
-        r, c = queue.popleft()
+        r, c, count = queue.popleft()
 
         for d in range(4):
             nr = r + dr[d]
             nc = c + dc[d]
 
-            if nr < 0 or nc < 0 or nr >= R or nc >= C or graph[nr][nc] is not 1:
+            if nr < 0 or nc < 0 or nr >= R or nc >= C or not graph[nr][nc]:
                 continue
 
-            queue.append((nr, nc))
-            graph[nr][nc] = graph[r][c] + 1 
+            queue.append((nr, nc, count+1))
+            answer[nr][nc] = count+1
+            graph[nr][nc] = 0
 
     for r in range(R):
         for c in range(C):
-            if graph[r][c] == 1: print(-1, end = " ")
-            elif graph[r][c] == 0: print(0, end = " ")
-            else: print(graph[r][c]-2, end = " ")
+            if graph[r][c] == 1:print(-1, end = " ")
+            else: print(answer[r][c], end = " ")
         print()
 
 
