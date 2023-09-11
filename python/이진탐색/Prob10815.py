d08d1be0a231c46d1a21a0df8262c55df6a0ad28
yurrim <yurimkang20@gmail.com>
2023-09-11 11:47:19 +0900

    Upload. 10815 숫자 카드

--- python/이진탐색/Prob10815.py
+++ python/이진탐색/Prob10815.py
@@ -6,30 +6,15 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-cards = list(map(int, input().split()))
+cards = set(map(int, input().split()))
 M = int(input())
 numbers = list(map(int, input().split()))
 
-def solution():
-    for n in numbers:
-        print(binary(n), end = " ")
-
-def binary(target):
+def solution(target):
+    if target in cards:
+        return 1
+    else:
+        return 0
     
-    start = 0
-    end = len(cards) - 1
-
-    while start <= end:
-        mid = (start + end) // 2
-
-        if cards[mid] == target:
-            return 1
-        elif cards[mid] < target:
-            start = mid + 1
-        else:
-            end = mid - 1
-
-    return 0
-
-cards.sort()
-solution()
+for n in numbers:
+    print(solution(n), end= " ")
