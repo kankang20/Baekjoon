891370086fc72ad3b775b6467eb6f775be8dfdd7
yurrim <yurimkang20@gmail.com>
2023-09-11 10:55:04 +0900

    Upload. 1789 수들의 합

--- python/이진탐색/Prob1789.py
+++ python/이진탐색/Prob1789.py
@@ -1,2 +1,26 @@
 
 """ 실버 5. 수들의 합 """
+
+import sys
+
+
+input = sys.stdin.readline
+S = int(input())
+
+
+def solution(start, end):
+    answer = 0
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if (mid * (mid + 1)) / 2 <= S:
+            answer = mid
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return answer
+
+
+print(solution(0, S))
