59df71a175c6d72f83a673169078b7e4d3842aab
yurrim <yurimkang20@gmail.com>
2023-09-11 21:03:23 +0900

    Upload. 2417 정수 제곱근

--- python/이진탐색/Prob2417.py
+++ python/이진탐색/Prob2417.py
@@ -1,2 +1,24 @@
 
 """ 실버 4. 정수 제곱근 """
+
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+
+def solution(start, end):
+
+    answer = 0
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if mid * mid < N:
+            start = mid + 1
+        else:
+            answer = mid
+            end = mid - 1
+
+    return answer
+
+print(solution(0, N))
