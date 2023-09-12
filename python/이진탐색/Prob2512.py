e4c12f69926dcda737de6e50bee0b8debe909f5f
yurrim <yurimkang20@gmail.com>
2023-09-12 15:54:00 +0900

    Upload. 2512 예산

--- python/이진탐색/Prob2512.py
+++ python/이진탐색/Prob2512.py
@@ -1,2 +1,35 @@
 
 """ 실버 2. 예산 """
+
+import sys
+
+input = sys.stdin.readline
+N = int(input())                            # 지방 수
+budget = list(map(int, input().split()))    # 예산
+maxBudget = int(input())                    # 총 예산
+
+
+def solution(start, end):
+    answer = 0
+
+    while start <= end:
+
+        mid = (start + end) // 2
+
+        total = 0
+        for b in budget:
+            if mid < b: total += mid
+            else: total += b
+
+        if total == maxBudget:
+            return mid
+        elif total < maxBudget:
+            answer = mid
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return answer
+
+
+print(solution(0, max(budget)))
