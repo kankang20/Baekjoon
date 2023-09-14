df80361cc3098a894cc4fb56a9294da50bff334b
yurrim <yurimkang20@gmail.com>
2023-09-14 14:53:21 +0900

    Upload. 22871 징검다리 건너기 (large)

--- python/이진탐색/Prob22871.py
+++ python/이진탐색/Prob22871.py
@@ -1,2 +1,13 @@
 
 """ 실버 1. 징검다리 건너기 (large) """
+
+import sys
+
+input = sys.stdin.readline
+N = int(input())                            # 돌의 개수
+rocks = list(map(int, input().split()))     # 돌의 수 Ai
+
+
+# def solution():
+
+
