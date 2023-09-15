b48a7e54410ac395b8a78235589b1289615047de
yurrim <yurimkang20@gmail.com>
2023-09-15 11:03:00 +0900

    Upload. 22871 징검다리 건너기 (large)

--- python/이진탐색/Prob22871.py
+++ python/이진탐색/Prob22871.py
@@ -4,10 +4,40 @@
 import sys
 
 input = sys.stdin.readline
-N = int(input())                            # 돌의 개수
-rocks = list(map(int, input().split()))     # 돌의 수 Ai
+N = int(input())                                # 돌의 개수
+A = [0] + list(map(int, input().split()))       # 돌의 수 Ai
 
+def solution(start, end):
+    answer = 0
 
-# def solution():
+    while start <= end:
+        mid = (start + end) // 2
 
+        visited = [False] * (N+1)
+        flag = False
 
+        stack = [1]
+        visited[1] = True
+
+        while stack:
+            now = stack.pop()
+
+            if now == N:
+                flag = True
+                break
+
+            for idx in range(now+1, N+1):
+                temp = (idx - now) * (1 + abs(A[now] - A[idx]))
+                if temp <= mid and not visited[idx]:
+                    stack.append(idx)
+                    visited[idx] = True
+
+        if flag:
+            answer = mid
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return answer
+
+print(solution(1, (N-1) * (1 + abs(A[N] - A[1]))))
