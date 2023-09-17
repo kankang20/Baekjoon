2dfaa272bec27630280a3c11a89a27504bda8c45
yurrim <yurimkang20@gmail.com>
2023-09-17 19:44:48 +0900

    Upload. 1561 놀이 공원

--- python/이진탐색/Prob1561.py
+++ python/이진탐색/Prob1561.py
@@ -1,2 +1,16 @@
 
 """ 골드 2. 놀이 공원 """
+
+import sys
+
+input = sys.stdin.readline
+N, M = map(int, input())                    # 아이들의 수 N, 1인승 놀이기구 종류 M
+clock = list(map(int, input().split))       # 각 놀이기구의 운행 시간 
+
+def solution():
+
+    """
+        (1, 7) (2, 8) (3, 9) (4, 7) (5, 8)
+
+        (1, 3) (2, 2)
+    """
