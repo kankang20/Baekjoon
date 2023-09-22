0fc9af6ff76ccb3cab935f0e68c30a4fdce74b26
yurrim <yurimkang20@gmail.com>
2023-09-22 14:21:12 +0900

    Upload. 1로 만들기

--- 이코테/chapter08/1로 만들기.py
+++ 이코테/chapter08/1로 만들기.py
@@ -1,2 +1,26 @@
 
 """ 예제 8-3) 1로 만들기 """
+
+"""
+26
+
+>> 3
+"""
+
+x = 26
+
+dp = [0] * 30001        # 1 <= x <= 30000
+
+for idx in range(2, x+1):
+    
+    dp[idx] = dp[idx-1] + 1
+
+    if idx % 2 == 0:
+        dp[idx] = min(dp[idx], dp[idx//2] + 1)
+    if idx % 3 == 0:
+        dp[idx] = min(dp[idx], dp[idx//3] + 1)
+    if idx % 5 == 0:
+        dp[idx] = min(dp[idx], dp[idx//5] + 1)
+
+print(dp)
+print(dp[x])
