d6bf650ba6f113e776a9e9caf06bf21a04c0cada
yurrim <yurimkang20@gmail.com>
2023-09-22 13:56:14 +0900

    Upload. 피보나치(재귀)

--- 이코테/chapter08/피보나치(재귀).py
+++ 이코테/chapter08/피보나치(재귀).py
@@ -1,2 +1,21 @@
 
 """ 예제 8-1) 피보나치(재귀) """
+
+# 메모제이션
+dp = [0] * 100
+
+def fibo(x):
+
+    print('f(' + str(x) + ')', end=' ')
+    
+    if x == 1 or x == 2:
+        return 1
+    
+    if dp[x] != 0:
+        return dp[x]
+    
+    dp[x] = fibo(x-1) + fibo(x-2)
+    return dp[x]
+
+print(fibo(6))
+# print(fibo(99))
