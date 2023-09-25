7db496af3460f8345d7c7edc2b2da9ff9cc681be
yurrim <yurimkang20@gmail.com>
2023-09-25 11:42:22 +0900

    Upload. 1913 달팽이

--- python/구현/Prob1913.py
+++ python/구현/Prob1913.py
@@ -1,2 +1,69 @@
 
 """ 실버 3. 달팽이 """
+
+N = int(input())        # 달팽이의 크기
+answer = int(input())   # 찾아야 하는 수
+
+
+def solution():
+    graph = [[0] * N for _ in range(N)]
+    
+    r, c = N//2, N//2
+    flag = True             # True면 위로 오른쪽으로 / False면 아래로 왼쪽으로
+    flag_answer = True      # 좌표를 찾은 경우 False / 좌표를 찾지 못한 경우 True
+
+    dr = [-1, 0, 1, 0]             # 위 오른 아래 왼
+    dc = [0, 1, 0, -1]
+
+    dnr, dnc, number = 0, 0, 2
+    ansr, ansc = 0, 0
+
+    graph[r][c] = 1
+    if answer == 1: 
+        flag_answer = False
+        ansr, ansc = r, c
+
+    for num in range(1, N):
+        
+        if flag:
+            dnr, dnc = 0, 1
+            flag = False
+        else:
+            dnr, dnc = 2, 3
+            flag = True
+
+        for _ in range(num):
+            r += dr[dnr]
+            c += dc[dnr]
+            graph[r][c] = number
+            if flag_answer and number == answer: 
+                flag_answer = False
+                ansr, ansc = r, c
+            number += 1
+
+        for _ in range(num):
+            r += dr[dnc]
+            c += dc[dnc]
+            graph[r][c] = number
+            if flag_answer and number == answer: 
+                flag_answer = False
+                ansr, ansc = r, c
+            number += 1
+
+    for _ in range(num):
+        r += dr[0]
+        c += dc[0]
+        graph[r][c] = number
+        if flag_answer and number == answer: 
+                flag_answer = False
+                ansr, ansc = r, c
+        number += 1
+
+    for i in range(N):
+        for j in range(N):
+            print(graph[i][j], end=" ")
+        print()
+
+    print(ansr+1, ansc+1)
+    
+solution()
