8dbe80acece399031620aafdf98979d7fd76a4ad
yurrim <yurimkang20@gmail.com>
2023-09-28 23:30:30 +0900

    Upload. 2839 설탕 배달

--- python/다이나믹 프로그래밍1/Prob2839.py
+++ python/다이나믹 프로그래밍1/Prob2839.py
@@ -5,18 +5,12 @@ N = int(input())        # 배달해야 하는 설탕 무게
 
 def solution():
 
-    table = [0, 0, 0, 1, 0, 1]   # 0, 1, 2, 3, 4, 5
+    table = [float('inf') for _ in range(6)]   # 0, 1, 2, 3, 4, 5
+    table[3], table[5] = 1, 1
 
     for idx in range(6, N+1):
+        table.append(min(table[idx-3], table[idx-5]) + 1)
 
-        table.append(table[idx-3] + 1 if table[idx-3] else 0)           # 3
-        temp = table[idx-5] + 1 if table[idx-5] else 0                  # 5
+    return -1 if table[N] == float('inf') else table[N]
 
-        if table[idx] and temp:
-            table[idx] = min(temp, table[idx])
-        elif not table[idx]:
-            table[idx] = temp
-
-    return table[N]
-
-print(-1 if not solution() else solution())
+print(solution())
