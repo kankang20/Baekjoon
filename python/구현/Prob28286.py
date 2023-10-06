a981df54ebd559662e4a852081cab0bd51789ffa
yurrim <yurimkang20@gmail.com>
2023-10-06 21:59:42 +0900

    Upload. 28286 재채점을 기다리는 중

--- python/구현/Prob28286.py
+++ python/구현/Prob28286.py
@@ -6,33 +6,18 @@ C = list(map(int, input().split()))         # 각 문제의 정답을 나타내�
 P = list(map(int, input().split()))         # 민규가 OMR 카드에 기입한 답안
 
 answer = 0
-
-def pull(idx, array):     # 당기기
-    temp = array[:idx] + array[idx+1:] + [0]
-    return temp
-
-def push(idx, array):     # 밀기
-    temp = array[:idx] + [0] + array[idx:-1]
-    return temp
-
-def calculate_sum(array):
-    count = 0
-    for i in range(N):
-        if C[i] == array[i]:
-            count += 1
-    return count
     
-def check(array, K):
+def check(array, idx, K):
     global answer
-    answer = max(answer, calculate_sum(array))
+    answer = max(answer, sum(C[i] == array[i] for i in range(N)))
 
     if K:
         for idx in range(N):
-            check(pull(idx, array), K-1)
-            check(push(idx, array), K-1)
+            check(array[:idx] + array[idx+1:] + [0], idx+1, K-1)
+            check(array[:idx] + [0] + array[idx:-1], idx+1, K-1)
 
 def solution():
-    check(P, K)
+    check(P, 0, K)
     return answer
 
 print(solution())
