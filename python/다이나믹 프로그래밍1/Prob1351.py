faabd68d100f7c6d0acf67dec9c33bbb646a666b
yurrim <yurimkang20@gmail.com>
2023-10-09 21:23:54 +0900

    Upload. 1351 무한 수열

---
+++ python/다이나믹 프로그래밍1/Prob1351.py
@@ -0,0 +1,14 @@
+
+""" 골드 5. 무한 수열 """
+
+import sys
+import math
+
+N, P, Q = map(int, sys.stdin.readline())
+
+def solution():
+    
+    cal_P = math.floor(N/P)
+    cal_Q = math.floor(N/Q)
+
+    
