bfedd86188954af5f993eb819a5e6cf32799e5b1
yurrim <yurimkang20@gmail.com>
2023-10-09 21:37:30 +0900

    Upload. 1351 무한 수열

--- python/다이나믹 프로그래밍1/Prob1351.py
+++ python/다이나믹 프로그래밍1/Prob1351.py
@@ -4,11 +4,21 @@
 import sys
 import math
 
-N, P, Q = map(int, sys.stdin.readline())
+N, P, Q = map(int, sys.stdin.readline().split())
 
 def solution():
     
-    cal_P = math.floor(N/P)
-    cal_Q = math.floor(N/Q)
+    max_value = max(N//P, N//Q)
 
-    
+    table = [0] * (max_value+1)
+    table[0] = 1
+
+    if N == 0:
+        return table[0]
+
+    for idx in range(1, max_value+1):
+        table[idx] = table[idx//P] + table[idx//Q]
+
+    return table[N//P] + table[N//Q]
+
+print(solution())
