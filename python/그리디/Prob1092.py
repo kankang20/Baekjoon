7fbd835dd44e6935e4df1aff18eedc1a4ae49a83
yurrim <yurimkang20@gmail.com>
2023-10-14 18:58:26 +0900

    Upload. 1092 배

--- python/그리디/Prob1092.py
+++ python/그리디/Prob1092.py
@@ -17,24 +17,27 @@ def solution():
         return -1
     
     # 박스를 배로 옮길 수 있으면
-    crane_weight.sort(reverse=True)
-    box_weight.sort()
+    crane_weight.sort(reverse=True)         # 내림차순  
+    box_weight.sort()                       # 오름차순
 
     answer = 0
     
+    # 가장 무거운 box부터 먼저 배로 옮기자
     while box_weight:
+        
+        # 제일 큰 crane부터 box 탐색
+        for crane in crane_weight:                  
 
-        for crane in crane_weight:
-            
-            idx = len(box_weight)-1
+            # 가장 무거운 box부터 crane이 옮길 수 있는 box 탐색          
+            idx = len(box_weight)-1              
 
-            while box_weight and idx >= 0:
+            while box_weight and idx >= 0:          
                 if box_weight[idx] <= crane:
-                    box_weight.pop(idx)
+                    box_weight.pop(idx)         # 옮긴 box는 삭제
                     break
                 idx -= 1
 
-        answer += 1
+        answer += 1     # 모든 크레인이 박스를 탐색했으니 하루 끝!
 
     return answer
 
