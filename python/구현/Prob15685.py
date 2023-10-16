fefc21a60ae5d582d6e154dbe0e382b98782bb4b
yurrim <yurimkang20@gmail.com>
2023-10-16 21:48:35 +0900

    Upload. 15685 드래곤 커브

--- python/구현/Prob15685.py
+++ python/구현/Prob15685.py
@@ -34,7 +34,7 @@ def solution():
                 q.append((endx + endy - starty, endy - endx + startx))
         
         # 3. 최종 드래곤 코드에 추가
-        answer += q
+        answer += set(q)
 
     answer = set(answer)
 
