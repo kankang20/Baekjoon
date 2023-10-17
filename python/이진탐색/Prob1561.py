3d97c6abd875315e359760671786193e2357a1a5
yurrim <yurimkang20@gmail.com>
2023-10-17 16:59:28 +0900

    Upload. 1561 놀이 공원

--- python/이진탐색/Prob1561.py
+++ python/이진탐색/Prob1561.py
@@ -3,14 +3,41 @@
 
 import sys
 
-input = sys.stdin.readline
-N, M = map(int, input())                    # 아이들의 수 N, 1인승 놀이기구 종류 M
-clock = list(map(int, input().split))       # 각 놀이기구의 운행 시간 
-
 def solution():
 
-    """
-        (1, 7) (2, 8) (3, 9) (4, 7) (5, 8)
+    N, M = map(int, sys.stdin.readline().split())               # 아이들의 수 N, 1인승 놀이기구 종류 M
+    plays = list(map(int, sys.stdin.readline().split()))        # 각 놀이기구의 운행 시간 
 
-        (1, 3) (2, 2)
-    """
+    if N <= M:
+        return N
+    
+    answer = 0
+    start, end = 0, 2000000000 * 30
+
+    # 시간 계산
+    while start <= end:
+        mid = (start + end) // 2
+
+        count = M
+        for p in plays:
+            count += mid // p
+
+        if N <= count:
+            answer = mid
+            end = mid - 1
+        else:
+            start = mid + 1 
+
+    # 마지막 아이가 타게되는 놀이기구
+    last_child, idx = M, 0
+
+    for p in plays:
+        last_child += (answer - 1) // p
+
+    while last_child < N:
+        last_child += (1 if answer % plays[idx] == 0 else 0)
+        idx += 1
+    
+    return idx
+
+print(solution())
