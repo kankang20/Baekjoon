e93e4caf4a4f0d392aad47847ef9324774f1a466
yurrim <yurimkang20@gmail.com>
2023-10-19 18:22:37 +0900

    Upload. 1092 배

---
+++ python/이진탐색/Prob1092.py
@@ -0,0 +1,18 @@
+
+""" 골드 5. 배 """
+
+import sys
+
+input = sys.stdin.readline
+
+
+
+def solution():
+
+
+    N = int(input())                                    # 크레인의 개수
+    crane_weight = list(map(int, input().split()))      # 각 크레인의 무게 제한
+    M = int(input())                                    # 박스의 수
+    box_weight = list(map(int, input().split()))        # 각 박스의 무게
+
+solution()
