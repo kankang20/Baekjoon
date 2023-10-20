a0a8a626177c7ef026446ed340e8e42cd3d88602
yurrim <yurimkang20@gmail.com>
2023-10-20 16:50:50 +0900

    Upload. 1167 트리의 지름

--- python/그래프 탐색/Prob1167.py
+++ python/그래프 탐색/Prob1167.py
@@ -1,2 +1,47 @@
 
 """ 골드 2. 트리의 지름 """
+
+import sys
+from collections import deque
+
+def solution():
+
+    input = sys.stdin.readline
+
+    V = int(input())    # 트리의 정점 개수
+
+    graph = [[] * (V+1) for _ in range(V+1)]
+    
+    for _ in range(V):
+        temp = list(map(int, input().split()))
+        for idx in range(1, len(temp)-1, 2):
+            graph[temp[0]].append((temp[idx], temp[idx+1]))
+    
+    answer = 0
+
+    for idx in range(1, V+1):
+        answer = max(answer, search(graph, V, idx))
+
+    return answer
+
+def search(graph, V, start):
+    
+    visited = [False] * (V+1)
+    distance = [0] * (V+1)
+
+    q = deque()
+
+    q.append(start)
+    visited[start] = True
+    distance[start] = 0
+    
+    while q:
+
+        node, dis = q.popleft()
+
+        
+
+    return max(distance)
+
+
+print(solution())
