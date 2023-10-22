dccf4ba1f728c8546692a582149a933160225afe
yurrim <yurimkang20@gmail.com>
2023-10-22 11:35:28 +0900

    Upload. 1167 트리의 지름

--- python/그래프 탐색/Prob1167.py
+++ python/그래프 탐색/Prob1167.py
@@ -17,35 +17,36 @@ def solution():
         for idx in range(1, len(temp)-1, 2):
             graph[temp[0]].append((temp[idx], temp[idx+1]))
     
-    answer = 0
+    idx, distance = search(graph, V, 1)
+    idx, distance = search(graph, V, idx)
 
-    for idx in range(1, V+1):
-        answer = max(answer, search(graph, V, idx))
-
-    return answer
+    return distance
 
 def search(graph, V, start):
     
     visited = [False] * (V+1)
-    distance = [0] * (V+1)
 
     q = deque()
 
     q.append((start, 0))
     visited[start] = True
-    distance[start] = 0
+
+    idx, distance = start, 0
     
     while q:
 
         node, dis = q.popleft()
 
+        if distance < dis:
+            distance = dis
+            idx = node
+
         for (n, d) in graph[node]:
             if not visited[n]:
                 visited[n] = True
-                distance[n] = dis + d
-                q.append((n, distance[n]))
-        
-    return max(distance)
+                q.append((n, dis + d))
+    
+    return idx, distance
 
 
 print(solution())
