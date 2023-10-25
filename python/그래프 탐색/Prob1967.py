ef65550e58f27bcf0f6a55f4c9374fae7d00df6c
yurrim <yurimkang20@gmail.com>
2023-10-25 16:04:48 +0900

    Upload. 1967 트리의 지름

--- python/그래프 탐색/Prob1967.py
+++ python/그래프 탐색/Prob1967.py
@@ -2,41 +2,47 @@
 """ 골드 4. 트리의 지름 """
 
 import sys
-sys.setrecursionlimit(10**6)        # 이거 없으면 RecursionError
+from collections import deque
 
 input = sys.stdin.readline
 
-N = int(input())    # 노드의 개수
-
-graph = [[] for _ in range(N+1)]
-for _ in range(N-1):
-    a, b, c = map(int, input().split())     # 부모 노드 a, 자식 노드 b, 가중치 c
-    graph[a].append((b, c))
-    graph[b].append((a, c))
-
-idx, answer = 0, 0
-
 def solution():
 
+    N = int(input())    # 노드의 개수
+
+    graph = [[] for _ in range(N+1)]
+    for _ in range(N-1):
+        a, b, c = map(int, input().split())     # 부모 노드 a, 자식 노드 b, 가중치 c
+        graph[a].append((b, c))
+        graph[b].append((a, c))
+
+    i, _ = search(N, graph, 1)
+    _, answer = search(N, graph, i)
+
+    return answer
+    
+def search(N, graph, start):
+
+    q = deque()
     visited = [False] * (N+1)
-    visited[1] = True
-    search(visited, 1, 0)
 
-    visited = [False] * (N+1)
-    visited[idx] = True
-    search(visited, idx, 0)
+    q.append((start, 0))
+    visited[start] = True
 
+    idx, answer = start, 0
 
-def search(visited, n, d):
+    while q:
 
-    global answer, idx
-    if answer < d:
-        idx, answer = n, d
+        n, d = q.popleft()
 
-    for (node, distance) in graph[n]:
-        if not visited[node]:
-            visited[node] = True
-            search(visited, node, d + distance)
+        if answer < d:
+            idx, answer = n, d
 
-solution()
-print(answer)
+        for (node, distance) in graph[n]:
+            if not visited[node]:
+                visited[node] = True
+                q.append((node, d + distance))
+
+    return idx, answer
+
+print(solution())
