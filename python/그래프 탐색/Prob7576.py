c00507b4b4e6d8faffa1137ed759af1fcee5df63
yurrim <yurimkang20@gmail.com>
2023-11-03 15:05:16 +0900

    Upload. 7576 토마토

---
+++ python/그래프 탐색/Prob7576.py
@@ -0,0 +1,64 @@
+
+""" 골드 5. 토마토 """
+
+import sys
+from collections import deque
+
+def solution():
+
+    input = sys.stdin.readline
+
+    C, R = map(int, input().split())                                # 상자의 가로칸, 세로칸
+    box = [list(map(int, input().split())) for _ in range(R)]       # 토마토 박스
+
+    return search(R, C, box)
+
+def search(R, C, box):
+
+    dr = [0, 0, -1, 1]
+    dc = [1, -1, 0, 0]
+
+    q = deque()
+
+    yet_tomato = 0
+    answer = 0
+
+    # 익지 않은 토마토와 익은 토마토
+    for r in range(0, R):
+        for c in range(0, C):
+            if box[r][c] == 0:
+                yet_tomato += 1
+            elif box[r][c] == 1:
+                q.append((r, c))
+
+    while not yet_tomato:
+
+        for _ in range(len(q)):
+
+            r, c = q.popleft()
+
+            for d in range(4):
+
+                nr = r + dr[d]
+                nc = c + dc[d]
+
+                if nr < 0 or nc < 0 or nr >= R or nc >= C or box[nr][nc] == -1 or box[nr][nc] == 1:
+                    continue
+
+                box[nr][nc] = 1
+                q.append((nr, nc))
+
+            yet_tomato -= 1
+
+        answer += 1
+
+    return answer
+
+
+
+
+
+
+
+
+print(solution())
