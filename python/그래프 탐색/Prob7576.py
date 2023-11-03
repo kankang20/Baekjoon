32d7fca104375e6583a89928791512ac2ea52cd9
yurrim <yurimkang20@gmail.com>
2023-11-03 15:40:02 +0900

    Upload. 7576 토마토

--- python/그래프 탐색/Prob7576.py
+++ python/그래프 탐색/Prob7576.py
@@ -21,7 +21,7 @@ def search(R, C, box):
     q = deque()
 
     yet_tomato = 0
-    answer = 0
+    answer = -1
 
     # 익지 않은 토마토와 익은 토마토
     for r in range(0, R):
@@ -31,14 +31,16 @@ def search(R, C, box):
             elif box[r][c] == 1:
                 q.append((r, c))
 
-    while not yet_tomato:
+    while q:
+
+        answer += 1
 
         for _ in range(len(q)):
 
             r, c = q.popleft()
 
             for d in range(4):
-
+                
                 nr = r + dr[d]
                 nc = c + dc[d]
 
@@ -48,17 +50,49 @@ def search(R, C, box):
                 box[nr][nc] = 1
                 q.append((nr, nc))
 
-            yet_tomato -= 1
+                yet_tomato -= 1
 
-        answer += 1
+    return -1 if yet_tomato else answer
 
-    return answer
+# def search(R, C, box):
 
+#     dr = [0, 0, -1, 1]
+#     dc = [1, -1, 0, 0]
 
+#     q = deque()
 
+#     yet_tomato = 0
+#     answer = -1
 
+#     # 익지 않은 토마토와 익은 토마토
+#     for r in range(0, R):
+#         for c in range(0, C):
+#             if box[r][c] == 0:
+#                 yet_tomato += 1
+#             elif box[r][c] == 1:
+#                 q.append((r, c))
 
+#     while q:
 
+#         answer += 1
 
+#         for _ in range(len(q)):
+
+#             r, c = q.popleft()
+
+#             for d in range(4):
+
+#                 nr = r + dr[d]
+#                 nc = c + dc[d]
+
+#                 if nr < 0 or nc < 0 or nr >= R or nc >= C or box[nr][nc] == -1 or box[nr][nc] == 1:
+#                     continue
+
+#                 box[nr][nc] = 1
+#                 q.append((nr, nc))
+
+#                 yet_tomato -= 1
+
+#     return -1 if yet_tomato else answer
 
 print(solution())
