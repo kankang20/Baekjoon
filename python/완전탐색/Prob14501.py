c560a5e64b022dee5051ee601fb726a1edad6946
yurrim <yurimkang20@gmail.com>
2023-11-04 17:41:42 +0900

    Upload. 14501 퇴사

--- python/완전탐색/Prob14501.py
+++ python/완전탐색/Prob14501.py
@@ -3,22 +3,17 @@
 
 import sys
 
+input = sys.stdin.readline
+
+N = int(input())    # 퇴사 날짜
+M = []              # 상담하는데 걸리는 기간, 돈
+for _ in range(N):
+    x, y = map(int, input().split())
+    M.append((x, y))
+
 answer = 0
 
-def solution():
-
-    input = sys.stdin.readline
-
-    N = int(input())    # 퇴사 날짜
-    M = []              # 상담하는데 걸리는 기간, 돈
-    for _ in range(N):
-        x, y = map(int, input().split())
-        M.append((x, y))
-
-    search(N, M, 0, 0)
-    return answer
-
-def search(N, M, day, money):
+def solution(day, money):
 
     if day > N:
         return
@@ -27,6 +22,7 @@ def search(N, M, day, money):
     answer = max(money, answer)
 
     for idx in range(day, N):
-        search(N, M, idx + M[idx][0], money + M[idx][1])
+        solution(idx + M[idx][0], money + M[idx][1])
 
-print(solution())
+solution(0, 0)
+print(answer)
