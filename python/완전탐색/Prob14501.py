2aaf7278468cac5472a6af7679e424225a658c50
yurrim <yurimkang20@gmail.com>
2023-11-04 17:39:03 +0900

    Upload. 14501 퇴사

---
+++ python/완전탐색/Prob14501.py
@@ -0,0 +1,32 @@
+
+""" 실버 3. 퇴사 """
+
+import sys
+
+answer = 0
+
+def solution():
+
+    input = sys.stdin.readline
+
+    N = int(input())    # 퇴사 날짜
+    M = []              # 상담하는데 걸리는 기간, 돈
+    for _ in range(N):
+        x, y = map(int, input().split())
+        M.append((x, y))
+
+    search(N, M, 0, 0)
+    return answer
+
+def search(N, M, day, money):
+
+    if day > N:
+        return
+    
+    global answer
+    answer = max(money, answer)
+
+    for idx in range(day, N):
+        search(N, M, idx + M[idx][0], money + M[idx][1])
+
+print(solution())
