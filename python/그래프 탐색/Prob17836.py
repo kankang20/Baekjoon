59638a4ecf98ce9a423f3b1b104879543f519abb
yurrim <yurimkang20@gmail.com>
2023-11-07 20:47:21 +0900

    Upload. 17836 공주님을 구해라!

--- python/그래프 탐색/Prob17836.py
+++ python/그래프 탐색/Prob17836.py
@@ -1,10 +1,50 @@
 
 """ 골드 5. 공주님을 구해라! """
 
+import sys
+from collections import deque
+
 def solution():
 
-    answer = 0
+    input = sys.stdin.readline
+    R, C, T = map(int, input().split())                             # 성의 크기 N, M, 제한 시간 T
+    graph = [list(map(int, input().split())) for _ in range(R)]     # 성
 
-    return answer
+    answer = search(R, C, T, graph) 
+    return answer if answer <= T else "Fail"
+
+def search(R, C, T, graph):
+
+    dir = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+    q = deque()
+    
+    q.append((0, 0, 0))      # 현재 위치, 시간, 그람 소지 여부
+    graph[0][0] = -1
+
+    gram_distance = T+1
+
+    while q:
+
+        r, c, time = q.popleft()
+        
+        if r == R-1 and c == C-1:
+            return min(time, gram_distance)
+        
+        for d in range(4):
+            
+            nr = r + dir[d][0]
+            nc = c + dir[d][1]
+
+            if nr < 0 or nc < 0 or nr >= R or nc >= C or graph[nr][nc] == 1 or graph[nr][nc] == -1:
+                continue
+
+            if graph[nr][nc] == 2:
+                gram_distance = time + 1 + ((R-1) - nr) + ((C-1) - nc)
+            
+            q.append((nr, nc, time+1))
+            graph[nr][nc] = -1
+                
+    return gram_distance
 
 print(solution())
