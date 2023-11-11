7084ff269dab60646f60766e2d723898b90e3942
yurrim <yurimkang20@gmail.com>
2023-11-11 20:01:28 +0900

    Upload. 2230 수 고르기

--- python/투 포인터/Prob2230.py
+++ python/투 포인터/Prob2230.py
@@ -6,6 +6,24 @@ def solution():
     N, M = map(int, input().split())        # 수열 크기 N, 차이 M
     numbers = [int(input()) for _ in range(N)]
 
-    return 0
+    if N == 1 or M == 0:
+        return 0
+
+    numbers.sort()
+
+    start, end = 0, 1
+    answer = numbers[-1] - numbers[0]
+
+    while end < len(numbers):
+
+        diff = numbers[end] - numbers[start]
+
+        if M <= diff:
+            answer = min(answer, diff)
+            start += 1
+        else:
+            end += 1
+
+    return answer
 
 print(solution())
