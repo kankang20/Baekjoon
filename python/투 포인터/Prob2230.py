7ef8614828bc905ce627f39af4d47ebebebbc4cc
yurrim <yurimkang20@gmail.com>
2023-11-11 19:59:44 +0900

    Upload. 2230 수 고르기

---
+++ python/투 포인터/Prob2230.py
@@ -0,0 +1,11 @@
+
+""" 골드 5. 수 고르기 """
+
+def solution():
+
+    N, M = map(int, input().split())        # 수열 크기 N, 차이 M
+    numbers = [int(input()) for _ in range(N)]
+
+    return 0
+
+print(solution())
