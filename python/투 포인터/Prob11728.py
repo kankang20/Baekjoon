2668168263f63a8dcb77a8bb27ba098607690a2c
yurrim <yurimkang20@gmail.com>
2023-11-12 13:21:43 +0900

    Upload. 11728 배열 합치기

---
+++ python/투 포인터/Prob11728.py
@@ -0,0 +1,16 @@
+
+""" 실버 5. 배열 합치기 """
+
+import sys
+
+def solution():
+    input = sys.stdin.readline
+
+    N, M = map(int, input().split())    # 배열 A의 크기 N, 배열 B의 크기 B
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+
+    print(A, B)
+
+
+print(solution())
