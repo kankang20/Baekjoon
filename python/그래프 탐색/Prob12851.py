85dcf188f71862b9be63100bfdcd9a6b4d5a5b81
yurrim <yurimkang20@gmail.com>
2023-11-15 23:39:25 +0900

    Upload. 12851 숨바꼭질 2

---
+++ python/그래프 탐색/Prob12851.py
@@ -0,0 +1,15 @@
+
+""" 골드 4. 숨바꼭질 2 """
+
+import sys
+
+def solution():
+
+    input = sys.stdin.readline
+    N, M = map(int, input().split())        # 수빈이 위치 N, 동생 위치 K
+    
+    answer = 0
+
+    return 0
+
+print(solution())
