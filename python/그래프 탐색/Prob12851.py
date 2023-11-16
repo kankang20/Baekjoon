e2453d6c326b724d112b076b0b3387d9aa95f808
yurrim <yurimkang20@gmail.com>
2023-11-16 23:54:09 +0900

    Upload. 12851 숨바꼭질 2

--- python/그래프 탐색/Prob12851.py
+++ python/그래프 탐색/Prob12851.py
@@ -2,14 +2,43 @@
 """ 골드 4. 숨바꼭질 2 """
 
 import sys
+from collections import deque
 
 def solution():
 
     input = sys.stdin.readline
-    N, M = map(int, input().split())        # 수빈이 위치 N, 동생 위치 K
-    
-    answer = 0
+    N, K = map(int, input().split())        # 수빈이 위치 N, 동생 위치 K
 
-    return 0
+    return search(N, K)
 
-print(solution())
+def search(N, K):
+
+    visited = [0] * (100001)
+
+    q = deque()
+    q.append((N, 0))
+
+    value, count = 1e9, 0
+
+    while q:
+
+        now, move = q.popleft()
+
+        if move > value:
+            continue
+
+        if now == K:
+            value = move
+            count += 1
+            continue
+
+        for next in (now-1, now+1, now*2):
+            if 0 <= next <= 100000 and (not visited[next] or visited[next] == visited[now] + 1):
+                q.append((next, move+1))
+                visited[next] = visited[now] + 1
+
+    return value, count
+
+value, count = solution()
+print(value)
+print(count)
