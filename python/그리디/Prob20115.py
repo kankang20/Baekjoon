306075ff452a639ddebfd10acb9b8a272470b7e5
yurrim <yurimkang20@gmail.com>
2023-11-17 18:01:55 +0900

    Upload. 20115 에너지 드링크

---
+++ python/그리디/Prob20115.py
@@ -0,0 +1,10 @@
+
+""" 실버 3. 에너지 드링크 """
+
+def solution():
+
+    N = int(input())                            # 에너지 드링크 수
+    drinks = list(map(int, input().split()))    # 각 에너지 드링크의 양
+
+
+print(solution())
