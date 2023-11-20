fea524fd72a243c84dd80a69260006f62b6a36c2
yurrim <yurimkang20@gmail.com>
2023-11-20 11:17:23 +0900

    Upload. 21921 블로그

---
+++ python/누적합/Prob21921.py
@@ -0,0 +1,14 @@
+
+""" 실버 3. 블로그 """
+
+import sys
+
+def solution():
+
+    input = sys.stdin.readline
+    N, X = map(int, input().split())
+    visitors = list(map(int, input().split()))
+
+    
+
+solution()
