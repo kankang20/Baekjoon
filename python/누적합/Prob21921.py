375bae014b008bf28d884c25598eb2d6f7b5f91e
yurrim <yurimkang20@gmail.com>
2023-11-20 11:29:36 +0900

    Upload. 21921 블로그

--- python/누적합/Prob21921.py
+++ python/누적합/Prob21921.py
@@ -9,6 +9,24 @@ def solution():
     N, X = map(int, input().split())
     visitors = list(map(int, input().split()))
 
-    
+    prefix_sum = [visitors[0]]
+    for idx in range(1, N):
+        prefix_sum.append(prefix_sum[idx-1] + visitors[idx])
+
+    answer = prefix_sum[X-1]
+    count = 1
+
+    for idx in range(X, N):
+        value = prefix_sum[idx] - prefix_sum[idx-X]
+        if answer == value:
+            count += 1
+        elif answer < value:
+            answer = value
+            count = 1
+
+    if answer:
+        print(answer, count, sep="\n")
+    else:
+        print("SAD")
 
 solution()
