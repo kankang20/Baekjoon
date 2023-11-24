9510abb44900884e5c2eb7dbd9154bcc2c7f3664
yurrim <yurimkang20@gmail.com>
2023-11-24 16:42:38 +0900

    Upload. 1713 후보 추천하기

--- python/구현/Prob1713.py
+++ python/구현/Prob1713.py
@@ -10,13 +10,31 @@ def solution():
     total_count = int(input())                  # 전체 학생의 총 추천 횟수
     numbers = list(map(int, input().split()))   # 추천 순서
 
-    answer = {}     # (학생 번호, 추천 순서) : 추천 횟수
+    answer = {}     # 학생 번호 : 추천 횟수
 
     for idx in range(total_count):
 
         num = numbers[idx]  # 추천하는 학생 번호
 
-        # if num in 
+        if num in answer.keys():
+            answer[num] = answer[num] + 1
+            continue
+
+        if len(answer) == N:
+            answer.pop(remove(answer))
+        answer[num] = 1
+
+    for num in sorted(answer.keys()):
+        print(num, end=" ")
 
 
-print(solution())
+def remove(answer: dict):
+    
+    min_value = min(answer.values())
+
+    for idx in answer.keys():
+        if answer[idx] == min_value:
+            return idx
+    
+
+solution()
