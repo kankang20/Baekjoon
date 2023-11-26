47f6d66b7b730feead6aacd4040ce926fadda4c3
yurrim <yurimkang20@gmail.com>
2023-11-26 15:52:25 +0900

    Upload. 1181 단어 정렬

---
+++ python/문자열/Prob1181.py
@@ -0,0 +1,14 @@
+
+""" 실버 5. 단어 정렬 """
+
+import sys
+
+def solution():
+
+    input = sys.stdin.readline
+    N = int(input())                # 단어 개수
+    words = []
+    for _ in range(N):
+        words.append(input())
+
+solution()
