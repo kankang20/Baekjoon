f8c4eb6e16702ebf295b797f31335ecf1ebed715
yurrim <yurimkang20@gmail.com>
2023-12-06 14:05:21 +0900

    Upload. 16398 행성 연결

--- python/최소 스패닝 트리/Prob16398.py
+++ python/최소 스패닝 트리/Prob16398.py
@@ -2,7 +2,6 @@
 """ 골드 4. 행성 연결 """
 
 import sys
-from heapq import heappush, heappop
 
 def find(parent, a):
     if parent[a] != a:
@@ -22,26 +21,25 @@ def solution():
 
     input = sys.stdin.readline
     N = int(input())                # 행성의 수
+    parent = [i for i in range(N)]
 
     edges = []
     for i in range(N):
         for (j, cost) in enumerate(list(map(int, input().split()))):
             if cost > 0:
-                heappush(edges, (cost, i, j))
+                edges.append((cost, i, j))
+
+    edges.sort()
 
     answer = 0
     count = 0
 
-    parent = [i for i in range(N)]
-
-    while edges:
-        cost, i, j = heappop(edges)
-
+    for cost, i, j in edges:
         if find(parent, i) != find(parent, j):
             union(parent, i, j)
             answer += cost
             count += 1
-        
+
         if count == N-1:
             break
 
