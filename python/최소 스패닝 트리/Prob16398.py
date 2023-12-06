492a459c81d43a2394fd34a509ef4f764bb4b383
yurrim <yurimkang20@gmail.com>
2023-12-06 15:17:38 +0900

    Upload. 16398 행성 연결

--- python/최소 스패닝 트리/Prob16398.py
+++ python/최소 스패닝 트리/Prob16398.py
@@ -2,48 +2,47 @@
 """ 골드 4. 행성 연결 """
 
 import sys
-from collections import defaultdict
-from heapq import heappush, heappop
 
-def prim(N, edges, start):
+def find(parent, a):
+    if parent[a] != a:
+        parent[a] = find(parent, parent[a])
+    return parent[a]
 
-    heap = []
-    connected = [False] * (N)
-    selected = defaultdict(list)
-
-    for i, j, count in edges:
-        selected[i].append((count, i, j))
-        selected[j].append((count, j, i))
-
-    connected[start] = True
-    for count, i, j in selected[start]:
-        heappush(heap, (count, i, j))
-
-    answer = 0
-
-    while heap:
-        count, i, j = heappop(heap)
-        if not connected[j]:
-            connected[j] = True
-            answer += count
-
-            for edge in selected[j]:
-                if not connected[edge[2]]:
-                    heappush(heap, edge)
-
-    return answer
+def union(parent, a, b):
+    a = find(parent, a)
+    b = find(parent, b)
+    
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
 
 def solution():
 
     input = sys.stdin.readline
-    N = int(input())
+    N = int(input())                # 행성의 수
+    parent = [i for i in range(N)]
 
     edges = []
     for i in range(N):
         for (j, cost) in enumerate(list(map(int, input().split()))):
             if cost > 0:
-                edges.append((i, j, cost))
+                edges.append((cost, i, j))
 
-    return prim(N, edges, 0)
+    edges.sort()
+
+    answer = 0
+    count = 0
+
+    for cost, i, j in edges:
+        if find(parent, i) != find(parent, j):
+            union(parent, i, j)
+            answer += cost
+            count += 1
+
+        if count == N-1:
+            break
+
+    return answer
 
 print(solution())
