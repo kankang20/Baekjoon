9d7102443313198a7ef346e85f613364be20a1cf
yurrim <yurimkang20@gmail.com>
2023-12-07 16:11:18 +0900

    Upload. 10828 스택

--- python/자료구조/Prob10828.py
+++ python/자료구조/Prob10828.py
@@ -8,9 +8,21 @@ def solution():
     input = sys.stdin.readline
     N = int(input())                # 명령 수
 
+    stack = []
+
     for _ in range(N):
 
         command = input().split()
-        print(command)
+        
+        if command[0] == "push":
+            stack.append(command[1])
+        elif command[0] == "pop":
+            print(stack.pop() if stack else -1)
+        elif command[0] == "size":
+            print(len(stack))
+        elif command[0] == "empty":
+            print(0 if stack else 1)
+        elif command[0] == "top":
+            print(stack[-1] if stack else -1)
 
 solution()
