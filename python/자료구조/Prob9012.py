1482e773aabe7eac85d815e91a0ec8bd461f94af
yurrim <yurimkang20@gmail.com>
2023-12-08 16:00:27 +0900

    Upload. 9012 괄호

---
+++ python/자료구조/Prob9012.py
@@ -0,0 +1,15 @@
+
+""" 실버 4. 괄호 """
+
+import sys
+
+def solution():
+
+    input = sys.stdin.readline
+    testcase = int(input())                # 테스트 케이스 개수
+
+    for _ in range(testcase):
+        
+        word = input()
+
+solution()
