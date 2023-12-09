3e0198f0119644444222f4d065351af178dd2f3b
yurrim <yurimkang20@gmail.com>
2023-12-09 23:04:52 +0900

    Upload. 2164 카드2

--- python/자료구조/Prob2164.py
+++ python/자료구조/Prob2164.py
@@ -2,11 +2,19 @@
 """ 실버 4. 카드2 """
 
 import sys
+from collections import deque
 
 def solution():
 
     input = sys.stdin.readline
 
-    N = int(input())                # 카드 개수
+    N = int(input())    
+    queue = deque([i for i in range(1, N+1)])
+
+    while len(queue) > 1:
+        queue.popleft()
+        queue.append(queue.popleft())
+
+    return queue[0]
 
 print(solution())
