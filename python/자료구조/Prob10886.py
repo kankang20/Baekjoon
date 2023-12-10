22f44c64eaec1a7f59fb584aa48badf8bb3ca1f0
yurrim <yurimkang20@gmail.com>
2023-12-10 13:17:24 +0900

    Upload. 10886 덱

---
+++ python/자료구조/Prob10886.py
@@ -0,0 +1,18 @@
+
+""" 실버 4. 덱 """
+
+import sys
+
+def solution():
+
+    input = sys.stdin.readline
+    N = int(input())
+
+    for _ in range(N):
+
+        command = input().split()
+
+        # if command[0] == "push_back":
+
+
+solution()
