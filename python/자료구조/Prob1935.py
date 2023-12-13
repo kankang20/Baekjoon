3b6cf272d7f324253204c287973632ef54a658b3
yurrim <yurimkang20@gmail.com>
2023-12-13 12:20:54 +0900

    Upload. 1935 후위 표기식2

---
+++ python/자료구조/Prob1935.py
@@ -0,0 +1,15 @@
+
+""" 실버 3. 후외 표기식2 """
+
+import sys
+
+def solution():
+
+    input = sys.stdin.readline
+
+    N = int(input())                        # 피연산자의 개수
+    command = input().rstrip()              # 후위 표기식
+
+    
+
+solution()
