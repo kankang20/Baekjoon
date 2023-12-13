1396f423a0349e940eabd3e1738727aafa0bedb7
yurrim <yurimkang20@gmail.com>
2023-12-13 12:43:22 +0900

    Upload. 1935 후위 표기식2

--- python/자료구조/Prob1935.py
+++ python/자료구조/Prob1935.py
@@ -7,9 +7,37 @@ def solution():
 
     input = sys.stdin.readline
 
-    N = int(input())                        # 피연산자의 개수
-    command = input().rstrip()              # 후위 표기식
+    N = int(input())                # 피연산자의 개수
+    command = input().rstrip()      # 후위 표기식
+    number = [int(input()) for _ in range(N)]   
 
-    
+    start, end = ord("A"), ord("Z")
 
-solution()
+    stack = []
+    for c in command:
+
+        if start <= ord(c) <= end:
+            stack.append(number[ord(c) - start])
+            continue
+
+        num2 = stack.pop()
+        num1 = stack.pop()
+
+        stack.append(calculate_number(c, num1, num2))
+
+    return "{:.2f}".format(stack.pop())
+
+
+def calculate_number(check, num1, num2):
+
+    if check == "+":
+        return num1 + num2
+    elif check == "-":
+        return num1 - num2
+    elif check == "*":
+        return num1 * num2
+    elif check == "/":
+        return num1 / num2
+
+
+print(solution())
