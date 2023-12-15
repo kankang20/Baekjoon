bab306203f469ef20b478bcdc3a15a639a7981cb
yurrim <yurimkang20@gmail.com>
2023-12-15 17:13:38 +0900

    Upload. 12851 숨바꼭질 2

--- python/그래프 탐색/Prob12851.py
+++ python/그래프 탐색/Prob12851.py
@@ -33,9 +33,9 @@ def search(N, K):
             continue
 
         for next in (now-1, now+1, now*2):
-            if 0 <= next <= 100000 and (not visited[next] or visited[next] == visited[now] + 1):
+            if 0 <= next <= 100000 and (not visited[next] or visited[next] == move + 1):
                 q.append((next, move+1))
-                visited[next] = visited[now] + 1
+                visited[next] = move + 1
 
     return value, count
 
