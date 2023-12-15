604a46e1f115eb457d1c4de9c5d36109d50ce308
yurrim <yurimkang20@gmail.com>
2023-12-15 16:26:51 +0900

    Upload. 2346 풍선 터뜨리기

--- python/자료구조/Prob2346.py
+++ python/자료구조/Prob2346.py
@@ -2,11 +2,25 @@
 """ 실버 3. 풍선 터뜨리기 """
 
 import sys
+from collections import deque
 
 def solution():
 
-    input = sys.stdin.readline()
-    N = int(input())                                # 풍선 개수
-    ballons = list(map(int, input().split()))       # 각 풍선 안의 종이에 적혀있는 수
+    input = sys.stdin.readline
+    N = int(input())                                                                                # 풍선 개수
+    ballons = deque((idx, num) for (idx, num) in enumerate(list(map(int, input().split()))))        # 각 풍선 안의 종이에 적혀있는 수
 
-print(solution())
+    while ballons:
+
+        (idx, number) = ballons.popleft()
+        print(idx+1, end = " ")
+
+        if ballons:
+
+            number += (-1 if number > 0 else len(ballons))
+            
+            while number:
+                ballons.append(ballons.popleft())
+                number -= 1
+
+solution()
