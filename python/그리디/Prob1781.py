5e8f272dfa2ffdb3eb180fb6240448f2b654dbd2
yurrim <yurimkang20@gmail.com>
2023-12-15 17:14:37 +0900

    Upload. 1781 컵라면

--- python/그리디/Prob1781.py
+++ python/그리디/Prob1781.py
@@ -2,14 +2,54 @@
 """ 골드 2. 컵라면 """
 
 import sys
+from heapq import heappush, heappop, heapify
+
+# 2 6 3 1 7 5 4
+# 1 2 3 1 6 2 3
+
+# 2, 6, 3, 7만 데드라인에 맞춰가능 - 15개의 컵라면
 
 def solution():
 
     input = sys.stdin.readline
 
     N = int(input())            # 숙제의 개수
-    problem = [list(map(int, input().split())) for _ in range(N)]
+    
+    problem = []
+    for _ in range(N):
+        day, cup = map(int, input().split())
+        heappush(problem, (day, cup))
+
+    answer = 0
+    deadline = 1
 
     print(problem)
+
+    while problem:
+
+        (day, cup) = heappop(problem)
+        deadline = day
+        
+        # 같은 날짜의 과제 중 max(컵라면) 구하기
+        temp = []
+        
+        while problem:
+            tday, tcup = heappop(problem)
+            if tday == day:
+                temp.append(tcup)
+            else:
+                heappush(problem, (tday, tcup))
+                break
+        
+        answer += max(temp) if temp else 0
+        if temp:
+            print(temp)
+            answer += max(temp)
+
+        deadline += 1
+        
+    
+    return answer
+
 
 print(solution())
