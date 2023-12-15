2999e853d211f8cf8a5aed40682e1b564f09a6f4
yurrim <yurimkang20@gmail.com>
2023-12-15 17:13:17 +0900

    Upload. 3109 빵집

--- python/그래프 탐색/Prob3109.py
+++ python/그래프 탐색/Prob3109.py
@@ -3,8 +3,43 @@
 
 import sys
 
+answer = 0
+
+dir = [[0, 1], [-1, 1], [1, 1]]
+
 def solution():
     
     input = sys.stdin.readline
+    R, C = map(int, input().split())
+    graph = [list(input()) for _ in range(R)]
+
+    search(R, C, graph, 0, 0, 0, 0)
+
+    return answer
+
+
+def search(R, C, graph, start_row, row, col, count):
+
+    if start_row == R-1 and col == C-1:
+        print(answer, count)
+        answer = max(count, answer)
+        return
+    
+    print(start_row, row, col)
+
+    for (r, c) in dir:
+        nr = row + r
+        nc = col + c
+
+        if 0 <= nr < R and 0 <= nc < C and graph[nr][nc] == '.':
+
+            if nc == C-1:
+                graph[nr][nc] = 'O'
+                search(R, C, graph, start_row+1, start_row+1, 0, count+1)
+                graph[nr][nc] = '.'
+            else:
+                graph[nr][nc] = 'O'
+                search(R, C, graph, start_row, nr, nc, count)
+                graph[nr][nc] = '.'
 
 print(solution())
