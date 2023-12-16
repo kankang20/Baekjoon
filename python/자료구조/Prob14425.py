2bace649b4bd7f52b519d6e382b306c312819d61
yurrim <yurimkang20@gmail.com>
2023-12-17 00:25:57 +0900

    Upload. 14425 문자열 집합

--- python/자료구조/Prob14425.py
+++ python/자료구조/Prob14425.py
@@ -6,5 +6,19 @@ import sys
 def solution():
 
     input = sys.stdin.readline
+    N, M = map(int, input().split())
+
+    answer = 0
+
+    check_string = set()
+    for _ in range(N):
+        check_string.add(input().rstrip())
+
+    for _ in range(M):
+        check = input().rstrip()
+        if check in check_string:
+            answer += 1
+    
+    return answer
 
 print(solution())
