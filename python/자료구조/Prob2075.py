e011693ae808877aea62255693e34cf9d1c1955d
yurrim <yurimkang20@gmail.com>
2023-12-19 14:26:29 +0900

    Upload. 2075 N번째 큰 수

--- python/자료구조/Prob2075.py
+++ python/자료구조/Prob2075.py
@@ -2,9 +2,28 @@
 """ 실버 2. N번째 큰 수 """
 
 import sys
+from heapq import heappush, heappop
 
 def solution():
 
     input = sys.stdin.readline
+    N = int(input())                # 표의 크기
+
+    answer = []
+    for idx in range(N):
+        for num in list(map(int, input().split())):
+            
+            if idx == 0:
+                heappush(answer, num)
+                continue
+            
+            temp = heappop(answer)
+            
+            if temp < num:
+                heappush(answer, num)
+            else:
+                heappush(answer, temp)
+    
+    return heappop(answer)
 
 print(solution())
