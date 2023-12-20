98b9fa089485505505ac2066577b9b9dea7facec
yurrim <yurimkang20@gmail.com>
2023-12-20 10:08:36 +0900

    Upload. 4358 생태학

--- python/자료구조/Prob4358.py
+++ python/자료구조/Prob4358.py
@@ -6,5 +6,21 @@ import sys
 def solution():
 
     input = sys.stdin.readline
+
+    trees = {}
+    total_tree = 0
+
+    while True:
+
+        tree = input().rstrip()
+
+        if not tree:
+            break
+
+        trees[tree] = trees.get(tree, 0) + 1
+        total_tree += 1
     
+    for key in sorted(trees.keys()):
+        print("{0} {1:.4f}".format(key, (trees.get(key) / total_tree) * 100))
+
 solution()
