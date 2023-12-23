56861f9c027c213f7d98f8e61dfef7e9899829a3
yurrim <yurimkang20@gmail.com>
2023-12-23 15:32:29 +0900

    Upload. 1781 컵라면

--- python/그리디/Prob1781.py
+++ python/그리디/Prob1781.py
@@ -9,31 +9,20 @@ def solution():
     input = sys.stdin.readline
     N = int(input())            # 숙제의 개수
 
-    maximum_day = 0
     problem = []
     for _ in range(N):
-        day, cup = map(int, input().split())
-        maximum_day = max(day, maximum_day)
-        heappush(problem, (-cup, day))     
+        deadline, cup = map(int, input().split())
+        heappush(problem, (deadline, cup))
 
-    selected = [False] * (maximum_day+1)
-    answer = 0
+    answer = []
+    while problem:
 
-    for _ in range(N):
+        deadline, cup = heappop(problem)
+        heappush(answer, cup)
 
-        temp = heappop(problem)
-        (cup, day) = -temp[0], temp[1]
-        print("(cup, day) = (", cup, day, ")")
-
-        while day > 0 and selected[day]:
-            day -= 1
-        
-        if day == 0:
-            continue
-
-        selected[day] = True
-        answer += cup
-
-    return answer
+        if deadline < len(answer):
+            heappop(answer)
+    
+    return sum(answer)
 
 print(solution())
