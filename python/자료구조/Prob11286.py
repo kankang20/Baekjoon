2e7da724963e9bf1dcaad5d74bb2fa68e31c3056
yurrim <yurimkang20@gmail.com>
2023-12-24 02:28:44 +0900

    Upload. 11286 절댓값 힙

--- python/자료구조/Prob11286.py
+++ python/자료구조/Prob11286.py
@@ -2,9 +2,28 @@
 """ 실버 1. 절댓값 힙 """
 
 import sys
+from heapq import heappush, heappop
 
 def solution():
 
     input = sys.stdin.readline
+    N = int(input().rstrip())
+
+    answer = []
+    for _ in range(N):
+
+        command = int(input().rstrip())
+
+        if answer > 0:
+            heappush(answer, (command, 1))      # 0이면 마이너스, 1이면 플러스
+        elif answer < 0:
+            heappush(answer, (-command, 0))
+        else:
+
+            if answer:
+                heappop(answer)
+            else:
+                print(0)
+
 
 solution()
