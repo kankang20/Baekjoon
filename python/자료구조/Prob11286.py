dd290d1df014998434511b449b4bd0085db7e589
yurrim <yurimkang20@gmail.com>
2023-12-25 01:47:20 +0900

    Upload. 11286 절댓값 힙

--- python/자료구조/Prob11286.py
+++ python/자료구조/Prob11286.py
@@ -9,21 +9,28 @@ def solution():
     input = sys.stdin.readline
     N = int(input().rstrip())
 
-    answer = []
+    answer_minus = []
+    answer_plus = []
+
     for _ in range(N):
 
         command = int(input().rstrip())
 
-        if answer > 0:
-            heappush(answer, (command, 1))      # 0이면 마이너스, 1이면 플러스
-        elif answer < 0:
-            heappush(answer, (-command, 0))
+        if command > 0:
+            heappush(answer_plus, command)
+        elif command < 0:
+            heappush(answer_minus, -command)
         else:
-
-            if answer:
-                heappop(answer)
-            else:
+            if not answer_plus and not answer_minus:
                 print(0)
+                continue
 
+            number_plus = answer_plus[0] if answer_plus else 1e9
+            number_minus = answer_minus[0] if answer_minus else 1e9
 
+            if number_plus < number_minus:
+                print(heappop(answer_plus))
+            else:
+                print(-heappop(answer_minus))
+        
 solution()
