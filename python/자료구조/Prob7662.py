d54259698ffd953bc2d141b59e925228f4d29229
yurrim <yurimkang20@gmail.com>
2023-12-28 18:05:06 +0900

    Upload. 7662 이중 우선순위 큐

--- python/자료구조/Prob7662.py
+++ python/자료구조/Prob7662.py
@@ -8,57 +8,49 @@ def solution():
 
     input = sys.stdin.readline
 
-    T = int(input())
-
-    for _ in range(T):
+    for _ in range(int(input())):
 
         Operations = int(input())       # 연산 개수
 
-        min_heap = []       # 최소힙 (작은 수 먼저)
-        max_heap = []       # 최대힙 (큰 수 먼저)
+        min_heap = []                       # 최소힙 (작은 수 먼저)
+        max_heap = []                       # 최대힙 (큰 수 먼저)
+        selected = [False] * Operations
 
-        numbers = dict()
-
-        for _ in range(Operations):
+        for idx in range(Operations):
 
             command = input().split()
 
             if command[0] == "I":
-
                 number = int(command[1])
-
-                heappush(min_heap, number)
-                heappush(max_heap, -number)
+                heappush(min_heap, (number, idx))
+                heappush(max_heap, (-number, idx))
             
-                numbers[number] = numbers.get(number, 0) + 1
-
             elif command[0] == "D":
 
-                if sum(numbers.values()) == 0:
-                    continue
+                if command[1] == "-1":          # 최솟값 삭제
+                    # while min_heap and selected[min_heap[0][1]]:
+                    #     heappop(min_heap)
+                    if min_heap:
+                        selected[heappop(min_heap)[1]] = True
+                    
+                elif command[1] == "1":         # 최댓값 삭제
+                    # while max_heap and selected[max_heap[0][1]]:
+                    #     heappop(max_heap)
+                    if max_heap:
+                        selected[heappop(max_heap)[1]] = True
 
-                if command[1] == "1":       # 최댓값 삭제
-                    while not numbers[-max_heap[0]]:
-                        heappop(max_heap)
-                    numbers[-heappop(max_heap)] -= 1
+            while min_heap and selected[min_heap[0][1]]:
+                heappop(min_heap)
+            while max_heap and selected[max_heap[0][1]]:
+                heappop(max_heap)
 
-                elif command[1] == "-1":    # 최솟값 삭제
-                    while not numbers[min_heap[0]]:
-                        heappop(min_heap)
-                    numbers[heappop(min_heap)] -= 1
-        
-        if sum(numbers.values()) == 0:
+            # print("max_heap = ", max_heap)
+            # print("min_heap = ", min_heap)
+            # print("selected = ", selected)
+
+        if not max_heap or not min_heap:
             print("EMPTY")
         else:
-
-            while not numbers[-max_heap[0]]:
-                heappop(max_heap)
-            max_number = -heappop(max_heap)
-
-            while not numbers[min_heap[0]]:
-                heappop(min_heap)
-            min_number = heappop(min_heap)
-
-            print(max_number, min_number)
+            print(-max_heap[0][0], min_heap[0][0])
 
 solution()
