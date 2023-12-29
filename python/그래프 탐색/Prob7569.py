a099d877c417657a0387403e1125d383cec68ca3
yurrim <yurimkang20@gmail.com>
2023-12-29 23:47:33 +0900

    Upload. 7569 토마토

--- python/그래프 탐색/Prob7569.py
+++ python/그래프 탐색/Prob7569.py
@@ -9,30 +9,27 @@ def solution():
     input = sys.stdin.readline
     C, R, H = map(int, input().split())     # 상자의 크기 R, C, 상자의 수 H
 
-    box = [[] for _ in range(H)]
-    for h in range(H):
-        for _ in range(R):
-            box[h].append(list(map(int, input().split())))     # 1 익은 토마토, 0 익지 않은 토마토, -1 비어있음
-
-    return move_tomato(R, C, H, box)
-    
-
-def move_tomato(R, C, H, box):
-
-    dir = [[1, 0, 0], [-1, 0, 0], [0, -1, 0], [0, 1, 0], [0, 0, -1], [0, 0, 1]]
-
     not_tomato = 0
-    day = -1
-
     tomato = deque()
 
+    box = [[] for _ in range(H)]
     for h in range(H):
         for r in range(R):
+            box[h].append(list(map(int, input().split())))     # 1 익은 토마토, 0 익지 않은 토마토, -1 비어있음
             for c in range(C):
                 if box[h][r][c] == 0:
                     not_tomato += 1
                 elif box[h][r][c] == 1:
                     tomato.append((h, r, c))
+
+    return move_tomato(R, C, H, box, tomato, not_tomato)
+    
+
+def move_tomato(R, C, H, box:list, tomato:deque, not_tomato:int):
+
+    dir = [[1, 0, 0], [-1, 0, 0], [0, -1, 0], [0, 1, 0], [0, 0, -1], [0, 0, 1]]
+
+    day = -1
 
     while tomato:
 
@@ -48,10 +45,7 @@ def move_tomato(R, C, H, box):
                 nr = r + dir[d][1]
                 nc = c + dir[d][2]
 
-                if nh < 0 or nh >= H or nr < 0 or nr >= R or nc < 0 or nc >= C:
-                    continue
-
-                if box[nh][nr][nc] == -1 or box[nh][nr][nc] == 1:
+                if nh < 0 or nh >= H or nr < 0 or nr >= R or nc < 0 or nc >= C or box[nh][nr][nc] == -1 or box[nh][nr][nc] == 1:
                     continue
 
                 box[nh][nr][nc] = 1
