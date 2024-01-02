b9a4eda7cd2de32206453de6675524e807d1c1e6
yurrim <yurimkang20@gmail.com>
2024-01-02 19:19:58 +0900

    Upload. 27172 수 나누기 게임

--- python/수학/Prob27172.py
+++ python/수학/Prob27172.py
@@ -10,20 +10,20 @@ def solution():
     N = int(input())                                # 플레이어의 수
     cards = list(map(int, input().split()))         # 각 플레이어가 가지고 있는 카드에 적힌 정수
 
-    answer = [0] * (N)
+    max_card = max(cards)
+    answer = [0] * (max_card + 1)
+    check = [False] * (max_card + 1)
 
-    for i in range(N):
-        for j in range(i, N):
+    for card in cards:
+        check[card] = True
 
-            if cards[i] % cards[j] == 0:
-                answer[i] -= 1
-                answer[j] += 1
-            elif cards[j] % cards[i] == 0:
-                answer[i] += 1
-                answer[j] -= 1
-            else:
-                continue
+    for card in cards:
+        for num in range(2*card, max_card+1, card):
+            if check[num]:
+                answer[num] -= 1
+                answer[card] += 1
     
-    print(answer)
+    for card in cards:
+        print(answer[card], end=" ")
 
 solution()
