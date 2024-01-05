7062ce0df94eb4ff441c5f61a6aad3522fbef30b
yurrim <yurimkang20@gmail.com>
2024-01-06 02:17:22 +0900

    Upload. 1543 문서 검색

--- python/그리디/Prob1543.py
+++ python/그리디/Prob1543.py
@@ -7,4 +7,9 @@ def solution():
 
     input = sys.stdin.readline
 
+    text = input().rstrip()         # 문서
+    word = input().rstrip()         # 검색하고 싶은 단어
+
+    return text.count(word)
+
 print(solution())
