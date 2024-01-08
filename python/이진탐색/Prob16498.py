b98b897ce0b2d422511c12e99feb5a9acf2b7067
yurrim <yurimkang20@gmail.com>
2024-01-08 10:58:37 +0900

    Upload.  16498 작은 벌점

--- python/이진탐색/Prob16498.py
+++ python/이진탐색/Prob16498.py
@@ -7,4 +7,19 @@ def solution():
 
     input = sys.stdin.readline
 
+    A, B, C = map(int, input().split())
+    
+    cards = []
+    for _ in range(3):
+        cards.append(list(map(int, input().split())))
+
+    answer = 1e9
+
+    for num1 in cards[0]:
+        for num2 in cards[1]:
+            for num3 in cards[2]:
+                answer = min(max(num1, num2, num3) - min(num1, num2, num3), answer)
+
+    print(answer)
+
 solution()
