07a7157b11f7da85b6b194da5d212adfb22f1c9f
yurrim <yurimkang20@gmail.com>
2024-01-09 15:12:56 +0900

    Upload. 1920 수 찾기

--- python/자료구조/Prob1920.py
+++ python/자료구조/Prob1920.py
@@ -7,11 +7,11 @@ def solution():
 
     input = sys.stdin.readline
 
-    _ = int(input())                                    # 정수 개수
-    numbers = sorted(list(map(int, input().split())))   # 수
+    _ = int(input())                           # 정수 개수
+    numbers = set(map(int, input().split()))   # 수
 
     _ = int(input())
     for num in list(map(int, input().split())):
-        print(1)
+        print(1 if num in numbers else 0)
 
 solution()
