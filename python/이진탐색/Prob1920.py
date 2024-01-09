48c0c2fe54f64e3586cf6e8ab4bb476c46442682
yurrim <yurimkang20@gmail.com>
2024-01-09 15:09:00 +0900

    Upload. 1920 수 찾기

--- python/이진탐색/Prob1920.py
+++ python/이진탐색/Prob1920.py
@@ -7,11 +7,29 @@ def solution():
 
     input = sys.stdin.readline
 
-    N = int(input())                            # 정수 개수
-    numbers = "".join(list(input().split()))   # 수
+    _ = int(input())                                    # 정수 개수
+    numbers = sorted(list(map(int, input().split())))   # 수
 
-    M = int(input())
-    for num in list(input().split()):
-        print(0 if numbers.find(num) < 0 else 1)
+    _ = int(input())
+    for num in list(map(int, input().split())):
+        print(1 if search(numbers, num) else 0)
+
+def search(numbers, target):
+
+    start, end = 0, len(numbers)-1
+
+    while start <= end:
+
+        mid = (start + end) // 2
+        temp = numbers[mid]
+
+        if temp == target:
+            return True
+        elif temp < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return False
 
 solution()
