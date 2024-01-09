495a2256ba8ae81cb547000b416947f9cdd6aa0a
yurrim <yurimkang20@gmail.com>
2024-01-09 14:53:19 +0900

    Upload. 1920 수 찾기

--- python/이진탐색/Prob1920.py
+++ python/이진탐색/Prob1920.py
@@ -7,4 +7,11 @@ def solution():
 
     input = sys.stdin.readline
 
+    N = int(input())                            # 정수 개수
+    numbers = "".join(list(input().split()))   # 수
+
+    M = int(input())
+    for num in list(input().split()):
+        print(0 if numbers.find(num) < 0 else 1)
+
 solution()
