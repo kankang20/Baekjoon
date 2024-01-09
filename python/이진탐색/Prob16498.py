d6235dcb159f3e3f8ab67e4677214f6c07ae41a0
yurrim <yurimkang20@gmail.com>
2024-01-09 14:43:48 +0900

    Upload.  16498 작은 벌점

--- python/이진탐색/Prob16498.py
+++ python/이진탐색/Prob16498.py
@@ -11,15 +11,39 @@ def solution():
     
     cards = []
     for _ in range(3):
-        cards.append(list(map(int, input().split())))
+        cards.append(sorted(list(map(int, input().split()))))
 
     answer = 1e9
 
-    for num1 in cards[0]:
-        for num2 in cards[1]:
-            for num3 in cards[2]:
-                answer = min(max(num1, num2, num3) - min(num1, num2, num3), answer)
+    for a in cards[0]:
 
-    print(answer)
+        b = search(cards[1], a)
+        c1 = search(cards[2], a)
+        c2 = search(cards[2], b)
 
-solution()
+        temp1 = max(a, b, c1) - min(a, b, c1)
+        temp2 = max(a, b, c2) - min(a, b, c2)
+        answer = min(answer, temp1, temp2)
+
+    return answer
+
+def search(card, target):
+
+    answer = 0
+    start, end = 0, len(card)-1
+
+    while start <= end:
+
+        mid = (start + end) // 2
+
+        if abs(card[mid] - target) < abs(answer - target):
+            answer = card[mid]
+
+        if card[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return answer
+
+print(solution())
