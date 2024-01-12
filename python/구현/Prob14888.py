b825dce9a2482b89e21112b8343e249dba1661a4
yurrim <yurimkang20@gmail.com>
2024-01-12 20:38:53 +0900

    Upload. 14888 연산자 끼워넣기

--- python/구현/Prob14888.py
+++ python/구현/Prob14888.py
@@ -4,41 +4,45 @@
 import sys
 from itertools import permutations
 
+max_answer = -1e9
+min_answer = 1e9
+
+numbers = []
+operator = []
+
 def solution():
 
     input = sys.stdin.readline
     
-    N = int(input())                            # 수의 개수
-    numbers = list(map(int, input().split()))   # 수
+    N = int(input())                                # 수의 개수
 
-    operator = []
-    for idx, num in enumerate(list(map(int, input().split()))):
-        for _ in range(num):
-            operator.append(idx)
-
-    max_answer = -1e9
-    min_answer = 1e9
-
-    for per in permutations(operator):
-        answer = numbers[0]
-        for idx in range(1, N):
-            answer = calculate(per[idx-1], answer, numbers[idx])
-
-        max_answer = max(answer, max_answer)
-        min_answer = min(answer, min_answer)
+    global numbers
+    numbers = list(map(int, input().split()))       # 수
     
+    global operator
+    operator = list(map(int, input().split()))      # 지옥의 연산자 (연산자는 왜 연산자일까)
+
+    calculate(N, 1, numbers[0], 0, 0, 0, 0)
+
     print(int(max_answer), int(min_answer), sep="\n")
 
-def calculate(operator:int, num1:int, num2:int):
-    if operator == 0:
-        return num1 + num2
-    elif operator == 1:
-        return num1 - num2
-    elif operator == 2:
-        return num1 * num2
-    else:
-        if num1 < 0 and num2 > 0:
-            return - (-num1 // num2)
-        return num1 // num2
+def calculate(N, idx, answer, plus, minus, multi, divis):
+
+    if idx == N:
+        global max_answer
+        max_answer = max(max_answer, answer)
+        global min_answer
+        min_answer = min(min_answer, answer)
+        return
+
+    if plus < operator[0]:
+        calculate(N, idx+1, answer + numbers[idx], plus+1, minus, multi, divis)
+    if minus < operator[1]:
+        calculate(N, idx+1, answer - numbers[idx], plus, minus+1, multi, divis)
+    if multi < operator[2]:
+        calculate(N, idx+1, answer * numbers[idx], plus, minus, multi+1, divis)
+    if divis < operator[3]:
+        answer = -(-answer // numbers[idx]) if answer < 0 else (answer // numbers[idx])
+        calculate(N, idx+1, answer, plus, minus, multi, divis+1)
 
 solution()
