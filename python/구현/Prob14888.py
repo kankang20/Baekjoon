0fa1fe4e3872ff64578d1d2b6c802019851f2db6
yurrim <yurimkang20@gmail.com>
2024-01-12 20:02:42 +0900

    Upload. 14888 연산자 끼워넣기

--- python/구현/Prob14888.py
+++ python/구현/Prob14888.py
@@ -2,9 +2,43 @@
 """ 실버 1. 연산자 끼워넣기 """
 
 import sys
+from itertools import permutations
 
 def solution():
 
     input = sys.stdin.readline
+    
+    N = int(input())                            # 수의 개수
+    numbers = list(map(int, input().split()))   # 수
+
+    operator = []
+    for idx, num in enumerate(list(map(int, input().split()))):
+        for _ in range(num):
+            operator.append(idx)
+
+    max_answer = -1e9
+    min_answer = 1e9
+
+    for per in permutations(operator):
+        answer = numbers[0]
+        for idx in range(1, N):
+            answer = calculate(per[idx-1], answer, numbers[idx])
+
+        max_answer = max(answer, max_answer)
+        min_answer = min(answer, min_answer)
+    
+    print(int(max_answer), int(min_answer), sep="\n")
+
+def calculate(operator:int, num1:int, num2:int):
+    if operator == 0:
+        return num1 + num2
+    elif operator == 1:
+        return num1 - num2
+    elif operator == 2:
+        return num1 * num2
+    else:
+        if num1 < 0 and num2 > 0:
+            return - (-num1 // num2)
+        return num1 // num2
 
 solution()
