760d614263b5b00efed5481df3961f131108ab94
yurrim <yurimkang20@gmail.com>
2024-01-14 18:08:57 +0900

    Upload. 14889 스타트와 링크

--- python/구현/Prob14889.py
+++ python/구현/Prob14889.py
@@ -2,9 +2,31 @@
 """ 실버 1. 스타트와 링크 """
 
 import sys
+from itertools import combinations
 
 def solution():
 
     input = sys.stdin.readline
 
-solution()
+    N = int(input())
+    ab = []
+    for _ in range(N):
+        ab.append(list(map(int, input().split())))
+
+    answer = 1e9
+    for t in list(combinations([i for i in range(N)], N//2)):
+
+        diff = 0
+        for i in range(N):
+            for j in range(i+1, N):
+                if i in t and j in t:
+                    diff += ab[i][j]
+                    diff += ab[j][i]
+                elif i not in t and j not in t:
+                    diff -= ab[i][j]
+                    diff -= ab[j][i]
+        answer = min(answer, abs(diff))
+
+    return answer
+
+print(solution())
