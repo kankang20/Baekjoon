08f154b7a6d72721210152f534ae3bd4f3c28594
yurrim <yurimkang20@gmail.com>
2024-01-15 11:19:24 +0900

    Upload. 14889 스타트와 링크

--- python/구현/Prob14889.py
+++ python/구현/Prob14889.py
@@ -2,10 +2,10 @@
 """ 실버 1. 스타트와 링크 """
 
 import sys
-from itertools import combinations
+
+answer = 1e9
 
 def solution():
-
     input = sys.stdin.readline
 
     N = int(input())
@@ -13,20 +13,30 @@ def solution():
     for _ in range(N):
         ab.append(list(map(int, input().split())))
 
-    answer = 1e9
-    for t in list(combinations([i for i in range(N)], N//2)):
+    selected = [False for _ in range(N)]
+    check(N, 0, ab, selected, 0)
 
+    return answer
+
+def check(N, count, ab, selected, idx):
+
+    if count == N//2:
         diff = 0
         for i in range(N):
             for j in range(i+1, N):
-                if i in t and j in t:
+                if selected[i] and selected[j]:
                     diff += ab[i][j]
                     diff += ab[j][i]
-                elif i not in t and j not in t:
+                elif not selected[i] and not selected[j]:
                     diff -= ab[i][j]
                     diff -= ab[j][i]
+        global answer
         answer = min(answer, abs(diff))
-
-    return answer
+    
+    for i in range(idx, N):
+        if not selected[i]:
+            selected[i] = True
+            check(N, count+1, ab, selected, i+1)
+            selected[i] = False
 
 print(solution())
