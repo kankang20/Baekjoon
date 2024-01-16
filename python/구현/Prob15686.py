4a9a7bea1876021a3b82b7ff8a8db0115e9afd67
yurrim <yurimkang20@gmail.com>
2024-01-16 16:15:24 +0900

    Upload. 15686 치킨 배달

--- python/구현/Prob15686.py
+++ python/구현/Prob15686.py
@@ -3,8 +3,67 @@
 
 import sys
 
+answer = 1e9
+
 def solution():
 
     input = sys.stdin.readline
+
+    N, M = map(int, input().split())
+
+    global chicken
+    chicken = []
+
+    global house
+    house = []
+
+    for i in range(N):              
+        temp = list(map(int, input().split()))      # 0 빈칸, 1 집, 2 치킨집
+        for j in range(N):
+            if temp[j] == 1:
+                house.append((i, j))
+            if temp[j] == 2:
+                chicken.append((i, j))
+
+    global chicken_count
+    chicken_count = len(chicken)
+
+    # 치킨집의 개수가 작거나 같아서 M개를 고르지 않아도 되는 경우
+    if chicken_count <= M:
+        return calculate_chicken_distance([True for _ in range(chicken_count)])
+
+    # 치킨집의 개수가 M보다 큰 경우
+    global selected    
+    selected = [False for _ in range(chicken_count)]
+
+    select_chicken(M, 0)
+
+    return answer
+
+def select_chicken(M, count):
+
+    if count == M:
+        global answer
+        answer = min(answer, calculate_chicken_distance(selected))
+        return
+    
+    for i in range(chicken_count):
+        if not selected[i]: 
+            selected[i] = True
+            select_chicken(M, count+1)
+            selected[i] = False
+
+def calculate_chicken_distance(selected:list):
+
+    temp_answer = 0
+    for (hr, hc) in house:
+        distance = 1e9
+        for c in range(chicken_count):
+            if selected[c]:
+                cr, cc = chicken[c]
+                distance = min(distance, abs(hr-cr) + abs(hc-cc))
+        temp_answer += distance
+    
+    return temp_answer
 
 print(solution())
