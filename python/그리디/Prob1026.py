bff1bac989e9b8270a17733bc228e4108607d090
yurrim <yurimkang20@gmail.com>
2024-01-17 12:14:39 +0900

    Upload. 1026 보물

--- python/그리디/Prob1026.py
+++ python/그리디/Prob1026.py
@@ -7,4 +7,11 @@ def solution():
 
     input = sys.stdin.readline
 
+    N = int(input())
+
+    A = sorted(list(map(int, input().split())), reverse=True)
+    B = sorted(list(map(int, input().split())))
+
+    return sum([A[idx]*B[idx] for idx in range(N)])
+
 print(solution())
