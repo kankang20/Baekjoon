89838a45f1eb1dee4651c028e0921735c69c91e1
yurrim <yurimkang20@gmail.com>
2024-01-18 12:51:42 +0900

    Upload. 1890 점프

--- python/다이나믹 프로그래밍/Prob1890.py
+++ python/다이나믹 프로그래밍/Prob1890.py
@@ -6,31 +6,24 @@ import sys
 def solution():
     input = sys.stdin.readline
 
-    global N
     N = int(input())
-    
-    global array
     array = [list(map(int, input().split())) for _ in range(N)]
 
-    global answer
-    answer = [[0]*N for _ in range(N)]
+    selected = [[0] * N for _ in range(N)]
 
-    search(0, 0)
-    print(answer)
+    selected[0][0] = 1
+    for r in range(N):
+        for c in range(N):
 
-    return answer[N-1][N-1]
+            if r == N-1 and c == N-1:
+                break
+            
+            if c + array[r][c] < N:
+                selected[r][c + array[r][c]] += selected[r][c]
 
-def search(r, c):
+            if r + array[r][c] < N:
+                selected[r + array[r][c]][c] += selected[r][c]
 
-    if r >= N or c >= N:
-        return
-    
-    answer[r][c] += 1
-
-    if r == N-1 and c == N-1:
-        return
-
-    search(r, c+array[r][c])        # 오른쪽으로 이동
-    search(r+array[r][c], c)        # 아래쪽으로 이동
+    return selected[N-1][N-1]
 
 print(solution())
