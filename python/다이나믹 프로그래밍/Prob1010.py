0c83f2865812f038c73fb64314b3e8fec07b7bad
yurrim <yurimkang20@gmail.com>
2024-01-18 11:04:33 +0900

    Upload. 1010 다리 놓기

---
+++ python/다이나믹 프로그래밍/Prob1010.py
@@ -0,0 +1,24 @@
+
+""" 실버 5. 다리 놓기  """
+
+T = int(input())        # 테스트케이스
+
+def solution(N, M):
+
+    table = [[0] * 30 for _ in range(30)]
+
+    if N == 1:
+        return M
+
+    for i in range(1, N+1):
+        for j in range(1, M+1):
+            if i == 1:
+                table[i][j] = j
+            else:
+                table[i][j] = table[i-1][j-1] + table[i][j-1]
+
+    return table[N][M]
+
+for _ in range(T):
+    N, M = map(int, input().split())
+    print(solution(N, M))
