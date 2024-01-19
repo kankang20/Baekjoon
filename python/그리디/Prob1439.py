324562fd80b4db7e786157acc99aa09c78d3063a
yurrim <yurimkang20@gmail.com>
2024-01-19 17:50:17 +0900

    Upload. 1439 뒤집기

--- python/그리디/Prob1439.py
+++ python/그리디/Prob1439.py
@@ -6,6 +6,19 @@ import sys
 def solution():
     input = sys.stdin.readline
 
-    
+    S = list(map(int, input().rstrip()))
+
+    pattern, idx = [], 0
+    while idx < len(S):
+        pattern.append(S[idx])
+        while idx < len(S)-1 and S[idx] == S[idx+1]:
+            idx += 1
+        idx += 1
+
+    count = [0] * 2
+    for p in pattern:
+        count[p] += 1
+
+    return min(count)
 
 print(solution())
