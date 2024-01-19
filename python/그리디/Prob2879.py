b9483247f83ffa72db357995a3a4808e16e4d465
yurrim <yurimkang20@gmail.com>
2024-01-19 17:08:57 +0900

    Upload. 2879 코딩은 예쁘게

--- python/그리디/Prob2879.py
+++ python/그리디/Prob2879.py
@@ -6,4 +6,28 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    N = int(input())
+    current = list(map(int, input().split()))
+    target = list(map(int, input().split()))
+
+    diff = [(target[i] - current[i]) for i in range(N)]
+
+    idx, answer = 0, 0
+    while idx < N:
+
+        day = abs(diff[idx])
+
+        if diff[idx] < 0:
+            while idx < N and diff[idx] < 0:
+                day = max(day, abs(diff[idx]))
+                idx += 1
+        elif diff[idx] > 0:
+            while idx < N and diff[idx] > 0:
+                day = max(day, abs(diff[idx]))
+                idx += 1
+            
+        answer += day
+
+    return answer
+
 print(solution())
