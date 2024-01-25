7423147f9f5b1a018ec4931cd90af693b9f96815
yurrim <yurimkang20@gmail.com>
2024-01-25 12:40:40 +0900

    Upload. 주행거리 비교하기

--- 소프티어/Level 1/주행거리 비교하기.py
+++ 소프티어/Level 1/주행거리 비교하기.py
@@ -1,2 +1,18 @@
 
 """ Lv 1. 주행거리 비교하기 """
+
+import sys
+
+def solution():
+    input = sys.stdin.readline
+
+    A, B = map(int, input().split())
+
+    if A < B:
+        return "B"
+    elif A > B:
+        return "A"
+    else:
+        return "same"
+
+print(solution())
