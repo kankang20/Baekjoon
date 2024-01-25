56fdaf7734f810e115253575d0ab902a98308d8a
yurrim <yurimkang20@gmail.com>
2024-01-25 12:05:02 +0900

    Upload. A+B

--- 소프티어/Level 1/A+B.py
+++ 소프티어/Level 1/A+B.py
@@ -1,2 +1,13 @@
 
-"""  """
+""" Lv 1. A + B """
+
+import sys
+
+def solution():
+    input = sys.stdin.readline
+    
+    for t in range(int(input())):
+        A, B = map(int, input().split())
+        print(f'Case #{t+1}: {A+B}')
+    
+solution()
