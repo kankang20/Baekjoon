721bafee64557c9388ef8a3564d0f5906af49302
yurrim <yurimkang20@gmail.com>
2024-01-28 18:21:37 +0900

    Upload. 지도 자동 구축

--- 소프티어/Level 2/지도 자동 구축.py
+++ 소프티어/Level 2/지도 자동 구축.py
@@ -6,3 +6,12 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    N = int(input())        # iteration 횟수
+
+    answer = 3
+    for _ in range(1, N):
+        answer += (answer-1)
+
+    return answer*answer
+
+print(solution())
