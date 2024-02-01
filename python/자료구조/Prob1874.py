9192311facb876c45068704367ceecddd989d284
yurrim <yurimkang20@gmail.com>
2024-02-01 18:06:54 +0900

    Upload. 1874 스택 수열

--- python/자료구조/Prob1874.py
+++ python/자료구조/Prob1874.py
@@ -6,4 +6,28 @@ import sys
 def solution():
     input = sys.stdin.readline
 
-solution()
+    N = int(input())    # 숫자 개수
+
+    answer, numbers = [], []
+    flag, count = False, 1
+    for _ in range(N):
+
+        num = int(input())
+
+        if flag:
+            continue
+
+        if count <= num:
+            for n in range(count, num+1, 1):
+                answer.append("+")
+                numbers.append(n)
+            count = num + 1
+        elif numbers[-1] != num:
+            flag = True
+
+        answer.append("-")
+        numbers.pop()
+
+    return "NO" if flag else "\n".join(answer)
+
+print(solution())
