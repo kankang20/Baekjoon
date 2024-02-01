b303f48517243109ace87793eac652456a54e878
yurrim <yurimkang20@gmail.com>
2024-02-01 18:06:42 +0900

    Upload. 10773 제로

--- python/자료구조/Prob10773.py
+++ python/자료구조/Prob10773.py
@@ -6,4 +6,17 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    K = int(input())        # 정수 개수
+
+    money = []
+    for _ in range(K):
+        number = int(input())
+
+        if number:
+            money.append(number)
+        else:
+            money.pop()
+
+    return sum(money)
+
 print(solution())
