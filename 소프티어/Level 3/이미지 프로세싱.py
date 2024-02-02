2840ca86b3eedc0d3c7ada8f8181c0d081c0ed80
yurrim <yurimkang20@gmail.com>
2024-02-02 12:01:50 +0900

    Upload. 이미지 프로세싱

--- 소프티어/Level 3/이미지 프로세싱.py
+++ 소프티어/Level 3/이미지 프로세싱.py
@@ -2,8 +2,46 @@
 """ Lv 3. [21년 재직자 대회 예선] 이미지 프로세싱 """
 
 import sys
+from collections import deque
 
 def solution():
     input = sys.stdin.readline
 
-print(solution())
+    global R, C
+    R, C = map(int, input().split())    # 행 R, 열 C
+
+    global board
+    board = [list(map(int, input().split())) for _ in range(R)]
+
+    for _ in range(int(input())):
+        i, j, c = map(int, input().split())
+        bfs(i-1, j-1, c)
+
+    for r in range(R):
+        print(" ".join(map(str, board[r])))
+
+def bfs(i, j, value):
+
+    visited = [[False] * C for _ in range(R)]
+    check_value = board[i][j]
+
+    q = deque()
+
+    q.append((i, j))
+    board[i][j] = value 
+    visited[i][j] = True
+
+    while q:
+
+        r, c = q.popleft()
+
+        for (dr, dc) in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+            nr = r + dr
+            nc = c + dc
+
+            if 0 <= nr < R and 0 <= nc < C and board[nr][nc] == check_value and not visited[nr][nc]:
+                q.append((nr, nc))
+                board[nr][nc] = value
+                visited[nr][nc] = True
+
+solution()
