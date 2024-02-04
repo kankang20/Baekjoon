c6d8d47555bc0dcd2054879526f647d1c4cb2e38
yurrim <yurimkang20@gmail.com>
2024-02-04 18:07:05 +0900

    Upload. 주행거리 비교하기

--- 소프티어/Level 1/주행거리 비교하기.py
+++ 소프티어/Level 1/주행거리 비교하기.py
@@ -8,6 +8,10 @@ def solution():
 
     A, B = map(int, input().split())
 
+    return check_distance(A, B)
+    
+def check_distance(A:int, B:int):
+
     if A < B:
         return "B"
     elif A > B:
