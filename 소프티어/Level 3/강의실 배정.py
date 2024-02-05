3c64c3a81fa55eccb4cdc74d20a2e97da9696343
yurrim <yurimkang20@gmail.com>
2024-02-05 13:16:41 +0900

    Upload. 강의실 배정

--- 소프티어/Level 3/강의실 배정.py
+++ 소프티어/Level 3/강의실 배정.py
@@ -2,11 +2,28 @@
 """ Lv 3. 강의실 배정 """
 
 import sys
+from heapq import heappush, heappop
 
 def solution():
     input = sys.stdin.readline
 
     N = int(input())    # 강의 개수
-    lecture = [list(map(int, input().split())) for _ in range(N)]   # 강의 (시작 시간, 종료 시간)
+    lecture = []        # 강의 (종료 시간, 시작 시간)
+
+    for _ in range(N):
+        start, end = map(int, input().split())
+        heappush(lecture, (end, start))
+
+    answer = 1
+    previous_time, _ = heappop(lecture)
+    while lecture:
+        
+        end_time, start_time = heappop(lecture)
+
+        if previous_time <= start_time:
+            answer += 1
+            previous_time = end_time
+
+    return answer
 
 print(solution())
