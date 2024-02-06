d71a9c090f29ceb59db6880449c637bff418201d
yurrim <yurimkang20@gmail.com>
2024-02-06 11:16:13 +0900

    Upload. 비밀 메뉴2

--- 소프티어/Level 3/비밀 메뉴2.py
+++ 소프티어/Level 3/비밀 메뉴2.py
@@ -2,6 +2,7 @@
 """ Lv 3. [21년 재직자 대회 본선] 비밀 메뉴2 """
 
 import sys
+from itertools import permutations
 
 def solution():
     input = sys.stdin.readline
@@ -11,28 +12,19 @@ def solution():
     target1 = list(map(int, input().split()))
     target2 = list(map(int, input().split()))
 
+    if N > M:
+        N, M = M, N
+        target1, target2 = target2, target1
+
     answer = 0
-    for idx in range(N):
 
-        jdx = 0
+    subset = set()
+    
+    for num in range(1, N):
+        for temp in list(permutations(target1, num)):
+            subset.add(temp)
 
-        while jdx < M:
-
-            while jdx < M and target1[idx] != target2[jdx]:
-                jdx += 1
-
-            if jdx >= M:
-                break
-
-            count = 0
-            c1, c2 = idx, jdx
-            while c1 < N and c2 < M and target1[c1] == target2[c2]:
-                count += 1
-                c1 += 1
-                c2 += 1
-            
-            answer = max(answer, count)
-            jdx += 1
+    print(subset)    
 
     return answer    
 
