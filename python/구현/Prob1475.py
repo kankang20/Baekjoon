51c0946d4826ab8f5bd3dd9e49fadb96de74aa20
yurrim <yurimkang20@gmail.com>
2024-02-12 20:22:48 +0900

    Upload. 1475 방 번호

--- python/구현/Prob1475.py
+++ python/구현/Prob1475.py
@@ -2,8 +2,20 @@
 """ 실버 5. 방 번호 """
 
 import sys
+import math
 
 def solution():
     input = sys.stdin.readline
+
+    N = input().rstrip()    # 방 번호
+    numbers = [0] * 9       # 0 1 2 3 4 5 6 7 8
+
+    for n in N:
+        num = int(n)
+        numbers[6 if num == 9 else num] += 1
+
+    numbers[6] = math.ceil(numbers[6] / 2)
+
+    return max(numbers)
 
 print(solution())
