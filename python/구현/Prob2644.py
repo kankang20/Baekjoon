0e1790cfb2911428389c98580bc9d81872ed9e89
yurrim <yurimkang20@gmail.com>
2024-02-15 12:42:23 +0900

    Upload. 2644 촌수계산

--- python/구현/Prob2644.py
+++ python/구현/Prob2644.py
@@ -2,8 +2,45 @@
 """ 실버 2. 촌수계산 """
 
 import sys
+from collections import deque
 
 def solution():
     input = sys.stdin.readline
+
+    N = int(input())    # 전체 사람 수
+    start, end = map(int, input().split())      # 촌수를 계산해야 하는 두 사람
+
+    graph = [0] + [[] for _ in range(N+1)]
+
+    M = int(input())    # 관계 개수
+    for _ in range(M):
+        parent, child = map(int, input().split())   # 부모, 자식
+        graph[parent].append(child)
+        graph[child].append(parent)
+    
+    return bfs(N, graph, start, end)
+
+def bfs(N:int, graph:list, start:int, end:int):
+
+    q = deque()
+    q.append(start)
+
+    selected = [False] * (N+1)
+    selected[start] = True
+    
+    answer = 0
+    while q:
+
+        answer += 1
+        for _ in range(len(q)):
+            for c in graph[q.popleft()]:
+
+                if c == end:
+                    return answer
+                
+                if not selected[c]:
+                    q.append(c)
+                    selected[c] = True
+    return -1
 
 print(solution())
