787559c8bd14041e41732e9c829be18e286659a7
yurrim <yurimkang20@gmail.com>
2024-02-16 19:19:44 +0900

    Upload. 2847 게임을 만든 동준이

--- python/그리디/Prob2847.py
+++ python/그리디/Prob2847.py
@@ -6,4 +6,16 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    N = int(input())    # 레벨의 수
+    level = list(int(input()) for _ in range(N))
+
+    answer = 0
+    for idx in range(N-2, -1, -1):
+
+        if level[idx+1] <= level[idx]:
+            answer += (level[idx] - level[idx+1] + 1)
+            level[idx] = level[idx+1] - 1 
+
+    return answer
+
 print(solution())
