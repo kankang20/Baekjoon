29f7f80bbc00f8df88f88efb2cd888f073d96c98
yurrim <yurimkang20@gmail.com>
2024-02-17 00:38:01 +0900

    Upload. 1931 회의실 배정

--- python/그리디/Prob1931.py
+++ python/그리디/Prob1931.py
@@ -2,8 +2,28 @@
 """ 실버 1. 회의실 배정 """
 
 import sys
+from heapq import heappush, heappop
 
 def solution():
     input = sys.stdin.readline
+
+    N = int(input())    # 회의 수
+    
+    meeting = []
+    for _ in range(N):
+        start, end = map(int, input().split())
+        heappush(meeting, (end, start))
+
+    answer = 0
+    previous_time = 0
+    while meeting:
+
+        tend, tstart = heappop(meeting)
+
+        if previous_time <= tstart:
+            answer += 1
+            previous_time = tend
+
+    return answer
 
 print(solution())
