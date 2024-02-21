8717947e1c69691411e0ba3bfcc09a0e3560f5b7
yurrim <yurimkang20@gmail.com>
2024-02-21 12:08:01 +0900

    Upload. 11000 강의실 배정

--- python/그리디/Prob11000.py
+++ python/그리디/Prob11000.py
@@ -2,8 +2,26 @@
 """ 골드 5. 강의실 배정 """
 
 import sys
+from heapq import heappush, heappop
 
 def solution():
     input = sys.stdin.readline
 
+    N = int(input())
+
+    meeting = []
+    for _ in range(N):
+        s, t = map(int, input().split())
+        heappush(meeting, (t, s))
+
+    answer, previous_t = 0, 0
+    while meeting:
+
+        t, s = heappop(meeting)
+        if previous_t <= s:
+            answer += 1
+            previous_t = t
+    
+    return answer
+    
 print(solution())
