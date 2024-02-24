bd2bb310f19dbfad56cbd64ddeca1a78f1a77d7c
yurrim <yurimkang20@gmail.com>
2024-02-24 23:15:21 +0900

    Upload. 9372 상근이의 여행

--- python/최소 스패닝 트리/Prob9372.py
+++ python/최소 스패닝 트리/Prob9372.py
@@ -6,4 +6,13 @@ import sys
 def solution():
     input = sys.stdin.readline
 
-print(solution())
+    T = int(input())
+    for _ in range(T):
+        
+        N, M = map(int, input().split())    # 국가 수 N, 비행기 종류 M
+        for _ in range(M):
+            input()
+
+        print(N-1)
+
+solution()
