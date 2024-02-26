c8b09b48866c77febf2c6900202ed2574a7d2edd
yurrim <yurimkang20@gmail.com>
2024-02-26 12:16:03 +0900

    Upload. 2170 선 긋기

--- python/그리디/Prob2170.py
+++ python/그리디/Prob2170.py
@@ -2,6 +2,7 @@
 """ 골드 5. 선 긋기 """
 
 import sys
+from heapq import heappush, heappop
 
 def solution():
     input = sys.stdin.readline
@@ -11,14 +12,13 @@ def solution():
     lines = []
     for _ in range(N):
         s, t = map(int, input().split())
-        lines.append((s, t))
-
-    lines.sort()
+        heappush(lines, (s, t))
 
     answer = 0
     start, target = 0, 0
-    for (s, t) in lines:
-        
+    while lines:
+        s, t = heappop(lines)
+
         if target < s:
             answer += (target - start)
             start, target = s, t
