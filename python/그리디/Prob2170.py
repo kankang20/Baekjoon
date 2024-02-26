697341c5904bf50916db787f90f5776051a03f62
yurrim <yurimkang20@gmail.com>
2024-02-26 12:12:58 +0900

    Upload. 2170 선 긋기

--- python/그리디/Prob2170.py
+++ python/그리디/Prob2170.py
@@ -6,4 +6,29 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    N = int(input())
+
+    lines = []
+    for _ in range(N):
+        s, t = map(int, input().split())
+        lines.append((s, t))
+
+    lines.sort()
+
+    answer = 0
+    start, target = 0, 0
+    for (s, t) in lines:
+        
+        if target < s:
+            answer += (target - start)
+            start, target = s, t
+            continue
+
+        if s < start:
+            start = s
+        if target < t:
+            target = t
+
+    return answer + (target - start)
+
 print(solution())
