38cd078e20a3e74598a188cf59a70e75b151a6d0
yurrim <yurimkang20@gmail.com>
2024-02-28 12:17:03 +0900

    Upload. 17070 파이프 옮기기 1

--- python/구현/Prob17070.py
+++ python/구현/Prob17070.py
@@ -2,8 +2,50 @@
 """ 골드 5. 파이프 옮기기 1 """
 
 import sys
+from collections import deque
 
 def solution():
     input = sys.stdin.readline
+
+    N = int(input())
+    board = [list(map(int, input().split())) for _ in range(N)]
+
+    return bfs(N, board)
+
+def bfs(N:int, board:list):
+
+    # 0 가로 방향 1 세로 방향 2 대각선 방향
+    q = deque()
+    q.append((0, 1, 0))
+
+    answer = 0
+    while q:
+
+        r, c, dir = q.popleft()
+
+        if r == N-1 and c == N-1:
+            answer += 1
+
+        if dir == 0:    # 방향이 가로(0)라면
+            if c + 1 < N and board[r][c+1] == 0:
+                q.append((r, c+1, 0))
+            if r + 1 < N and c + 1 < N and board[r][c+1] == 0 and board[r+1][c] == 0 and board[r+1][c+1] == 0:
+                q.append((r+1, c+1, 2))
+
+        elif dir == 1:  # 방향이 세로(1)라면
+            if r + 1 < N and board[r+1][c] == 0:
+                q.append((r+1, c, 1))
+            if r + 1 < N and c + 1 < N and board[r][c+1] == 0 and board[r+1][c] == 0 and board[r+1][c+1] == 0:
+                q.append((r+1, c+1, 2))
+        
+        else:
+            if c + 1 < N and board[r][c+1] == 0:
+                q.append((r, c+1, 0))
+            if r + 1 < N and board[r+1][c] == 0:
+                q.append((r+1, c, 1))
+            if r + 1 < N and c + 1 < N and board[r][c+1] == 0 and board[r+1][c] == 0 and board[r+1][c+1] == 0:
+                q.append((r+1, c+1, 2))
+
+    return answer
 
 print(solution())
