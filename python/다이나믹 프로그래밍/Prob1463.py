207ca0e6db034e7f24a7a2fd423ab80b6eaf847e
yurrim <yurimkang20@gmail.com>
2024-02-29 11:20:14 +0900

    Upload. 1463 1로 만들기

--- python/다이나믹 프로그래밍/Prob1463.py
+++ python/다이나믹 프로그래밍/Prob1463.py
@@ -6,4 +6,17 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    N = int(input())
+
+    table = [0] * (N+1)
+    for num in range(2, N+1):
+        
+        table[num] = table[num-1] + 1
+        if num % 2 == 0:
+            table[num] = min(table[num], table[num//2] + 1)
+        if num % 3 == 0:
+            table[num] = min(table[num], table[num//3] + 1)
+
+    return table[N]
+
 print(solution())
