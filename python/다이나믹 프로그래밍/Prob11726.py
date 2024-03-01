a6f563c84d7d271cb9df5a58ac09130755653684
yurrim <yurimkang20@gmail.com>
2024-03-02 00:15:47 +0900

    Upload. 11726 2xn 타일링

--- python/다이나믹 프로그래밍/Prob11726.py
+++ python/다이나믹 프로그래밍/Prob11726.py
@@ -6,4 +6,18 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    N = int(input())
+
+    if N < 3:
+        return N
+
+    table = [0] * (N+1)
+    table[1] = 1
+    table[2] = 2
+
+    for idx in range(3, N+1):
+        table[idx] = (table[idx-1] + table[idx-2]) % 10007
+
+    return table[N]
+
 print(solution())
