7fce3a8cc0c226093f92ed2779075e9e967e7b8c
yurrim <yurimkang20@gmail.com>
2024-03-03 21:40:42 +0900

    Upload. 17070 파이프 옮기기 1

--- python/구현/Prob17070.py
+++ python/구현/Prob17070.py
@@ -2,40 +2,34 @@
 """ 골드 5. 파이프 옮기기 1 """
 
 import sys
-from collections import deque
+
+answer = 0
 
 def solution():
     input = sys.stdin.readline
 
+    global N, board
     N = int(input())
     board = [list(map(int, input().split())) for _ in range(N)]
 
-    return bfs(N, board)
-
-def bfs(N:int, board:list):
-
-    # 0 가로 방향 1 세로 방향 2 대각선 방향
-    q = deque()
-    q.append((0, 1, 0))
-
-    answer = 0
-    while q:
-
-        r, c, dir = q.popleft()
-
-        if r == N-1 and c == N-1:
-            answer += 1
-            continue
-
-        if (dir == 0 or dir == 2) and c + 1 < N and board[r][c+1] == 0:
-            q.append((r, c+1, 0))
-        
-        if (dir == 1 or dir == 2) and r + 1 < N and board[r+1][c] == 0:
-            q.append((r+1, c, 1))
-
-        if r + 1 < N and c + 1 < N and board[r][c+1] == 0 and board[r+1][c] == 0 and board[r+1][c+1] == 0:
-            q.append((r+1, c+1, 2))
+    dfs(0, 1, 0)
 
     return answer
+
+def dfs(r, c, dir):
+
+    if r == N-1 and c == N-1:
+        global answer
+        answer += 1
+        return
+    
+    if (dir == 0 or dir == 2) and c + 1 < N and board[r][c+1] == 0:
+        dfs(r, c+1, 0)
+    
+    if (dir == 1 or dir == 2) and r + 1 < N and board[r+1][c] == 0:
+        dfs(r+1, c, 1)
+
+    if r + 1 < N and c + 1 < N and board[r][c+1] == 0 and board[r+1][c] == 0 and board[r+1][c+1] == 0:
+        dfs(r+1, c+1, 2)
 
 print(solution())
