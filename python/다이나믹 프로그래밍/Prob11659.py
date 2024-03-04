2c1440a070396e79921396bd1360caa94bef1f22
yurrim <yurimkang20@gmail.com>
2024-03-04 12:22:49 +0900

    Upload. 11659 구간 합 구하기 4

--- python/다이나믹 프로그래밍/Prob11659.py
+++ python/다이나믹 프로그래밍/Prob11659.py
@@ -6,4 +6,18 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    N, M = map(int, input().split())
+
+    numbers = list(map(int, input().split()))
+    table = [0] * (N+1)
+
+    previous = 0
+    for idx in range(N):
+        previous += numbers[idx]
+        table[idx+1] = previous
+
+    for _ in range(M):
+        s, t = map(int, input().split())
+        print(table[t] - table[s-1])
+
 solution()
