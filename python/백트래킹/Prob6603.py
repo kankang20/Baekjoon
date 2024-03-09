1a07b48df0ab678132c7d98c8c8814c47a322a08
yurrim <yurimkang20@gmail.com>
2024-03-09 12:48:25 +0900

    Upload. 6603 로또

--- python/백트래킹/Prob6603.py
+++ python/백트래킹/Prob6603.py
@@ -2,8 +2,21 @@
 """ 실버 2. 로또 """
 
 import sys
+from itertools import combinations
 
 def solution():
     input = sys.stdin.readline
+
+    while True:
+
+        command = list(map(int, input().split()))
+
+        if command[0] == 0:
+            return
+        
+        for ans in list(combinations(command[1:], 6)):
+            print(*ans)
+
+        print()
 
 solution()
