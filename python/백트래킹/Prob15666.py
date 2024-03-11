84b68b468a0dd93f162d384acd9e557941a3c558
yurrim <yurimkang20@gmail.com>
2024-03-11 23:01:14 +0900

    Upload. 15666 N과 M (12)

--- python/백트래킹/Prob15666.py
+++ python/백트래킹/Prob15666.py
@@ -6,4 +6,29 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    global N, M, numbers
+    N, M = map(int, input().split())
+    numbers = list(input().split())
+
+    numbers.sort()
+
+    global answer
+    answer = set()
+
+    dfs(0, 0, [])
+
+    for ans in sorted(list(answer)):
+        print(ans)
+
+def dfs(count:int, pos:int, selected:list):
+
+    if count == M:
+        answer.add(" ".join(selected))
+        return 
+    
+    for idx in range(pos, N):
+        selected.append(numbers[idx])
+        dfs(count+1, idx, selected)
+        selected.pop()
+    
 solution()
