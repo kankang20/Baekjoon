d00ebc0f7bd005ffd1bac239a2a393b6bd75e10b
yurrim <yurimkang20@gmail.com>
2024-03-27 22:23:07 +0900

    Upload. 15683 감시

--- python/구현/Prob15683.py
+++ python/구현/Prob15683.py
@@ -3,8 +3,6 @@
 
 import sys
 
-direction = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-
 def solution():
     input = sys.stdin.readline
 
@@ -12,66 +10,50 @@ def solution():
     R, C = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(R)]
 
-    cctv = []
-
     global answer
     answer = R*C
 
+    cctv = []
     for r in range(R):
         for c in range(C):
-            if board[r][c] == 0:
-                answer += 1
-            elif 1 <= board[r][c] <= 5:
-                cctv.append((r, c))
+            if 1 <= board[r][c] <= 5:
+                cctv.append((r, c, board[r][c]))
 
+    direction = [(-1, 0), (0, 1), (1, 0), (0, -1)]
     cctv_direction = [0, [[0], [1], [2], [3]], 
                 [[0, 2], [1, 3]], 
                 [[0, 1], [1, 2], [2, 3], [3, 0]],
                 [[0, 1, 2], [1, 2, 3], [2, 3, 0], [3, 0, 1]],
                 [[0, 1, 2, 3]]]
 
-    dfs(board, cctv, 0, cctv_direction)
+    def count_area(board):
+        return sum(board[r].count(0) for r in range(R))
+
+    def dfs(idx):
+
+        if idx == len(cctv):
+            global answer
+            answer = min(answer, count_area(board))
+            return
+
+        r, c, cctv_num = cctv[idx]
+        for cc in cctv_direction[cctv_num]:
+            make_board(cc, r, c, 1)
+            dfs(idx+1)
+            make_board(cc, r, c, -1)
+
+    def make_board(dir, sr, sc, num):
+
+        for d in dir:
+            nr = sr + direction[d][0]
+            nc = sc + direction[d][1]
+            while 0 <= nr < R and 0 <= nc < C and board[nr][nc] < 6:
+                board[nr][nc] += num
+                nr += direction[d][0]
+                nc += direction[d][1]   
+
+    dfs(0)
 
     return answer
-
-def count_area(board):
-        zero = 0
-        for r in range(R):
-            for c in range(C):
-                if board[r][c] == 0:
-                    zero += 1
-        return zero
-
-def dfs(board, cctv, idx, cctv_dir):
-
-    if idx == len(cctv):
-        global answer
-        answer = min(answer, count_area(board))
-        return
-
-    r = cctv[idx][0]
-    c = cctv[idx][1]
-    cctv_num = board[r][c]
-
-    for cc in cctv_dir[cctv_num]:
-        make_board(board, cc, r, c, -1)
-        dfs(board, cctv, idx+1, cctv_dir)
-        make_board(board, cc, r, c, 0)
-
-def make_board(board, dir, sr, sc, num):
-
-    for d in dir:
-
-        nr = sr + direction[d][0]
-        nc = sc + direction[d][1]
-
-        while 0 <= nr < R and 0 <= nc < C and board[nr][nc] < 6:
-            if board[nr][nc] == -1 or board[nr][nc] == 0:
-                board[nr][nc] = num
-            nr += direction[d][0]
-            nc += direction[d][1]   
-
-
-
 
 print(solution())
