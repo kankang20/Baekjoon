ab98181ef7a22d664882dc3c56f6067026349795
yurrim <yurimkang20@gmail.com>
2024-04-18 17:33:39 +0900

    Upload. 15649 N과 M (1)

--- python/백트래킹/Prob15649.py
+++ python/백트래킹/Prob15649.py
@@ -2,8 +2,15 @@
 """ 실버 3. N과 M (1) """
 
 import sys
+from itertools import permutations
 
 def solution():
     input = sys.stdin.readline
+
+    N, M = map(int, input().split())
+    number = [str(idx+1) for idx in range(N)]
+    
+    for temp in list(permutations(number, M)):
+        print(" ".join(temp))
 
 solution()
