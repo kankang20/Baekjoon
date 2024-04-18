8ba27e98a66b30ae2c2824613576dda15dbadfba
yurrim <yurimkang20@gmail.com>
2024-04-18 17:08:10 +0900

    Upload. 15989 1, 2, 3, 더하기 4

--- python/다이나믹 프로그래밍/Prob15989.py
+++ python/다이나믹 프로그래밍/Prob15989.py
@@ -6,4 +6,10 @@ import sys
 def solution():
     input = sys.stdin.readline
 
-print(solution())
+    T = int(input())                            # 테스트케이스 개수
+    numbers = [int(input()) for _ in range(T)]  
+
+    max_number = max(numbers)
+    table = [0] * (4 if max_number < 4 else (max_number+1))
+
+solution()
