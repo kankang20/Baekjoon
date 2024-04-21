1c42ab23b639cc4f5e2bf556849e122fef9493b5
yurrim <yurimkang20@gmail.com>
2024-04-22 01:20:04 +0900

    Upload. 2866 문자열 잘라내기

--- python/문자열/Prob2866.py
+++ python/문자열/Prob2866.py
@@ -6,4 +6,24 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    R, C = map(int, input().split())
+    word = [list(input().rstrip()) for _ in range(R)]
+    
+    check = set()
+    for c in range(C):
+        check.add("".join([word[r][c] for r in range(R)]))
+    
+    answer = 0
+    for _ in range(1, R):
+        temp = set()
+        for _ in range(C):
+            w = check.pop()[1:]
+            if w in temp:
+                return answer
+            temp.add(w)
+        check = temp
+        answer += 1
+
+    return R-1
+
 print(solution())
