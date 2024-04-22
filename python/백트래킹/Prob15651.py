0e2018f5af3e4ec58c1ce64dc1070ff7eadfaf6d
yurrim <yurimkang20@gmail.com>
2024-04-22 23:56:10 +0900

    Upload. 15651 N과 M (3)

--- python/백트래킹/Prob15651.py
+++ python/백트래킹/Prob15651.py
@@ -6,4 +6,19 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    N, M = map(int, input().split())
+
+    def make(count:int, word:list):
+
+        if count == M:
+            print(" ".join(word))
+            return
+        
+        for jdx in range(1, N+1):
+            word.append(str(jdx))
+            make(count+1, word)
+            word.pop()
+
+    make(0, [])
+
 solution()
