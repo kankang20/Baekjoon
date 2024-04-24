4ece235affd2375db6c2ab9020a5897b8e52f25b
yurrim <yurimkang20@gmail.com>
2024-04-24 18:13:37 +0900

    Upload. 15654 N과 M (5)

--- python/백트래킹/Prob15654.py
+++ python/백트래킹/Prob15654.py
@@ -2,8 +2,15 @@
 """ 실버 3. N과 M (5) """
 
 import sys
+from itertools import permutations
 
 def solution():
     input = sys.stdin.readline
+
+    N, M = map(int, input().split())
+    numbers = list(map(int, input().split()))
+
+    for temp in list(permutations(sorted(numbers), M)):
+        print(" ".join(map(str, temp)))
 
 solution()
