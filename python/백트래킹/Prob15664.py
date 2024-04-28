2ab43b606b74186b74fc1c93eafd9881c6bb1758
yurrim <yurimkang20@gmail.com>
2024-04-28 22:40:30 +0900

    Upload. 15664 N과 M (10)

--- python/백트래킹/Prob15664.py
+++ python/백트래킹/Prob15664.py
@@ -7,4 +7,25 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    N, M = map(int, input().split())
+    numbers = sorted(list(map(int, input().split())))
+
+    check = set()
+
+    def make(idx:int, count:int, answer:list):
+
+        if count == M:
+            temp = " ".join(answer)
+            if temp not in check:
+                print(temp)
+                check.add(temp)
+            return
+
+        for jdx in range(idx+1, N):
+            answer.append(str(numbers[jdx]))
+            make(jdx, count+1, answer)
+            answer.pop()
+
+    make(-1, 0, [])
+
 solution()
