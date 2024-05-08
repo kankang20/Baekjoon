3c993311168a5c5b9c4ae00fbca5d873ade12254
yurrim <yurimkang20@gmail.com>
2024-05-08 23:32:34 +0900

    Upload. 2206 벽 부수고 이동하기

--- python/그래프 탐색/Prob2206.py
+++ python/그래프 탐색/Prob2206.py
@@ -12,11 +12,9 @@ def solution():
 
     dr = [0, 0, 1, -1]
     dc = [1, -1, 0, 0]
-
     visited = [[[0] * C for _ in range(R)] for _ in range(2)]
 
     def bfs():
-
         q = deque()
         q.append((0, 0, 0))
         visited[0][0][0] = 1
@@ -30,19 +28,18 @@ def solution():
             for d in range(4):
                 nr = r + dr[d]
                 nc = c + dc[d]
-
                 if not checkBoundry(nr, nc):
                     continue
-
-                # 벽을 부수고 이동
-                if flag == 0 and board[nr][nc] == 1 and visited[1][nr][nc] == 0:
-                    q.append((nr, nc, 1))
-                    visited[1][nr][nc] = visited[0][r][c] + 1
 
                 # 벽을 부수지 않고 이동
                 if board[nr][nc] == 0 and visited[flag][nr][nc] == 0:
                     q.append((nr, nc, flag))
                     visited[flag][nr][nc] = visited[flag][r][c] + 1
+
+                # 벽을 부수고 이동
+                elif flag == 0 and board[nr][nc] == 1 and visited[1][nr][nc] == 0:
+                    q.append((nr, nc, 1))
+                    visited[1][nr][nc] = visited[0][r][c] + 1
 
         return -1
 
