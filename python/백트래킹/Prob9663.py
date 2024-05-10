7893bde2f68ebfcc4323df34f4a98faba349d098
yurrim <yurimkang20@gmail.com>
2024-05-10 21:31:14 +0900

    Upload. 9663 N-Queen

--- python/백트래킹/Prob9663.py
+++ python/백트래킹/Prob9663.py
@@ -3,7 +3,30 @@
 
 import sys
 
-def solution():
-    input = sys.stdin.readline
+input = sys.stdin.readline
 
-print(solution())
+N = int(input())
+answer = 0
+
+col = [0] * N
+
+def check(row:int):
+    for idx in range(row):
+        if col[row] == col[idx] or (row - idx) == abs(col[row] - col[idx]):
+            return False
+    return True
+
+def queen(row:int):
+
+    if row == N:
+        global answer
+        answer += 1
+        return
+    
+    for idx in range(N):
+        col[row] = idx
+        if check(row):
+            queen(row+1)
+
+queen(0)
+print(answer)
