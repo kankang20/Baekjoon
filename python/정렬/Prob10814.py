c6bac504a6c0a1c02c639790e0d3ecb493bf6d63
yurrim <yurimkang20@gmail.com>
2024-05-11 21:25:40 +0900

    Upload. 10814 나이순 정렬

--- python/정렬/Prob10814.py
+++ python/정렬/Prob10814.py
@@ -6,4 +6,16 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    N = int(input())    # 회원 수
+
+    people = []
+    for idx in range(N):
+        age, name = input().rstrip().split()
+        people.append((int(age), idx, name))
+
+    people.sort(key=lambda x : (x[0], x[1]))
+    
+    for p in people:
+        print(p[0], p[2])
+
 solution()
