b33fd8037d41dea8fe1ed4cf689fa8468b80e3d7
yurrim <yurimkang20@gmail.com>
2024-05-12 18:26:04 +0900

    Upload. 23971 ZOAC 4

--- python/코테연습/Prob23971.py
+++ python/코테연습/Prob23971.py
@@ -8,27 +8,10 @@ def solution():
     input = sys.stdin.readline
     
     R, C, N, M = map(int, input().split())
-    
-    def calculate():
-        dr = [0, N+1, N+1]
-        dc = [M+1, 0, M+1]
-        
-        q = deque()
-        q.append((0, 0))
 
-        total = set((0, 0))
-        while q:
-            r, c = q.popleft()
+    W = (R // (N+1)) + (1 if R % (N+1) else 0)
+    H = (C // (M+1)) + (1 if C % (M+1) else 0)
 
-            for d in range(3):
-                nr = r + dr[d]
-                nc = c + dc[d]
-                if 0 <= nr < R and 0 <= nc < C:
-                    q.append((nr, nc))
-                    total.add((nr, nc))
-        
-        return len(total)
-    
-    return calculate()
+    return W * H
 
 print(solution())
