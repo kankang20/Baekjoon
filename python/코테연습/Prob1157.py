64e4f960c254d446905022dd2aa80c714e536173
yurrim <yurimkang20@gmail.com>
2024-05-14 14:14:07 +0900

    Upload. 1157 단어 공부

--- python/코테연습/Prob1157.py
+++ python/코테연습/Prob1157.py
@@ -6,4 +6,15 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    word = input().rstrip().upper()
+
+    alp = dict()
+    for w in word:
+        alp[w] = alp.get(w, 0) + 1
+
+    keys = sorted(alp.keys(), key=lambda x:alp[x], reverse=True)
+    
+    return keys[0] if len(keys) == 1 else ("?" if alp[keys[0]] == alp[keys[1]] else keys[0])
+
+
 print(solution())
