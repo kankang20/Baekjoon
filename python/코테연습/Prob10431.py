248bda28031ef89d58bf2321e20a67d18c4b0073
yurrim <yurimkang20@gmail.com>
2024-05-16 23:33:22 +0900

    Upload. 10431 줄세우기

--- python/코테연습/Prob10431.py
+++ python/코테연습/Prob10431.py
@@ -6,4 +6,24 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    T = int(input())
+    for _ in range(T):
+
+        numbers = list(map(int, input().split()))
+
+        answer = 0
+        student = []
+        for ndx in range(1, 21):
+            
+            temp = []
+            while student and numbers[ndx] < student[-1]:
+                temp.append(student.pop())
+                answer+=1
+
+            student.append(numbers[ndx])
+            while temp:
+                student.append(temp.pop())
+    
+        print(numbers[0], answer)
+
 solution()
