d9a1388df20b6b64924623cba24a089fa6912ba0
yurrim <yurimkang20@gmail.com>
2024-05-21 22:24:43 +0900

    Upload. 25757 임스와 함께하는 미니게임

--- python/코테연습/Prob25757.py
+++ python/코테연습/Prob25757.py
@@ -6,4 +6,17 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    game = list(input().rstrip().split())
+    
+    name = set()
+    for _ in range(int(game[0])):
+        name.add(input().rstrip())
+
+    if game[1] == 'Y':
+        return len(name)
+    elif game[1] == 'F':
+        return len(name) // 2
+    elif game[1] == 'O':
+        return len(name) // 3
+
 print(solution())
