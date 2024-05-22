b480e1a9823848643a2e05f41e47bb6e044468cc
yurrim <yurimkang20@gmail.com>
2024-05-22 11:59:51 +0900

    Upload. 7568 덩치

--- python/코테연습/Prob7568.py
+++ python/코테연습/Prob7568.py
@@ -6,4 +6,24 @@ import sys
 def solution():
     input = sys.stdin.readline
 
-print(solution())
+    N = int(input())
+    
+    people = []
+    for ndx in range(N):
+        weight, height = map(int, input().split())
+        people.append((ndx, weight, height))
+
+    people.sort(key= lambda x : x[1])
+    
+    answer = [0] * N
+    for idx in range(N):
+        w, h = people[idx][1], people[idx][2]
+        count = 0
+        for jdx in range(idx+1, N):
+            if w < people[jdx][1] and h < people[jdx][2]:
+                count += 1
+        answer[people[idx][0]] = count+1
+
+    print(*answer)
+
+solution()
