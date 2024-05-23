077fafaf8da33ce3f23bf1f7b7abdfbaed2c5fcf
yu <yurimkang20@gmail.com>
2024-05-23 23:14:27 +0900

    Upload. 8978 올림픽

---
+++ python/코테연습/Prob8979.py
@@ -0,0 +1,34 @@
+
+""" 실버 5. 올림픽 """
+
+import sys
+
+def solution():
+    input = sys.stdin.readline
+
+    N, K = map(int, input().split())
+    
+    result = []
+    G, S, B = 0, 0, 0
+    for _ in range(N):
+        n, g, s, b = map(int, input().split())
+        if n == K:
+            G, S, B = g, s, b
+        else:
+            result.append((g, s, b))
+
+    answer = 1
+    for idx in range(N-1):
+        temp = result[idx]
+        if G < temp[0]:
+            answer += 1
+        elif G == temp[0]:
+            if S < temp[1]:
+                answer += 1
+            elif S == temp[1]:
+                if B < temp[2]:
+                    answer += 1
+
+    return answer 
+        
+print(solution())
