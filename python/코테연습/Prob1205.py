8a12f2a61c406fc3037863299c93275361d1e5fe
yu <yurimkang20@gmail.com>
2024-05-25 15:27:52 +0900

    Upload. 1205 등수 구하기

--- python/코테연습/Prob1205.py
+++ python/코테연습/Prob1205.py
@@ -6,4 +6,20 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    N, G, P = map(int, input().split())
+
+    if N == 0:
+        return 1
+    
+    grade = list(map(int, input().split()))
+    
+    if N == P and G <= grade[-1]:
+        return -1
+
+    for idx in range(N):
+        if grade[idx] <= G:
+            return idx + 1
+
+    return N + 1
+
 print(solution())
