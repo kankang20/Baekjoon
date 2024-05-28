25a00d4073319951a338762f86960ba203731794
yu <yurimkang20@gmail.com>
2024-05-28 11:10:40 +0900

    Upload. 11652 카드

--- python/정렬/Prob11652.py
+++ python/정렬/Prob11652.py
@@ -6,4 +6,24 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    numbers = dict()
+    ansNum = 0
+    ansCnt = 0
+
+    N = int(input())
+
+    for _ in range(N):
+        num  = int(input())
+        temp = numbers.get(num, 0) + 1
+
+        if ansCnt == temp and num < ansNum:
+            ansNum = num
+        elif ansCnt < temp:
+            ansNum = num
+            ansCnt = temp
+
+        numbers[num] = temp
+
+    return ansNum
+
 print(solution())
