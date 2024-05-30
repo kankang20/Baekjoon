20d86099da36a6ce94a4d903dcfe8974dedaeccc
yu <yurimkang20@gmail.com>
2024-05-30 12:23:27 +0900

    Upload. 10825 국영수

--- python/정렬/Prob10825.py
+++ python/정렬/Prob10825.py
@@ -6,4 +6,16 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    N = int(input())
+
+    answer = []
+    for _ in range(N):
+        temp = list(input().rstrip().split())
+        answer.append((temp[0], int(temp[1]), int(temp[2]), int(temp[3])))
+
+    answer.sort(key=lambda x: (-x[1], x[2], -x[3], x[0]))
+
+    for ans in answer:
+        print(ans[0])
+
 solution()
