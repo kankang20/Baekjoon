9627913b70ab06bdc7525980feda8f5b2bd0de94
yu <yurimkang20@gmail.com>
2024-06-01 12:27:13 +0900

    Upload. 20920 영단어 암기는 괴로워

--- python/정렬/Prob20920.py
+++ python/정렬/Prob20920.py
@@ -6,6 +6,17 @@ import sys
 def solution():
     input = sys.stdin.readline
 
-    
+    N, M = map(int, input().split())
+
+    word = dict()
+
+    for _ in range(N):
+        w = input().rstrip()
+
+        if M <= len(w):
+            word[w] = word.get(w, 0) + 1
+
+    for (k, _) in sorted(word.items(), key=lambda x: (-x[1], -len(x[0]), x[0])):
+        print(k)
 
 solution()
