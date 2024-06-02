ebc0aa26c846f2648611a9866aa7814bf2d4acfa
yu <yurimkang20@gmail.com>
2024-06-02 22:59:36 +0900

    Upload. 2910 빈도 정렬렬

--- python/정렬/Prob2910.py
+++ python/정렬/Prob2910.py
@@ -6,4 +6,17 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    N, C = map(int, input().split())
+
+    number = list(map(int, input().split()))
+    
+    count = dict()
+    for idx in range(N):
+        num = number[idx]
+        count[num] = count.get(num, 0) + 1
+
+    for (key, value) in sorted(count.items(), key=lambda x : (-x[1])):
+        for _ in range(value):
+            print(key, end=" ")
+
 solution()
