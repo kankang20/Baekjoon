b0529c5ec7893cbc822f18129db56cbc9cfb307e
yu <yurimkang20@gmail.com>
2024-06-03 13:48:07 +0900

    Upload. 1431 시리얼 번호

--- python/정렬/Prob1431.py
+++ python/정렬/Prob1431.py
@@ -6,4 +6,21 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    N = int(input())
+    number = [input().rstrip() for _ in range(N)]
+
+    def calculate(num:str):
+        temp = 0
+        for n in num:
+            if n.isdigit():
+                temp += int(n)
+        return temp
+
+    answer = []
+    for num in number:
+        answer.append((num, calculate(num)))
+
+    for (ans, _) in sorted(answer, key=lambda x : (len(x[0]), x[1], x)):
+        print(ans)
+    
 solution()
