61acdfe13ef79fd74b87f4306bd1d17b4bc4afd0
yu <yurimkang20@gmail.com>
2024-06-04 12:52:53 +0900

    Upload. 27496 발머의 피크 이론

--- python/누적합/Prob27496.py
+++ python/누적합/Prob27496.py
@@ -6,4 +6,18 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    N, L = map(int, input().split())
+    drink = list(map(int, input().split()))
+
+    total = 0
+    answer = 0
+    for idx in range(N):
+        total += drink[idx]
+        if L <= idx:
+            total -= drink[idx-L]
+        if 129 <= total <= 138:
+            answer += 1
+
+    return answer
+
 print(solution())
