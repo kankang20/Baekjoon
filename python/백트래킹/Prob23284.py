07eb01f7e794c3e76f60f073f1908f74c0fe3a48
yu <yurimkang20@gmail.com>
2024-06-04 12:13:43 +0900

    Upload. 23284 모든 스택 수열

--- python/백트래킹/Prob23284.py
+++ python/백트래킹/Prob23284.py
@@ -2,8 +2,14 @@
 """ 골드 5. 모든 스택 수열 """
 
 import sys
+from itertools import permutations
 
 def solution():
     input = sys.stdin.readline
+
+    N = int(input())
+
+    for ans in list(permutations([(idx+1) for idx in range(N)], N)):
+        print(ans)
 
 solution()
