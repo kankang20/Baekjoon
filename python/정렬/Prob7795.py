98dcde88370b6667e06c2d975d5b84f90261c4a1
yu <yurimkang20@gmail.com>
2024-06-05 11:42:00 +0900

    Upload. 7795 먹을 것인가 먹힐 것인가

--- python/정렬/Prob7795.py
+++ python/정렬/Prob7795.py
@@ -6,4 +6,24 @@ import sys
 def solution():
     input = sys.stdin.readline
 
-print(solution())
+    T = int(input())
+    for _ in range(T):
+
+        N, M = map(int, input().split())        # A의 수 N, B의 수 M
+        A = list(map(int, input().split()))
+        B = list(map(int, input().split()))
+
+        A.sort()
+        B.sort(reverse=True)
+
+        answer = 0
+
+        bdx = M-1
+        for a in A:
+            while 0 <= bdx and B[bdx] < a:
+                bdx -= 1
+            answer += (M - bdx - 1)
+        
+        print(answer)
+
+solution()
