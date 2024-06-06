c319be394cc43922a76c65514f3e7d0d7ee769ae
yu <yurimkang20@gmail.com>
2024-06-06 18:54:49 +0900

    Upload. 2493 탑

--- python/자료구조/Prob2493.py
+++ python/자료구조/Prob2493.py
@@ -6,4 +6,20 @@ import sys
 def solution():
     input = sys.stdin.readline
 
-print(solution())
+    N = int(input())
+    wall = list(map(int, input().split()))
+
+    answer = [0] * N
+    
+    stack = [(1, wall[0])]
+    for wdx in range(1, N):
+
+        while stack and stack[-1][1] <= wall[wdx]:
+            stack.pop()
+
+        answer[wdx] = stack[-1][0] if stack else 0
+        stack.append((wdx+1, wall[wdx]))
+
+    print(*answer)
+
+solution()
