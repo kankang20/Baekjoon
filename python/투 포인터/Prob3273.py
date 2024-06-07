946b4509ab269c457a091ae45c6ea3e045fea0a7
yu <yurimkang20@gmail.com>
2024-06-07 13:15:44 +0900

    Upload. 3273 두 수의 합

---
+++ python/투 포인터/Prob3273.py
@@ -0,0 +1,21 @@
+
+""" 실버 3. 두 수의 합 """
+
+import sys
+
+def solution():
+    input = sys.stdin.readline
+
+    N = int(input())
+    number = sorted(list(map(int, input().split())))
+    X = int(input())
+
+    lp, rp = 0, 0
+
+    answer = 0
+    while rp < N:
+        
+    
+    return answer
+
+print(solution())
