eed8bca8b9bd5ff3c9b5f4213efb5d5d09bca5cb
yu <yurimkang20@gmail.com>
2024-06-08 17:39:31 +0900

    Upload. 2751 수 정렬하기 2

--- python/정렬/Prob2751.py
+++ python/정렬/Prob2751.py
@@ -6,4 +6,10 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    N = int(input())
+    number = [int(input()) for _ in range(N)]
+
+    for num in sorted(number):
+        print(num)
+
 solution()
