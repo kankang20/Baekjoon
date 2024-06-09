0fe4dfc18dd62c3bd6c48b1e5264b13486f1e9e0
yu <yurimkang20@gmail.com>
2024-06-09 16:17:10 +0900

    Upload. 5567 결혼식

--- python/그래프 탐색/Prob5567.py
+++ python/그래프 탐색/Prob5567.py
@@ -2,9 +2,43 @@
 """ 실버 2. 결혼식 """
 
 import sys
+from collections import deque
 
 def solution():
     input = sys.stdin.readline
+
+    N = int(input())
+    M = int(input())
+
+    edge = [[] for _ in range(N+1)]
+    for _ in range(M):
+        a, b = map(int, input().split())
+        edge[a].append(b)
+        edge[b].append(a)
+
+    def bfs():
+
+        check = [False] * (N+1)
+        check[1] = True
+
+        q = deque()
+        q.append(1)
+
+        answer = 0
+        for _ in range(2):
+            for _ in range(len(q)):
+                node = q.popleft()
+
+                for nn in edge[node]:
+                    if not check[nn]:
+                        q.append(nn)
+                        check[nn] = True
+            
+            answer += len(q)
+
+        return answer
+    
+    return bfs()
 
 print(solution())
 
