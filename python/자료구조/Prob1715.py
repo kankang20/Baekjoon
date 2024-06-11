d5263f9aa7be1c9780a6a37ec0d263fb4b91f630
yu <yurimkang20@gmail.com>
2024-06-11 15:51:09 +0900

    Upload. 1715 카드 정렬하기

--- python/자료구조/Prob1715.py
+++ python/자료구조/Prob1715.py
@@ -2,8 +2,22 @@
 """ 골드 4. 카드 정렬하기 """
 
 import sys
+from heapq import heappush, heappop
 
 def solution():
     input = sys.stdin.readline
+
+    N = int(input())
+
+    card = []
+    for _ in range(N):
+        heappush(card, int(input()))
+
+    answer = 0
+    for _ in range(N-1):
+        answer += (heappop(card) + heappop(card))
+        heappush(card, answer)
+    
+    return answer
 
 print(solution())
