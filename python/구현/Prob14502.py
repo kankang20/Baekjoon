a9852342bc582e39658313bdf911b84655277537
yu <yurimkang20@gmail.com>
2024-06-15 01:53:58 +0900

    Upload. 14502 연구소

--- python/구현/Prob14502.py
+++ python/구현/Prob14502.py
@@ -4,7 +4,11 @@
 import sys
 from collections import deque
 
+dr = [0, 0, 1, -1]
+dc = [1, -1, 0, 0]
+
 answer = 0
+virus = deque()
 
 def solution():
     input = sys.stdin.readline
@@ -13,7 +17,6 @@ def solution():
     R, C = map(int, input().split())
 
     board = []
-    virus = deque()
 
     for r in range(R):
         board.append(list(map(int, input().split())))
@@ -22,15 +25,16 @@ def solution():
                 virus.append((r, c))
 
 
-    make_wall(0)
+    make_wall(board, 0)
 
     return answer
 
 def make_wall(board:list, count:int):
 
     if count == 3:
+        vv = virus
         global answer
-        answer = max(answer, bfs(board))
+        answer = max(answer, bfs(board, vv))
         return
     
     for r in range(R):
@@ -41,9 +45,31 @@ def make_wall(board:list, count:int):
                 board[r][c] = 0
 
 
-def bfs(board:list, virus:list):
+def bfs(board:list, virus:deque):
 
-    selected = [[False] * C in range(R)]
+    selected = [[False] * C for _ in range(R)]
+
+    count = 0
+    while virus:
+
+        r, c = virus.popleft()
+
+        for d in range(4):
+            nr = r + dr[d]
+            nc = c + dc[d]
+
+            if nr < 0 or nr >= R or nc < 0 or nc >= C:
+                continue
+
+            if board[nr][nc] > 0 or selected[nr][nc]:
+                continue
+
+            virus.append((nr, nc))
+            selected[nr][nc] = True
+
+            count += 1
+    
+    return count
     
 
 
