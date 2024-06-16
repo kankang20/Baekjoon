9ce9512031353890e79f180ce47d7d49b9e90a99
yu <yurimkang20@gmail.com>
2024-06-17 00:50:54 +0900

    Upload. 2012 등수 매기기

--- python/그리디/Prob2012.py
+++ python/그리디/Prob2012.py
@@ -7,12 +7,14 @@ def solution():
     input = sys.stdin.readline
 
     N = int(input())
-    grade = sorted([int(input()) for _ in range(N)])
+    grade = sorted([0] + [int(input()) for _ in range(N)])
 
-    answer = 0
-    for idx in range(N):
-        answer += abs(idx + 1 - grade[idx])
+    # answer = 0
+    # for idx in range(N):
+    #     answer += abs(idx + 1 - grade[idx])
 
-    return answer
+    # return answer
+
+    return sum(abs(idx - g) for (idx, g) in enumerate(grade))
 
 print(solution())
