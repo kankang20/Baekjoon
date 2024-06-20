df645e713e3c65dafa39d02a046ca2ef9c4d07d4
yu <yurimkang20@gmail.com>
2024-06-20 22:09:43 +0900

    Upload. 18429 근손실

--- python/코테연습/Prob18429.py
+++ python/코테연습/Prob18429.py
@@ -2,8 +2,24 @@
 """ 실버 3. 근손실 """
 
 import sys
+from itertools import permutations
 
 def solution():
     input = sys.stdin.readline
 
-solution()
+    N, K = map(int, input().split())
+    exer = list(map(int, input().split()))
+
+    answer = 0
+    for temp in list(permutations([num for num in range(N)], N)):
+        count = 0
+        for t in temp:
+            count += (exer[t] - K)
+            if count < 0:
+                break
+        else:
+            answer += 1
+    
+    return answer
+
+print(solution())
