3975c99f7e532f2b4ddf0389b6af94fa230fc08f
yu <yurimkang20@gmail.com>
2024-06-21 14:04:22 +0900

    Upload. 18870 좌표 압축

--- python/이진탐색/Prob18870.py
+++ python/이진탐색/Prob18870.py
@@ -10,25 +10,12 @@ def solution():
     coor = list(map(int, input().split()))
 
     sorted_coor = sorted(list(set(coor)))
-    N = len(sorted_coor)
-
-    def search(target:int):
-        start, end = 0, N - 1
-        ans = 0
-        while start <= end:
-
-            mid = (start + end) // 2
-            num = sorted_coor[mid]
-
-            if num <= target:
-                start += 1
-                ans = mid
-            elif num > target:
-                end -= 1
-        
-        return ans
+    
+    number = {}
+    for idx in range(len(sorted_coor)):
+        number[sorted_coor[idx]] = idx
     
     for c in coor:
-        print(search(c), end=" ")
+        print(number[c], end=" ")
     
 solution()
