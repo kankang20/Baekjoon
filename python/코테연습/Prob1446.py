dec72739c6cdde9867c1beeba0b55bb46a7c78b2
yu <yurimkang20@gmail.com>
2024-06-22 10:31:57 +0900

    Upload. 1446 지름길

--- python/코테연습/Prob1446.py
+++ python/코테연습/Prob1446.py
@@ -6,4 +6,24 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    N, D = map(int, input().split())
+
+    short = []
+    for _ in range(N):
+        start, end, length = map(int, input().split())
+        short.append((start, end, length))
+    
+    short.sort(key=lambda x : (x[0], x[2], x[1]))
+
+    print(short)
+
+    # ans = 0
+    # pos, idx = 0
+    # while pos < D:
+
+
+
+
+
+
 print(solution())
