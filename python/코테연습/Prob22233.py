c34b26a4152592eace2108e3786830cf40d71ba8
yu <yurimkang20@gmail.com>
2024-06-23 15:16:16 +0900

    Upload. 22233 가희와 키워드

--- python/코테연습/Prob22233.py
+++ python/코테연습/Prob22233.py
@@ -6,4 +6,19 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    N, M = map(int, input().split())
+
+    answer = 0
+
+    keyword = {}
+    for _ in range(N):
+        keyword[input().rstrip()] = 1
+        answer += 1
+
+    for _ in range(M):
+        for word in list(input().rstrip().split(",")):
+            if keyword.pop(word, 0):
+                answer -= 1
+        print(answer)
+
 solution()
