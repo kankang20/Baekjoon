731a20995e2e2e94a32a4b8d5617dbc0f406e4d6
yu <yurimkang20@gmail.com>
2024-07-07 23:50:19 +0900

    upload. 11650 좌표 정렬하기

--- python/정렬/Prob11650.py
+++ python/정렬/Prob11650.py
@@ -6,4 +6,14 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    N = int(input())
+
+    coor = []
+    for _ in range(N):
+        x, y = map(int, input().split())
+        coor.append((x, y))
+
+    for (x, y) in sorted(coor, key= lambda x : (x[0], x[1])):
+        print(x, y)
+
 solution()
