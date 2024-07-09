0d7b988b844a8dbe1229149db39a5ed89905ea5f
yu <yurimkang20@gmail.com>
2024-07-09 15:48:10 +0900

    upload. 23309 철도 공사

--- python/자료구조/Prob23309.py
+++ python/자료구조/Prob23309.py
@@ -13,16 +13,15 @@ def solution():
     input = sys.stdin.readline
 
     N, M = map(int, input().split())
-    N -= 1
 
     position = {}
     subway = []
     for idx, value in enumerate(list(map(int, input().split()))):
         position[value] = idx
         if idx == 0:
-            subway.append(Station(value, N, 1))
+            subway.append(Station(value, N-1, 1))
         elif idx == (N-1):
-            subway.append(Station(value, N-1, 0))
+            subway.append(Station(value, N-2, 0))
         else:
             subway.append(Station(value, idx-1, idx+1))
 
@@ -30,27 +29,81 @@ def solution():
         com = list(input().rstrip().split())
 
         if com[0] == "BN":
-            # position에서 현재 위치 가져오기
-            temp = position[int(com[1])]
-            # 현재 Station의 다음 역 출력, next 변경
-            tnext, tprev = 0, 0
-            station = subway[temp]
-            print(subway[station.next].value)
-            station.next = N
+            now_pos = position[int(com[1])]
+            now_station = subway[now_pos]
+
+            next_pos = now_station.next
+            next_station = subway[next_pos]
+
+            print(next_station.value)
             
+            subway.append(Station(int(com[2]), now_pos, next_pos))
+            now_station.next = N
+            next_station.prev = N
 
+            position[int(com[2])] = N
 
-            # 다음 역의 prev 변경
-            # 역 추가하기
-
-            station = subway[temp]
             N += 1
+
         elif com[0] == "BP":
+            now_pos = position[int(com[1])]
+            now_station = subway[now_pos]
+
+            prev_pos = now_station.prev
+            prev_station = subway[prev_pos]
+
+            print(prev_station.value)
+
+            subway.append(Station(int(com[2]), prev_pos, now_pos))
+            prev_station.next = N
+            now_station.prev = N
+
+            position[int(com[2])] = N
+
             N += 1
+
         elif com[0] == "CN":
-            N -= 1
-        elif com[0] == "CP":
+            now_pos = position[int(com[1])]
+            now_station = subway[now_pos]
+
+            next_pos = now_station.next
+            next_station = subway[next_pos]
+
+            print(next_station.value)
+
+            nnext_pos = next_station.next
+            nnext_station = subway[nnext_pos]
+
+            now_station.next = nnext_pos
+            nnext_station.prev = now_pos
+
             N -= 1
 
+        elif com[0] == "CP":
+            now_pos = position[int(com[1])]
+            now_station = subway[now_pos]
+
+            prev_pos = now_station.prev
+            prev_station = subway[prev_pos]
+
+            print(prev_station.value)
+
+            pprev_pos = prev_station.prev
+            pprev_station = subway[pprev_pos]
+
+            pprev_station.next = now_pos
+            now_station.prev = pprev_pos
+
+            N -= 1
+
+        # subway_print(subway, N)
+
+# def subway_print(subway:list, N:int):
+#     pos = subway[0]
+#     print("!! subway print -----")
+#     for _ in range(N):
+#         print(pos.value)
+#         pos = subway[pos.next]
+#     print("---------------------")
 
 solution()
