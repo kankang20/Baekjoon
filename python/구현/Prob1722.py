143f72c37a713419bc89abb2a7f32a31a7fb091c
yu <yurimkang20@gmail.com>
2024-07-10 17:36:43 +0900

    upload. 1722 순열의 순서

--- python/구현/Prob1722.py
+++ python/구현/Prob1722.py
@@ -2,22 +2,63 @@
 """ 골드 5. 순열의 순서 """
 
 import sys
-from itertools import permutations
 
 def solution():
     input = sys.stdin.readline
 
     N = int(input())    
-    question = list(map(int, input().split()))
+    command = list(map(int, input().split()))
 
-    numbers = list(permutations([num for num in range(1, N+1)]))
-    if question[0] == 1:    # k 번째 수열 출력
-        print(" ".join(list(map(str, numbers[question[1]-1]))))
-    elif question[0] == 2:  # 입력받은 순열의 순서
-        answer = tuple(question[1:])
-        for idx in range(len(numbers)):
-            if numbers[idx] == answer:
-                print(idx+1)
+    position = [1] + [0] * N
+    for idx in range(1, N+1):
+        position[idx] = position[idx-1] * idx
+
+    selected = [False] * (N+1)
+
+    if command[0] == 1:
+        search_permutation(N, position, command[1], selected)
+    elif command[0] == 2:
+        search_kth(N, position, command, selected)
+    
+
+def search_permutation(N:int, position:list, K:int, selected:list):
+
+    answer = [0] * (N+1)
+    for idx in range(1, N+1):
+        
+        count = 1
+        pos = N - idx       # permutation의 남은 자릿수
+        for jdx in range(1, N+1):
+            
+            if selected[jdx]:
+                continue
+
+            if K <= count * position[pos]:
+                K -= (count-1) * position[pos]
+                answer[idx] = jdx
+                selected[jdx] = True
                 break
+
+            count += 1
+
+    for ans in answer[1:]:
+        print(ans, end=" ")
+
+def search_kth(N:int, position:list, number:list, selected:list):
+
+    answer = 1
+    for idx in range(1, N+1):
+
+        count = 0
+        pos = N - idx
+        for jdx in range(1, number[idx]):
+
+            if not selected[jdx]:
+                count += 1
+        
+        answer += count * position[pos]
+        selected[number[idx]] = True
+
+    print(answer)
 
 solution()
