ef29cdc861ab2cd8032598e475724744b1cbe9ad
yu <yurimkang20@gmail.com>
2024-07-12 23:38:05 +0900

    upload. 17141 연구소 2

--- python/구현/Prob17141.py
+++ python/구현/Prob17141.py
@@ -6,4 +6,10 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    R, C = map(int, input().split())
+    
+    board = []
+    for _ in range(R):
+        board.append(list(map(int, input().split())))
+
 print(solution())
