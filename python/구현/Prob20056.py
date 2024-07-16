2420d8a7703010602cf94c6f01d1db62cbb28a32
yu <yurimkang20@gmail.com>
2024-07-16 23:30:34 +0900

    upload. 20056 마법사 상어와 파이어볼

--- python/구현/Prob20056.py
+++ python/구현/Prob20056.py
@@ -3,7 +3,20 @@
 
 import sys
 
+
 def solution():
     input = sys.stdin.readline
+
+    N, M, K = map(int, input().split())
+    
+    fireballs = []
+    for _ in range(M):
+        r, c, m, d, s = map(int, input().split())
+        fireballs.append((r-1, c-1, m, d, s))
+
+    
+
+    
+
 
 print(solution())
