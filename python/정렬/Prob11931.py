b025f805fc53bb6fe938f7bdd90643561973e27f
yu <yurimkang20@gmail.com>
2024-07-21 10:18:18 +0900

    upload. 11931 수 정렬하기 4

--- python/정렬/Prob11931.py
+++ python/정렬/Prob11931.py
@@ -6,4 +6,10 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    N = int(input())
+    numbers = [int(input()) for _ in range(N)]
+
+    for num in sorted(numbers, reverse=True):
+        print(num)
+
 solution()
