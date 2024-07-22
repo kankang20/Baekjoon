b0882b85e3acb032483a2bb10a81f2833237ddf8
yu <yurimkang20@gmail.com>
2024-07-22 19:35:50 +0900

    upload. 1822 차집합

--- python/이진탐색/Prob1822.py
+++ python/이진탐색/Prob1822.py
@@ -6,4 +6,35 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    N, M = map(int, input().split())
+    A = sorted(list(map(int, input().split())))
+    B = sorted(list(map(int, input().split())))
+
+    def search(target:int):
+
+        start = 0
+        end = M-1
+
+        while start <= end:
+
+            mid = (start + end) // 2
+
+            if B[mid] == target:
+                return False
+            elif B[mid] < target:
+                start = mid + 1
+            elif target < B[mid]:
+                end = mid - 1 
+            
+        return True
+    
+    answer = []
+    for a in A:
+        if search(a):
+            answer.append(str(a))
+
+    print(len(answer))
+    if answer:
+        print(" ".join(answer))
+
 solution()
--- python/자료구조/Prob1822.py
+++ python/자료구조/Prob1822.py
@@ -6,4 +6,14 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    N, M = map(int, input().split())
+    A = set(list(map(int, input().split())))
+    B = set(list(map(int, input().split())))
+
+    answer = list(A - B)
+
+    print(len(answer))
+    for ans in sorted(answer):
+        print(ans, end = " ")
+
 solution()
