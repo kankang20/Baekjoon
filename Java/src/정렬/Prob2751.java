94a5716a26565a1e8ec54b7747c96abaa0916068
yu <yurimkang20@gmail.com>
2024-07-23 23:49:07 +0900

    upload. 2751 수 정렬하기 2

--- Java/src/정렬/Prob2751.java
+++ Java/src/정렬/Prob2751.java
@@ -1,5 +1,30 @@
 package 정렬;
 
+import java.io.*;
+import java.util.*;
+
 public class Prob2751 {
     
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N  = Integer.parseInt(br.readLine());
+
+        int[] number = new int[N];
+        for (int i = 0; i < N; i++) {
+            number[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(number);
+
+        for (int i = 0; i < (N-1); i++) {
+            sb.append(number[i]).append("\n");
+        }
+
+        sb.append(number[N-1]);
+
+        System.out.println(sb);
+    }
 }
