7246260b9af357e1c4da93d9c5bc157347bc2c6a
yu <yurimkang20@gmail.com>
2024-07-28 20:07:56 +0900

    upload. 10825 국영수

--- Java/src/정렬/Prob10825.java
+++ Java/src/정렬/Prob10825.java
@@ -5,23 +5,32 @@ import java.util.*;
 
 public class Prob10825 {
 
-    static class Student implements Comparable{
+    static class Student implements Comparable<Student> {
 
         String name;
-        int grade1;
-        int grade2;
-        int grade3;
+        int kor;
+        int eng;
+        int mat;
 
-        public Student(String name, int grade1, int grade2, int grade3) {
+        public Student(String name, int kor, int eng, int mat) {
             this.name = name;
-            this.grade1 = grade1;
-            this.grade2 = grade2;
-            this.grade3 = grade3;
+            this.kor = kor;
+            this.eng = eng;
+            this.mat = mat;
         }
 
         @Override
-        public int compareTo(Object o) {
-            return grade1;
+        public int compareTo(Student o) {
+            if (this.kor == o.kor) {
+                if (this.eng == o.eng) {
+                    if (this.mat == o.mat) {
+                        return this.name.compareTo(o.name);
+                    }
+                    return o.mat - this.mat;
+                }
+                return this.eng - o.eng;
+            }
+            return o.kor - this.kor;
         }
     }
     
@@ -32,13 +41,23 @@ public class Prob10825 {
 
         int N = Integer.parseInt(br.readLine());
 
-        Student[] name = new Student[N];
+        Student[] students = new Student[N];
 
         StringTokenizer st;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            
+            students[i] = new Student(st.nextToken(), 
+                            Integer.parseInt(st.nextToken()), 
+                            Integer.parseInt(st.nextToken()), 
+                            Integer.parseInt(st.nextToken()));
         }
 
+        Arrays.sort(students);
+
+        for (int i = 0; i < N; i++) {
+            sb.append(students[i].name).append("\n");
+        }
+
+        System.out.println(sb);
     }
 }
