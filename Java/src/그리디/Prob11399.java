df97b848a83f3a96bba7aaff82ddcdc5482f8a94
yu <yurimkang20@gmail.com>
2024-07-30 14:44:05 +0900

    upload. 11399 ATM

--- Java/src/그리디/Prob11399.java
+++ Java/src/그리디/Prob11399.java
@@ -5,4 +5,26 @@ import java.util.*;
 
 public class Prob11399 {
 
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] atm = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            atm[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(atm);
+
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            answer += (atm[i] * (N - i)); 
+        }
+
+        System.out.println(answer);
+
+    } // end main
 }
