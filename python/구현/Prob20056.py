d2fb84aaa7dcc8a7db33069907c3ca5957b3b5bd
yu <yurimkang20@gmail.com>
2024-08-02 14:05:36 +0900

    upload. 20056 마법사 상어와 파이어볼

--- python/구현/Prob20056.py
+++ python/구현/Prob20056.py
@@ -14,8 +14,8 @@ def solution():
     
     fireballs = deque()
     for _ in range(M):
-        r, c, m, d, s = map(int, input().split())
-        fireballs.append((r-1, c-1, m, d, s))
+        r, c, m, s, d = map(int, input().split())
+        fireballs.append((r-1, c-1, m, s, d))
 
     board = [[deque() for _ in range(N)] for _ in range(N)]
 
@@ -23,10 +23,10 @@ def solution():
 
         # 1. 모든 파이어볼이 자신의 방향 d로 속력 s칸 만큼 이동한다.
         while fireballs:
-            r, c, m, d, s = fireballs.popleft()
+            r, c, m, s, d = fireballs.popleft()
             nr = (r + dr[d] * s) % N
             nc = (c + dc[d] * s) % N
-            board[nr][nc].append((m, d, s))
+            board[nr][nc].append((m, s, d))
 
         # 2. 이동이 모두 끝난 뒤, 2개 이상의 파이어볼이 있는 칸에서는 다음과 같은 일이 일어난다.
         for r in range(N):
@@ -36,45 +36,42 @@ def solution():
                     continue
 
                 if len(board[r][c]) == 1:
-                    nm, nd, ns = board[r][c][0]
-                    fireballs.append((r, c, nm, nd, ns))
+                    nm, ns, nd = board[r][c].popleft()
+                    fireballs.append((r, c, nm, ns, nd))
 
                 else:
-                    # 2-1. 같은 칸에 있는 파이어볼은 모두 하나로 합쳐진다.
-                    # 2-2. 파이어볼은 4개의 파이어볼로 나누어진다.
-                    # 2-3. 나누어진 파이어볼의 질량, 속력, 방향은 다음과 같다. 
-                    totalf = 0      # 파이어볼의 개수
-                    totalm = 0      # 파이어볼 질량의 합
-                    totals = 0      # 파이어볼 속력의 합
+                    totalf = len(board[r][c])       # 파이어볼 개수
+                    totalm = 0                      # 파이어볼 질량의 합
+                    totals = 0                      # 파이어볼 속력의 합
 
-                    check = board[r][c][0][1] % 2
+                    check = board[r][c][0][2] % 2
                     flag = True
-                    for (m, d, s) in board[r][c]:
-                        totalf += 1
+
+                    # 2-1. 같은 칸에 있는 파이어볼은 모두 하나로 합쳐진다.
+                    while board[r][c]:
+                        m, s, d = board[r][c].popleft()
                         totalm += m
                         totals += s
+
                         if d % 2 != check:
                             flag = False
-                
+
+                    # 2-2. 파이어볼은 4개의 파이어볼로 나누어진다.
+                    # 2-3. 나누어진 파이어볼의 질량, 속력, 방향은 다음과 같다. 
                     nm = totalm // 5
                     ns = totals // totalf
 
                     if nm == 0:
                         continue
 
-                    if flag:    # 방향 [0, 2, 4, 6]
+                    if flag:
                         for nd in [0, 2, 4, 6]:
-                            fireballs.append((r, c, nm, nd, ns))
+                            fireballs.append((r, c, nm, ns, nd))
                     else:
                         for nd in [1, 3, 5, 7]:
-                            fireballs.append((r, c, nm, nd, ns))
+                            fireballs.append((r, c, nm, ns, nd))
 
-    answer = 0
-    while fireballs:
-        _, _, m, _, _ = fireballs.popleft()
-        answer += m
-
-    return answer
+    return sum(fireball[2] for fireball in fireballs)
 
 
 print(solution())
