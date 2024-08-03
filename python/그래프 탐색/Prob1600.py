542ad0ef57fa95b8fb2e89c4ee14b772f02d8f3a
yu <yurimkang20@gmail.com>
2024-08-04 00:22:55 +0900

    upload. 1600 말이 되고픈 원숭이

--- python/그래프 탐색/Prob1600.py
+++ python/그래프 탐색/Prob1600.py
@@ -2,8 +2,22 @@
 """ 골드 3. 말이 되고픈 원숭이 """
 
 import sys
+from collections import deque
 
 def solution():
     input = sys.stdin.readline
+
+    K = int(input())
+    C, R = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(R)]
+
+def bfs():
+    
+    dr = [-2, -1, 1, 2, 2, 1, -1, -2]
+    dc = [-1, -2, -2, -1, 1, 2, 2, 1]
+
+    q = deque()
+    
+
 
 print(solution())
