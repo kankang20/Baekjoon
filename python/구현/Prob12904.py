a1fe01facfeb20846f83def45d28878b312415db
yu <yurimkang20@gmail.com>
2024-08-06 17:17:36 +0900

    upload. 12904 A와 B

--- python/구현/Prob12904.py
+++ python/구현/Prob12904.py
@@ -6,4 +6,25 @@ import sys
 def solution():
     input = sys.stdin.readline
 
+    S = input().rstrip()
+    T = input().rstrip()
+
+    max_length = len(T)
+    answer = 0
+
+    def search(target:str, length:int):
+        
+        if max_length <= length:
+            if T == target:
+                nonlocal answer
+                answer = 1
+            return
+        
+        search(target + "A", length + 1)
+        search(target[::-1] + "B", length + 1)
+
+    search(S, len(S))
+
+    return answer
+
 print(solution())
