dcd8e16dd5fde5aa644ffe08df3c68ca9a11065b
yu <yurimkang20@gmail.com>
2024-08-11 00:26:22 +0900

    solved: [백준] 14889. 스타트와 링크 (Python)

---
+++ 02 백준/구현/Python/Prob14889.py
@@ -0,0 +1,46 @@
+
+"""
+title  : 14889. 스타트와 링크 (Silver 1)
+time   : 3444ms    1112ms
+memory : 31120KB   123268KB
+"""
+
+import sys
+
+answer = 1e9
+
+def solution():
+    input = sys.stdin.readline
+
+    N = int(input())
+    ab = []
+    for _ in range(N):
+        ab.append(list(map(int, input().split())))
+
+    selected = [False for _ in range(N)]
+    check(N, 0, ab, selected, 0)
+
+    return answer
+
+def check(N, count, ab, selected, idx):
+
+    if count == N//2:
+        diff = 0
+        for i in range(N):
+            for j in range(i+1, N):
+                if selected[i] and selected[j]:
+                    diff += ab[i][j]
+                    diff += ab[j][i]
+                elif not selected[i] and not selected[j]:
+                    diff -= ab[i][j]
+                    diff -= ab[j][i]
+        global answer
+        answer = min(answer, abs(diff))
+    
+    for i in range(idx, N):
+        if not selected[i]:
+            selected[i] = True
+            check(N, count+1, ab, selected, i+1)
+            selected[i] = False
+
+print(solution())
