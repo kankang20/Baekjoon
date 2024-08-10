1608eca4e86312d9a3f734b661c0e4b575484343
yu <yurimkang20@gmail.com>
2024-08-11 00:26:43 +0900

    solved: [백준] 15683. 감시 (Python)

---
+++ 02 백준/구현/Python/Prob15683.py
@@ -0,0 +1,66 @@
+
+"""
+title  : 15683. 감시 (Gold 3)
+time   : 328ms     248ms
+memory : 31120KB   119160KB
+"""
+
+import sys
+
+def solution():
+    input = sys.stdin.readline
+
+    global R, C
+    R, C = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(R)]
+
+    global answer
+    answer = R*C
+
+    cctv = []
+    for r in range(R):
+        for c in range(C):
+            if 1 <= board[r][c] <= 5:
+                cctv.append((r, c, board[r][c]))
+
+    direction = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    cctv_direction = [0, [[0], [1], [2], [3]], 
+                [[0, 2], [1, 3]], 
+                [[0, 1], [1, 2], [2, 3], [3, 0]],
+                [[0, 1, 2], [1, 2, 3], [2, 3, 0], [3, 0, 1]],
+                [[0, 1, 2, 3]]]
+
+    def count_area(board):
+            zero = 0
+            for r in range(R):
+                zero += board[r].count(0)
+            return zero
+
+    def dfs(idx):
+
+        if idx == len(cctv):
+            global answer
+            answer = min(answer, count_area(board))
+            return
+
+        r, c, cctv_num = cctv[idx]
+        for cc in cctv_direction[cctv_num]:
+            make_board(cc, r, c, 1)
+            dfs(idx+1)
+            make_board(cc, r, c, -1)
+
+    def make_board(dir, sr, sc, num):
+
+        for d in dir:
+            nr = sr + direction[d][0]
+            nc = sc + direction[d][1]
+            while 0 <= nr < R and 0 <= nc < C and board[nr][nc] < 6:
+                board[nr][nc] += num
+                nr += direction[d][0]
+                nc += direction[d][1]   
+
+    dfs(0)
+
+    return answer
+
+print(solution())
