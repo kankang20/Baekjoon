ca5e2a67427b6c596591100e8041b44f80fd3800
yu <yurimkang20@gmail.com>
2024-08-11 17:11:23 +0900

    solved: [프로그래머스] 84512. 모음사전 (Python)

---
+++ 03 프로그래머스/05 완전탐색/Python/Prob84512.py
@@ -0,0 +1,23 @@
+
+"""
+title  : 84512. 모음사전 (Level 2)
+"""
+
+from itertools import product
+
+def solution(word):
+    answer = []
+
+    alphabet = ['A', 'E', 'I', 'O', 'U']
+
+    for idx in range(1, 6):
+        for temp in list(product(alphabet, repeat=idx)):
+            answer.append(''.join(temp))
+
+    # answer.sort()
+    return sorted(answer).index(word) + 1
+
+print(solution("AAAAE"))        # 6
+print(solution("AAAE"))         # 10
+print(solution("I"))            # 1563
+print(solution("EIO"))          # 1189
