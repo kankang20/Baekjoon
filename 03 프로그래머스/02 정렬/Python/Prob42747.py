eac95b493557c9b05dbe1c582314f718b6d35de7
yu <yurimkang20@gmail.com>
2024-08-11 16:13:43 +0900

    solved: [프로그래머스] 42747. H-Index (Python)

---
+++ 03 프로그래머스/02 정렬/Python/Prob42747.py
@@ -0,0 +1,30 @@
+
+"""
+title  : 42747. H-Index (Level 2)
+"""
+
+def solution(citations):
+    answer = 0
+
+    start = 0
+    end = max(citations)
+
+    while start <= end:
+        
+        mid = (start + end) // 2
+
+        check = 0
+        for c in citations:
+            if c >= mid:
+                check += 1
+        
+        if check >= mid:
+            answer = mid
+            start += 1
+        else:
+            end -= 1
+
+    return answer
+
+
+print(solution([3, 0, 6, 1, 5]))        # 3
