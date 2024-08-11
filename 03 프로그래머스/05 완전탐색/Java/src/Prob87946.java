e47017300c99aaa5a6a80187e693e6ef29a33f76
yu <yurimkang20@gmail.com>
2024-08-11 17:12:45 +0900

    solved: [프로그래머스] 87946. 피로도 (Java)

---
+++ 03 프로그래머스/05 완전탐색/Java/src/Prob87946.java
@@ -0,0 +1,41 @@
+
+/**
+* title  : 87946. 피로도 (Level 2)
+*/
+
+public class Prob87946 {
+    
+    static int answer;
+    static int length;
+    static boolean[] selected;
+    
+    public static void main(String[] args) {
+
+        int k1 = 80;
+        int[][] dungeons = {{80, 20}, {50, 40}, {30, 10}};
+        System.out.println(solution(k1, dungeons));
+    }   
+    
+    public static int solution(int k, int[][] dungeons) {
+        
+        answer = 0;
+        length = dungeons.length;
+        selected = new boolean[length];
+        
+        move(dungeons, k, 0);
+        return answer;
+    }
+    
+    public static void move(int[][] dungeons, int hp, int count) {
+        
+        answer = Math.max(count, answer);
+    
+        for (int i = 0; i < length; i++) {
+            if (!selected[i] && dungeons[i][0] <= hp) {
+                selected[i] = true;
+                move(dungeons, hp-dungeons[i][1], count+1);
+                selected[i] = false;
+            }
+        }
+    }
+}
