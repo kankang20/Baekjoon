e7b1b79889157acd9cdf630569582e764d858d04
yu <yurimkang20@gmail.com>
2024-08-11 16:54:33 +0900

    solved: [프로그래머스] 42885. 구명보트 (Java)

---
+++ 03 프로그래머스/04 탐욕법(그리디)/Java/src/Prob42885.java
@@ -0,0 +1,50 @@
+
+/**
+* title  : 42885. 구명보트 (Level 2)
+*/
+
+import java.util.*;
+
+public class Prob42885 {
+    
+    public static void main(String[] args) {
+
+        int[] p1 = {70, 50, 80, 50};
+        int l1 = 100;
+        System.out.println(solution(p1, l1));
+
+        int[] p2 = {70, 80, 50};
+        int l2 = 100;
+        System.out.println(solution(p2, l2));
+
+        int[] p3 = {50};
+        int l3 = 100;
+        System.out.println(solution(p3, l3));
+    }
+
+    public static int solution(int[] people, int limit) {
+        
+        Arrays.sort(people);
+        
+        int start = 0;
+        int end = people.length - 1;
+        
+        int answer = 0;
+        while (start <= end) {
+            
+            while (start <= end && people[start] + people[end] > limit) {
+                answer++;
+                end--;
+            }
+            
+            if (start <= end) {
+                answer++;
+            }
+
+            start++;
+            end--;
+        }
+
+        return answer;
+    }
+}
