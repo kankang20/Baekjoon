2576e4f9be3215c15c6851be915179330073d40e
yu <yurimkang20@gmail.com>
2024-08-12 13:15:23 +0900

    solved: [소프티어] 6270. GBC (Python)

---
+++ 04 소프티어/Level 2/Python/Prob6270.py
@@ -0,0 +1,31 @@
+
+"""
+title  : 6270. GBC (Level 2)
+time   : 
+memory : 
+"""
+
+import sys
+
+def solution():
+    input = sys.stdin.readline
+
+    N, M = map(int, input().split())
+
+    standard = []
+    for _ in range(N):
+        length, speed = standard.append((length, speed))
+
+    test = []
+    for _ in range(M):
+        length, speed = test.append((length, speed))
+
+    answer = 0
+
+    alen, adx = 0, 0
+    blen, bdx = 0, 0
+
+    # while alen < 100:
+
+    
+print(solution())
