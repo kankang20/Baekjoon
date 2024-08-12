33a045ca59b5536a94c52f8e0c533565e1547ef4
yu <yurimkang20@gmail.com>
2024-08-12 12:38:59 +0900

    solved: [소프티어] 7626. 연탄 배달의 시작 (Python)

---
+++ 04 소프티어/Level 1/Python/Prob7626.py
@@ -0,0 +1,25 @@
+
+"""
+title  : 7626. 연탄 배달의 시작 (Level 1)
+time   : 98ms
+memory : 37.03MB
+"""
+
+import sys
+
+def solution():
+    input = sys.stdin.readline
+
+    N = int(input())
+    town = list(map(int, input().split()))
+
+    min_diff = town[N-1] - town[0]
+    diff = [0] * (N-1)
+    
+    for idx in range(N-1):
+        diff[idx] = town[idx+1] - town[idx]
+        min_diff = min(min_diff, diff[idx])
+
+    print(diff.count(min_diff))
+    
+solution()
