36e15a469f3653c04814e575d9e095debb2cca72
yu <yurimkang20@gmail.com>
2024-08-12 12:37:02 +0900

    solved: [소프티어] 6295. A+B (Java)

---
+++ 04 소프티어/Level 1/Java/src/Prob6295.java
@@ -0,0 +1,28 @@
+
+/**
+* title  : 6295. A+B (Level 1)
+* time   : 121ms
+* memory : 11.94MB
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Prob6295 {
+    
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        int A, B;
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            A = Integer.parseInt(st.nextToken());
+            B = Integer.parseInt(st.nextToken());
+            System.out.printf("Case #%d: %d\n", (i+1), (A+B));
+        }
+    }
+}
