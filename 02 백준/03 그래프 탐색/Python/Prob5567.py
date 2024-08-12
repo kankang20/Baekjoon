0a721df70dfcf3f778b2ede271a68c21ed202d48
yu <yurimkang20@gmail.com>
2024-08-13 01:41:03 +0900

    solved: [백준] 5567. 결혼식 (Python)

---
+++ 02 백준/03 그래프 탐색/Python/Prob5567.py
@@ -0,0 +1,47 @@
+
+"""
+title  : 5567. 결혼식 (Silver 2)
+time   : 56ms       168ms
+memory : 34044KB    113944KB
+"""
+
+import sys
+from collections import deque
+
+def solution():
+    input = sys.stdin.readline
+
+    N = int(input())
+    M = int(input())
+
+    edge = [[] for _ in range(N+1)]
+    for _ in range(M):
+        a, b = map(int, input().split())
+        edge[a].append(b)
+        edge[b].append(a)
+
+    def bfs():
+
+        check = [False] * (N+1)
+        check[1] = True
+
+        q = deque()
+        q.append(1)
+
+        answer = 0
+        for _ in range(2):
+            for _ in range(len(q)):
+                node = q.popleft()
+
+                for nn in edge[node]:
+                    if not check[nn]:
+                        q.append(nn)
+                        check[nn] = True
+            
+            answer += len(q)
+
+        return answer
+    
+    return bfs()
+
+print(solution())
