e94fea618cc089f002170b3a15cb530b66e642fd
yu <yurimkang20@gmail.com>
2024-08-13 01:42:36 +0900

    solved: [백준] 13913. 숨바꼭질 4 (Python)

---
+++ 02 백준/03 그래프 탐색/Python/Prob13913.py
@@ -0,0 +1,66 @@
+
+"""
+title  : 13913. 숨바꼭질 4 (Gold 4)
+time   : 148ms      172ms
+memory : 44276KB    123560KB
+"""
+
+import sys
+from collections import deque
+
+def solution():
+    input = sys.stdin.readline
+
+    N, M = map(int, input().split())
+
+    previous = [0] * (100001)
+    previous[N] = N
+
+    day = bfs(N, M, previous)
+
+    answer = []
+
+    node = M
+    while node != N:
+        answer.append(str(node))
+        node = previous[node]
+    answer.append((str(node)))
+
+    print(day)
+    print(" ".join(answer[::-1]))
+
+def bfs(N, M, previous):
+
+    q = deque()
+    q.append((N, 0))    
+
+    selected = [False] * (100001)
+    selected[N] = True
+
+    while q:
+
+        now, day = q.popleft()
+
+        if now == M:
+            return day
+
+        day += 1
+
+        if now + 1 <= 100000 and not selected[now+1]:
+            q.append((now+1, day))
+            selected[now+1] = True
+            previous[now+1] = now
+        
+        if 1 <= now and not selected[now-1]:
+            q.append((now-1, day))
+            selected[now-1] = True
+            previous[now-1] = now
+
+        if now * 2 <= 100000 and not selected[now*2]:
+            q.append((now*2, day))
+            selected[now*2] = True
+            previous[now*2] = now
+
+    return day
+
+solution()
