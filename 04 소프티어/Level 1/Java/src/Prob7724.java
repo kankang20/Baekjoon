5d12dd0b08e48711391e0805c4f0b37c7d9996a6
yu <yurimkang20@gmail.com>
2024-08-12 12:37:58 +0900

    solved: [소프티어] 7724. 효도의 시작 (Java)

---
+++ 04 소프티어/Level 1/Java/src/Prob7724.java
@@ -0,0 +1,13 @@
+
+/**
+* title  : 7724. 효도의 시작 (Level 1)
+* time   : 71ms
+* memory : 9.89MB
+*/
+
+public class Prob7724 {
+    
+    public static void main(String[] args) {
+        System.out.println("엄마 아빠 사랑해요!");
+    }
+}
