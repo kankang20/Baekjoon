9fe7cf24efbeccca50120f73a73892e6fa1f1810
yu <yurimkang20@gmail.com>
2024-08-14 04:05:48 +0900

    solved: [백준] 2470. 두 용액 (Python)

---
+++ 02 백준/12 투포인터/Python/Prob2470.py
@@ -0,0 +1,34 @@
+
+"""
+title  : 2470. 두 용액 (Gold 5)
+time   : 136ms       144ms
+memory : 42036KB     124068KB
+"""
+
+import sys
+
+N = int(sys.stdin.readline())                       # 전체 용액의 수
+S = list(map(int, sys.stdin.readline().split()))    # 각 용액의 특성값
+
+S.sort()
+
+start = 0
+end = N-1
+
+answer = sys.maxsize
+number = ()
+
+while start < end:
+
+    sum = S[start] + S[end]
+
+    if abs(sum) < answer:
+        answer = abs(sum)
+        number = (S[start], S[end])
+    
+    if sum < 0:
+        start += 1
+    else:
+        end -= 1
+
+print(number[0], number[1])
