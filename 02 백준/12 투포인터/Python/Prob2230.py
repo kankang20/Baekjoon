fd833c4aa115623eaae1151384004266eb0e0f28
yu <yurimkang20@gmail.com>
2024-08-14 04:05:39 +0900

    solved: [백준] 2230. 수 고르기 (Python)

---
+++ 02 백준/12 투포인터/Python/Prob2230.py
@@ -0,0 +1,36 @@
+
+"""
+title  : 2230. 수 고르기 (Gold 5)
+time   : 140ms       164ms
+memory : 35364KB     111712KB
+"""
+
+import sys
+
+def solution():
+    
+    input = sys.stdin.readline
+    N, M = map(int, input().split())        # 수열 크기 N, 차이 M
+    numbers = [int(input()) for _ in range(N)]
+
+    if N == 1 or M == 0:
+        return 0
+
+    numbers.sort()
+
+    start, end = 0, 1
+    answer = numbers[-1] - numbers[0]
+
+    while end < len(numbers):
+
+        diff = numbers[end] - numbers[start]
+
+        if M <= diff:
+            answer = min(answer, diff)
+            start += 1
+        else:
+            end += 1
+
+    return answer
+
+print(solution())
