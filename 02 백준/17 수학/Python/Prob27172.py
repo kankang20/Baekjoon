f7b86d6150f244972c7657024b69475421272274
yu <yurimkang20@gmail.com>
2024-08-14 05:01:26 +0900

    solved: [백준] 27172. 수 나누기 게임 (Python)

---
+++ 02 백준/17 수학/Python/Prob27172.py
@@ -0,0 +1,33 @@
+
+"""
+title  : 27172. 수 나누기 게임 (Gold 5)
+time   : 692ms       304ms
+memory : 52788KB     138212KB
+"""
+
+import sys
+
+def solution():
+
+    input = sys.stdin.readline
+
+    N = int(input())                                # 플레이어의 수
+    cards = list(map(int, input().split()))         # 각 플레이어가 가지고 있는 카드에 적힌 정수
+
+    max_card = max(cards)
+    answer = [0] * (max_card + 1)
+    check = [False] * (max_card + 1)
+
+    for card in cards:
+        check[card] = True
+
+    for card in cards:
+        for num in range(2*card, max_card+1, card):
+            if check[num]:
+                answer[num] -= 1
+                answer[card] += 1
+    
+    for card in cards:
+        print(answer[card], end=" ")
+
+solution()
