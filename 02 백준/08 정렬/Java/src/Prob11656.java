6f04aee8a9bf619acf45bde327444ab2f6ff01d8
yu <yurimkang20@gmail.com>
2024-08-14 01:46:04 +0900

    solved: [백준] 11656. 접미사 배열 (Java)

---
+++ 02 백준/08 정렬/Java/src/Prob11656.java
@@ -0,0 +1,34 @@
+
+/**
+* title  : 11656. 접미사 배열 (Silver 4)
+* time   : 164ms
+* memory : 17520KB
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Prob11656 {
+    
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String word = br.readLine();
+        int length = word.length();
+
+        String[] answer = new String[length];
+        for (int i = 0; i < length; i++) {
+            answer[i] = word.substring(i, length);
+        }
+
+        Arrays.sort(answer);
+
+        for (String ans : answer) {
+            sb.append(ans).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
