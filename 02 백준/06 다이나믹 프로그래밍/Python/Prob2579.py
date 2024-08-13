15b6fb2891635a8761cc9550b662aa28c88c4412
yu <yurimkang20@gmail.com>
2024-08-13 15:44:22 +0900

    solved: [백준] 2579. 계단 오르기 (Python)

---
+++ 02 백준/06 다이나믹 프로그래밍/Python/Prob2579.py
@@ -0,0 +1,29 @@
+
+"""
+title  : 2579. 계단 오르기 (Silver 3)
+time   : 40ms        112ms
+memory : 31120KB     108080KB
+"""
+
+import sys
+
+def solution():
+    input = sys.stdin.readline
+
+    N = int(input())
+    stair = [int(input()) for _ in range(N)]
+
+    if N <= 2:
+        return sum(stair)
+
+    table = [0] * N
+    table[0] = stair[0]
+    table[1] = stair[0] + stair[1]
+    table[2] = max(stair[0] + stair[2], stair[1] + stair[2])
+
+    for idx in range(3, N):
+        table[idx] = max(table[idx-3] + stair[idx-1] + stair[idx], table[idx-2] + stair[idx])
+
+    return table[N-1]
+
+print(solution())
