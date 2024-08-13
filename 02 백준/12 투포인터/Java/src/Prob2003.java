195d04fded3e3bb5736e5892effc136ab60388f9
yu <yurimkang20@gmail.com>
2024-08-14 04:04:54 +0900

    solved: [백준] 2003. 수들의 합 2 (Java)

---
+++ 02 백준/12 투포인터/Java/src/Prob2003.java
@@ -0,0 +1,56 @@
+
+/**
+ * title  : 2003. 수들의 합 2 (Silver 4)
+ * time   : 128ms
+ * memory : 15072KB
+ */
+
+import java.io.*;
+import java.util.*;
+ 
+public class Prob2003 {
+    
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] number = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            number[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(search(N, M, number));
+    }
+
+    public static int search(int N, int M, int[] number) {
+
+        int answer = 0;
+        int sum = 0;
+
+        int start = 0;
+        int end = 0;
+        
+        while (start < N) {
+
+            if (M <= sum) {
+                sum -= number[start++];
+            } else if (end == N) {
+                return answer;
+            } else if (sum < M) {
+                sum += number[end++];
+            }
+
+            if (sum == M) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
