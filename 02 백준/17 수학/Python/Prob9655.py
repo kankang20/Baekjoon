d38a95bd9f7866b8895770afc00cdf00b61bafa2
yu <yurimkang20@gmail.com>
2024-08-14 05:01:17 +0900

    solved: [백준] 9655. 돌 게임 (Python)

---
+++ 02 백준/17 수학/Python/Prob9655.py
@@ -0,0 +1,13 @@
+
+"""
+title  : 9655. 돌 게임 (Silver 5)
+time   : 44ms       120ms
+memory : 31388KB    113112KB
+"""
+
+N = int(input())        # 돌의 개수
+
+if N % 2 == 0:
+    print("CY")
+else:
+    print("SK")
