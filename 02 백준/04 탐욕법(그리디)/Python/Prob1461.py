a4835b71875860afef6d5fdef81f44b92e4079bc
yu <yurimkang20@gmail.com>
2024-08-13 13:17:50 +0900

    solved: [백준] 1461. 도서관 (Python)

---
+++ 02 백준/04 탐욕법(그리디)/Python/Prob1461.py
@@ -0,0 +1,47 @@
+
+"""
+title  : 1461. 도서관 (Gold 4)
+time   : 44ms        108ms
+memory : 31120KB     108080KB
+"""
+
+import sys
+
+def solution():
+    input = sys.stdin.readline
+
+    N, M = map(int, input().split())
+    books = list(map(int, input().split()))
+
+    left = []
+    right = []
+
+    for book in books:
+        if book < 0:
+            left.append(book)
+        else:
+            right.append(book)
+
+    def move(position:list):
+
+        answer = 0
+        length, target = len(position), 0
+
+        while target < length:
+
+            temp, count = target, 1
+            while temp < (length-1) and count < M:
+                temp += 1
+                count += 1
+
+            answer += (abs(position[target]) * 2)
+            target = temp + 1
+
+        return answer, abs(position[0]) if position else 0
+
+    left_answer, left_value = move(sorted(left))
+    right_answer, right_value = move(sorted(right, reverse=True))
+
+    return left_answer + right_answer - max(left_value, right_value)
+
+print(solution())
