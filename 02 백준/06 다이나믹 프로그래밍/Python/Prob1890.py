42a740bf3b0643fbe288aacc26cda7f18851b0d2
yu <yurimkang20@gmail.com>
2024-08-13 15:43:45 +0900

    solved: [백준] 1890. 점프 (Python)

---
+++ 02 백준/06 다이나믹 프로그래밍/Python/Prob1890.py
@@ -0,0 +1,33 @@
+
+"""
+title  : 1890. 점프 (Silver 1)
+time   : 44ms        112ms
+memory : 31120KB     109240KB
+"""
+
+import sys
+
+def solution():
+    input = sys.stdin.readline
+
+    N = int(input())
+    array = [list(map(int, input().split())) for _ in range(N)]
+
+    selected = [[0] * N for _ in range(N)]
+
+    selected[0][0] = 1
+    for r in range(N):
+        for c in range(N):
+
+            if r == N-1 and c == N-1:
+                break
+            
+            if c + array[r][c] < N:
+                selected[r][c + array[r][c]] += selected[r][c]
+
+            if r + array[r][c] < N:
+                selected[r + array[r][c]][c] += selected[r][c]
+
+    return selected[N-1][N-1]
+
+print(solution())
