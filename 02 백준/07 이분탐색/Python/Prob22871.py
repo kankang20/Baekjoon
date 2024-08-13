1c277dd17868825fe32e58a21667b19a38620f2f
yu <yurimkang20@gmail.com>
2024-08-14 00:48:42 +0900

    solved: [백준] 22871. 징검다리 건너기 (large) (Python)

---
+++ 02 백준/07 이분탐색/Python/Prob22871.py
@@ -0,0 +1,47 @@
+
+"""
+title  : 22871. 징검다리 건너기 (large) (Silver 1)
+time   : 6232ms       788ms
+memory : 31256KB      115488KB
+"""
+
+import sys
+
+input = sys.stdin.readline
+N = int(input())                                # 돌의 개수
+A = [0] + list(map(int, input().split()))       # 돌의 수 Ai
+
+def solution(start, end):
+    answer = 0
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        visited = [False] * (N+1)
+        flag = False
+
+        stack = [1]
+        visited[1] = True
+
+        while stack:
+            now = stack.pop()
+
+            if now == N:
+                flag = True
+                break
+
+            for idx in range(now+1, N+1):
+                temp = (idx - now) * (1 + abs(A[now] - A[idx]))
+                if temp <= mid and not visited[idx]:
+                    stack.append(idx)
+                    visited[idx] = True
+
+        if flag:
+            answer = mid
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return answer
+
+print(solution(1, (N-1) * (1 + abs(A[N] - A[1]))))
