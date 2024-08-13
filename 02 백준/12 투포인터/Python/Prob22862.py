a19f89895bd95fc3be52d54ff059656aceadb753
yu <yurimkang20@gmail.com>
2024-08-14 04:06:03 +0900

    solved: [백준] 22862. 가장 긴 짝수 연속한 부분 수열 (large) (Python)

---
+++ 02 백준/12 투포인터/Python/Prob22862.py
@@ -0,0 +1,49 @@
+
+"""
+title  : 22862. 가장 긴 짝수 연속한 부분 수열 (large) (Gold 5)
+time   : 760ms       500ms
+memory : 155560KB    214120KB
+"""
+
+import sys
+
+def solution():
+
+    input = sys.stdin.readline
+    N, K = map(int, input().split())
+    numbers = list(map(int, input().split()))
+    
+    start, end, = 0, 0
+    length_even, count_odd = 0, 0
+
+    if numbers[start] % 2 == 0:
+        length_even += 1
+    else:
+        count_odd += 1
+
+    answer = length_even
+
+    while end < N-1:
+
+        end += 1
+
+        if numbers[end] % 2 == 0:
+            length_even += 1
+        else:
+            count_odd += 1
+
+        if K < count_odd:
+
+            while numbers[start] % 2 == 0:
+                length_even -= 1
+                start += 1
+
+            count_odd -= 1
+            start += 1
+
+        answer = max(answer, length_even)
+
+    return answer
+
+
+print(solution())
