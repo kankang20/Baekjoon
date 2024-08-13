5016ae90e834cbe648558980ce19e826bca77f3f
yu <yurimkang20@gmail.com>
2024-08-13 13:18:09 +0900

    solved: [백준] 1744. 수 묶기 (Python)

---
+++ 02 백준/04 탐욕법(그리디)/Python/Prob1744.py
@@ -0,0 +1,52 @@
+
+"""
+title  : 1744. 수 묶기 (Gold 4)
+time   : 40ms        112ms
+memory : 31120KB     108080KB
+"""
+
+import sys
+
+def solution():
+    input = sys.stdin.readline
+
+    N = int(input())
+
+    low_numbers, high_numbers = [], []
+    for _ in range(N):
+        number = int(input())
+        if number > 0:
+            high_numbers.append(number)
+        else:
+            low_numbers.append(number)
+
+    answer = 0
+    
+    low_numbers.sort()
+    length = len(low_numbers)
+    
+    idx = 0
+    while idx < length-1:
+        answer += (low_numbers[idx] * low_numbers[idx+1])
+        idx += 2
+
+    if idx < length:
+        answer += low_numbers[idx]
+
+    high_numbers.sort(reverse=True)
+    length = len(high_numbers)
+
+    idx = 0
+    while idx < length-1:
+        num1 = high_numbers[idx]
+        num2 = high_numbers[idx+1]
+
+        answer += ((num1 * num2) if num1 > 1 and num2 > 1 else (num1 + num2))
+        idx += 2
+    
+    if idx < length:
+        answer += high_numbers[idx]
+
+    return answer
+
+print(solution())
