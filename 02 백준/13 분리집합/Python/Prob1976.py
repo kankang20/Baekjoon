1341250345e86c65a42ca6a7c59669259a451ecf
yu <yurimkang20@gmail.com>
2024-08-14 04:24:17 +0900

    solved: [백준] 1976. 여행 가자 (Python)

---
+++ 02 백준/13 분리집합/Python/Prob1976.py
@@ -0,0 +1,41 @@
+
+"""
+title  : 1976. 여행 가자 (Gold 4)
+time   : 56ms        128ms
+memory : 31256KB     110824KB
+"""
+
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+def union(parent, a, b):
+    a = find(parent, a)
+    b = find(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+N = int(input())    # 도시 수
+M = int(input())    # 여행 계획에 속한 도시들의 수
+
+parent = [num for num in range(N)]      
+
+for i in range(N):
+    temp = list(map(int, input().split()))
+    for j in range(N):
+        if temp[j] == 1:
+            union(parent, i, j)
+
+plan = list(map(int, input().split()))
+root = find(parent, plan[0]-1)
+
+for city in plan[1:]:
+    if find(parent, city-1) != root:
+        print("NO")
+        break
+
+else:
+    print("YES")
