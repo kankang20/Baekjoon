01b32e6818c8c311ff5b8fb15f3bfe303e54def7
yu <yurimkang20@gmail.com>
2024-08-14 03:38:11 +0900

    solved: [백준] 18352. 특정 거리의 도시 찾기 (Python)

---
+++ 02 백준/11 최단거리/Python/Prob18352.py
@@ -0,0 +1,50 @@
+
+"""
+title  : 18352. 특정 거리의 도시 찾기 (Silver 2)
+time   : 1612ms       900ms
+memory : 99960KB      154472KB
+"""
+
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N, M, K, X = map(int, input().split())      # 도시 개수 N, 도로 개수 M, 거리 정보 K, 출발 도시 번호 X
+
+graph = [[] for _ in range(N+1)]
+visited = [False] * (N+1)
+
+for _ in range(M):
+    i, j = map(int, input().split())
+    graph[i].append(j)
+
+def solution():
+    
+    q = deque()
+
+    q.append((0, X))
+    visited[X] = True
+
+    answer = []
+
+    while q:
+
+        dist, now = q.popleft()
+
+        for g in graph[now]:
+            if not visited[g]:
+                if dist + 1 < K:
+                    visited[g] = True
+                    q.append((dist+1, g))
+                else:
+                    visited[g] = True
+                    answer.append(g)
+
+    if len(answer):
+        for a in sorted(answer):
+            print(a)
+    else:
+        print(-1)
+
+solution()
