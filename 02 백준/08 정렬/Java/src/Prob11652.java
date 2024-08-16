6f62bf075dd051489aa7250e2f66708db2fdbfb1
yu <yurimkang20@gmail.com>
2024-08-16 11:48:21 +0900

    solved: [백준] 11652. 카드 (Java)

--- 02 백준/08 정렬/Java/src/Prob11652.java
+++ 02 백준/08 정렬/Java/src/Prob11652.java
@@ -1,8 +1,8 @@
 
 /**
 * title  : 11652. 카드 (Silver 4)
-* time   : 360ms
-* memory : 33796KB
+* time   : 316ms
+* memory : 34496KB
 */
 
 import java.io.*;
@@ -16,24 +16,41 @@ public class Prob11652 {
 
         int N = Integer.parseInt(br.readLine());
 
+        Integer maxValue = 0;
+        Long numValue = 0L;
+
         HashMap<Long, Integer> numberMap = new HashMap<>();
         for (int i = 0; i < N; i++) {
             Long num = Long.parseLong(br.readLine());
             numberMap.put(num, numberMap.getOrDefault(num, 0) + 1);
+
+            if (maxValue < numberMap.get(num)) {
+                numValue = num;
+                maxValue = numberMap.get(num);
+            } else if (maxValue.equals(numberMap.get(num))) {
+                numValue = Math.min(num, numValue);
+            }
         }
 
-        List<Long> keySet = new ArrayList<>(numberMap.keySet());
+        System.out.println(numValue);
 
-        keySet.sort(new Comparator<Long>() {
-            @Override
-            public int compare(Long o1, Long o2) {
-                if (numberMap.get(o1).equals(numberMap.get(o2))) {
-                    return o1.compareTo(o2);
-                }
-                return -numberMap.get(o1).compareTo(numberMap.get(o2));
-            }
-        });
+        /**
+        * time   : 360ms
+        * memory : 33796KB
+        */
 
-        System.out.println(keySet.get(0));
+        // List<Long> keySet = new ArrayList<>(numberMap.keySet());
+
+        // keySet.sort(new Comparator<Long>() {
+        //     @Override
+        //     public int compare(Long o1, Long o2) {
+        //         if (numberMap.get(o1).equals(numberMap.get(o2))) {
+        //             return o1.compareTo(o2);
+        //         }
+        //         return -numberMap.get(o1).compareTo(numberMap.get(o2));
+        //     }
+        // });
+
+        // System.out.println(keySet.get(0));
     }
 }
