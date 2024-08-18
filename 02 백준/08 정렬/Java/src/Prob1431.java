46061d5fefb990b29bb3351c580f81532b15a7b6
yu <yurimkang20@gmail.com>
2024-08-18 16:36:20 +0900

    solved: [백준] 1431. 시리얼 번호 (Java)

--- 02 백준/08 정렬/Java/src/Prob1431.java
+++ 02 백준/08 정렬/Java/src/Prob1431.java
@@ -1,13 +1,61 @@
 
 /**
 * title  : 1431. 시리얼 번호 (Silver 3)
-* time   : ms
-* memory : KB
+* time   : 104ms
+* memory : 14276KB
 */
 
 import java.io.*;
 import java.util.*;
 
 public class Prob1431 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        String[] words = new String[N];
+        for (int i = 0; i < N; i++) {
+            words[i] = br.readLine();
+        }
+
+        Arrays.sort(words, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                
+                if (o1.length() == o2.length()) {
+                    
+                    int c1 = calculate_number(o1);
+                    int c2 = calculate_number(o2);
+
+                    if (c1 == c2) {
+                        return o1.compareTo(o2);
+                    }
+
+                    return c1 - c2;
+                }
+
+                return o1.length() - o2.length();
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++) {
+            sb.append(words[i]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
     
+    public static int calculate_number(String word) {
+        int count = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if(Character.isDigit(word.charAt(i))) {
+                count += (word.charAt(i) - '0');
+            }
+        }
+        return count;
+    }
 }
