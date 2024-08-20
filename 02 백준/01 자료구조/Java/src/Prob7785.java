5fe2ac12d19dba42891c20f7cae8309a6ba1db4a
yu <yurimkang20@gmail.com>
2024-08-20 15:58:26 +0900

    solved: [백준] 7785. 회사에 있는 사람 (Java)

--- 02 백준/01 자료구조/Java/src/Prob7785.java
+++ 02 백준/01 자료구조/Java/src/Prob7785.java
@@ -1,8 +1,8 @@
 
 /**
 * title  : 7785. 회사에 있는 사람 (Silver 5)
-* time   : ms
-* memory : KB
+* time   : 604ms
+* memory : 50428KB
 */
 
 import java.io.*;
@@ -10,4 +10,37 @@ import java.util.*;
 
 public class Prob7785 {
     
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        HashMap<String, Integer> workMap = new HashMap<>();
+
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            String state = st.nextToken();
+
+            if (state.equals("enter")) {
+                workMap.put(name, 1);
+            } else {
+                workMap.remove(name);
+            }
+        }
+
+        List<String> keySet = new ArrayList<>(workMap.keySet());
+
+        keySet.sort(Comparator.reverseOrder());
+
+        StringBuilder sb = new StringBuilder();
+        for (String key : keySet) {
+            sb.append(key).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
 }
