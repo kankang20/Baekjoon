5ee77d1675497b56eea6a8ad232806e239ada6d9
yu <yurimkang20@gmail.com>
2024-08-20 17:19:57 +0900

    solved: [백준] 1052. 물병 (Python)

--- 02 백준/17 수학/Python/Prob1052.py
+++ 02 백준/17 수학/Python/Prob1052.py
@@ -1,6 +1,24 @@
 
 """
 title  : 1052. 물병 (Gold 5)
-time   :
-memory :
+time   : 2648ms     3408ms
+memory : 31120KB    110272KB
 """
+
+import sys
+
+def solution():
+    input = sys.stdin.readline
+
+    N, K = map(int, input().split())
+
+    # print(type(bin(N)))     # str
+
+    answer = 0
+    while bin(N).count("1") > K:
+        N += 1
+        answer += 1
+
+    return answer
+
+print(solution())
