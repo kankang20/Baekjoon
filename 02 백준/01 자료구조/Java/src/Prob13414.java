61e41a85f98afe33fc23ea7d8cbad86bbfd70c07
yu <yurimkang20@gmail.com>
2024-08-21 13:53:35 +0900

    not solved: [백준] 13414. 수강신청 (Java)

---
+++ 02 백준/01 자료구조/Java/src/Prob13414.java
@@ -0,0 +1,18 @@
+
+/**
+* title  : 13414. 수강신청 (Silver 3)
+* time   : ms
+* memory : KB
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Prob13414 {
+
+    public static void main(String[] args) throws IOException {
+
+        
+    }
+
+}
