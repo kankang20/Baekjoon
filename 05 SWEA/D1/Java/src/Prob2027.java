b5e6a2574b54bbc8bc5a2c3b9047da112693ca8e
yu <yurimkang20@gmail.com>
2024-08-29 01:20:39 +0900

    solved: [SWEA] 2027. 대각선 출력하기 (Java)

---
+++ 05 SWEA/D1/Java/src/Prob2027.java
@@ -0,0 +1,21 @@
+
+/**
+* title  : 2027. 대각선 출력하기 (D1)
+* time   : 111ms
+* memory : 18640kb
+*/
+ 
+public class Prob2027 {
+    
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("#++++").append("\n")
+            .append("+#+++").append("\n")
+            .append("++#++").append("\n")
+            .append("+++#+").append("\n")
+            .append("++++#");
+
+        System.out.println(sb);
+    }
+}
