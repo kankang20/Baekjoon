b5cf0f201d0ec39e87905dbf43bf65aea9ebe881
yu <yurimkang20@gmail.com>
2024-08-29 00:43:53 +0900

    solved: [SWEA] 2047. 신문 헤드라인 (Java)

---
+++ 05 SWEA/D1/Java/src/Prob2047.java
@@ -0,0 +1,17 @@
+
+/**
+* title  : 2047. 신문 헤드라인 (D1)
+* time   : 99ms
+* memory : 16096kb
+*/
+
+import java.io.*;
+ 
+public class Prob2047 {
+    
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println((br.readLine()).toUpperCase());
+    }
+}
