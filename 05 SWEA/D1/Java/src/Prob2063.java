101a20c18813acad3ae9c6b9c9e6c8fb9c42789b
yu <yurimkang20@gmail.com>
2024-08-28 23:35:12 +0900

    solved: [SWEA] 2063. 중간값 찾기 (Java)

---
+++ 05 SWEA/D1/Java/src/Prob2063.java
@@ -0,0 +1,29 @@
+
+/**
+* title  : 2063. 중간값 찾기 (D1)
+* time   : 102ms
+* memory : 19400MB
+*/
+
+import java.io.*;
+import java.util.*;
+ 
+public class Prob2063 {
+    
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] number = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            number[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(number);
+
+        System.out.println(number[N/2]);
+    }
+}
