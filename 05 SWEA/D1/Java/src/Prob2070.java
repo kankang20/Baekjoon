681a2611452f5b917eed3033060a65416eeb33e8
yu <yurimkang20@gmail.com>
2024-08-28 23:34:51 +0900

    solved: [SWEA] 2070. 큰 놈, 작은 놈, 같은 놈 (Java)

---
+++ 05 SWEA/D1/Java/src/Prob2070.java
@@ -0,0 +1,40 @@
+
+/**
+* title  : 2070. 큰 놈, 작은 놈, 같은 놈 (D1)
+* time   : 101ms
+* memory : 18364MB
+*/
+
+import java.io.*;
+import java.util.*;
+ 
+public class Prob2070 {
+    
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n1 = Integer.parseInt(st.nextToken());
+            int n2 = Integer.parseInt(st.nextToken());
+
+            String answer = "";
+
+            if (n1 == n2) {
+                answer = "=";
+            } else if (n1 < n2) {
+                answer = "<";
+            } else if (n1 > n2) {
+                answer = ">";
+            }
+
+            sb.append("#" + t).append(" " + answer).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
