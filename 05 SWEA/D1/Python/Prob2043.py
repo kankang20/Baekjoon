b4fa17fbd74609246ca65cbedde64c28deb39ad1
yu <yurimkang20@gmail.com>
2024-08-30 14:38:00 +0900

    solved: [SWEA] 2043. 서랍의 비밀번호 (Ptyhon)

---
+++ 05 SWEA/D1/Python/Prob2043.py
@@ -0,0 +1,13 @@
+
+"""
+title  : 2043. 서랍의 비밀번호 (D1)
+time   : 106ms
+memory : 44540kb
+"""
+
+def solution():
+
+    num1, num2 = map(int, input().split())
+    print(num1 - num2 + 1)
+
+solution()
