b6bf46e9116d643492637548900b639828a4cc74
yu <yurimkang20@gmail.com>
2024-09-14 22:44:11 +0900

    solved: [백준] 5874. 소를 찾아라 (Python)

--- 02 백준/14 누적합/Python/Prob5874.py
+++ 02 백준/14 누적합/Python/Prob5874.py
@@ -1,7 +1,27 @@
 
 """
 title  : 5874. 소를 찾아라 (Silver 3)
-time   : ms     ms
-memory : KB     KB
+time   : 40ms        92ms
+memory : 31120KB     109260KB
 """
 
+import sys
+
+def solution():
+
+    input = sys.stdin.readline
+
+    board = input().rstrip()
+
+    count = 0
+    answer = 0
+    for i in range(len(board)-1):
+        
+        if board[i] == ")" and board[i+1] == ")":
+            answer += count
+        elif board[i] == "(" and board[i+1] == "(":
+            count += 1
+
+    return answer
+
+print(solution())
