db73d265ebba2e1f38a632dbf6fe3745dca33337
yu <yurimkang20@gmail.com>
2024-09-17 14:48:09 +0900

    solved: [백준] 2578. 빙고 (Java)

--- 02 백준/02 구현/Java/src/Prob2578.java
+++ 02 백준/02 구현/Java/src/Prob2578.java
@@ -1,8 +1,8 @@
 
 /**
- * title  : 20546. 기적의 매매법 (Silver 5)
+ * title  : 2578. 빙고 (Silver 4)
  * time   : 100ms
- * memory : 14240KB
+ * memory : 14112KB
  */
 
 import java.io.*;
@@ -10,54 +10,78 @@ import java.util.*;
 
 public class Prob2578 {
 
+    private static int SIZE = 5;
     private static int[][] board;
-    private static boolean[][] check;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        board = new int[5][5];
-        check = new boolean[5][5];
+        board = new int[SIZE][SIZE];
 
         StringTokenizer st;
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < SIZE; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 5; j++) {
+            for (int j = 0; j < SIZE; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int count = 0;
         int answer = 1;
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < SIZE; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 5; j++) {
+            for (int j = 0; j < SIZE; j++) {
 
                 int number = Integer.parseInt(st.nextToken());
-                int[] pos = find_number(number);
+                find_number(number);    
 
-                
+                if (3 <= search_bingo()) break;
 
-
+                answer++;
             }
         }
 
         System.out.println(answer);
     }
 
-    public static int[] find_number(int number) {
+    public static void find_number(int number) {
 
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
                 if (board[i][j] == number) {
-                    check[i][j] = true;
-                    return new int[] {i, j};
+                    board[i][j] = 0;
                 }
             }
         }
+    }
 
-        return null;
+    public static int search_bingo() {
+
+        int count = 0;
+
+        int di1 = 0;
+        int di2 = 0;
+
+        for (int i = 0; i < SIZE; i++) {
+            int row = 0;
+            int col = 0;
+
+            for (int j = 0; j < SIZE; j++) {
+                if (board[i][j] == 0) row++;
+                if (board[j][i] == 0) col++;
+            }
+
+            if (row == SIZE) count++;
+            if (col == SIZE) count++;
+
+            if (board[i][i] == 0) di1++;
+            if (board[i][SIZE-1-i] == 0) di2++;
+        }
+
+        if (di1 == SIZE) count++;
+        if (di2 == SIZE) count++;
+
+        return count;
     }
 }
