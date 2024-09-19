efbf15166951b145c73a960b8f215b4336ba3c44
yu <yurimkang20@gmail.com>
2024-09-19 22:17:58 +0900

    not solved: [백준] 4396. 지뢰 찾기 (Java)

--- 02 백준/02 구현/Java/src/Prob4396.java
+++ 02 백준/02 구현/Java/src/Prob4396.java
@@ -6,9 +6,11 @@
 */
 
 import java.io.*;
-import java.util.*;
 
 public class Prob4396 {
+
+    private static int[][] dir = {{-1, -1}, {-1, 0}, {-1, 1}, 
+                                    {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
 
     private static int N;
     private static char[][] board;
@@ -16,40 +18,57 @@ public class Prob4396 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         N = Integer.parseInt(br.readLine());
         board = new char[N][N];
+
+        int[][] answer = new int[N][N];
 
         for (int i = 0; i < N; i++) {
             String temp = br.readLine();
             for (int j = 0; j < N; j++) {
                 board[i][j] = temp.charAt(j);
+                answer[i][j] = -1;
             }
         }
-
-        char[][] answer = new char[N][N];
 
         for (int i = 0; i < N; i++) {
             char[] temp = br.readLine().toCharArray();
             for (int j = 0; j < N; j++) {
-
-                if (temp[j] == '.') {
-                    answer[i][j] = '.';
-                    continue;
+                if (temp[j] == 'x') {
+                    answer[i][j] = count_bomb(i, j);
                 }
-
-                // answer[i][j] = count_bomb(i, j);
             }
         }
 
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (0 <= answer[i][j]) {
+                    sb.append(answer[i][j]);
+                } else {
+                    sb.append('.');
+                }
+            }
+            sb.append("\n");
+        }
 
+        System.out.println(sb);
     }
 
     public static int count_bomb(int row, int col) {
 
         int count = 0;
+        for (int[] d : dir) {
+
+            int nr = row + d[0];
+            int nc = col + d[1];
+
+            if (0 <= nr && nr < N && 0 <= nc && nc < N && board[nr][nc] == '*') {
+                count++;
+            }
+        }
 
         return count;
     }
-
 }
