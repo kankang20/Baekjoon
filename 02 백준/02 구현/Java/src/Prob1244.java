5be4e15e4747913e9928dee5c02941b17c633aa6
yu <yurimkang20@gmail.com>
2024-09-22 13:42:59 +0900

    not solved: [백준] 1244. 스위치 켜고 끄기 (Java)

--- 02 백준/02 구현/Java/src/Prob1244.java
+++ 02 백준/02 구현/Java/src/Prob1244.java
@@ -6,7 +6,7 @@
 */
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Prob1244 {
     
@@ -15,10 +15,10 @@ public class Prob1244 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int[] state = new int[N];
+        int[] state = new int[N+1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             state[i] = Integer.parseInt(st.nextToken());
         }
 
@@ -29,7 +29,25 @@ public class Prob1244 {
             int gender = Integer.parseInt(st.nextToken());
             int number = Integer.parseInt(st.nextToken());
 
-            
+            if (gender == 1) {  // 남학생
+                while (number < N) {
+                    state[number] = Math.abs(state[number] - 1);
+                    number += number;
+                }
+            } else {            // 여학생
+                state[number] = Math.abs(state[number] - 1);
+
+                int ln = number;
+                int rn = number;
+                while (0 <= --ln && ++rn < N && state[ln] == state[rn]) {
+                    state[ln] = Math.abs(state[ln] - 1);
+                    state[rn] = Math.abs(state[rn] - 1);
+                }
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            System.out.print(state[i] + " ");
         }
     }
 }
