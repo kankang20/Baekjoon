3801b1e85447ae790c87485395646a53a7830929
yu <yurimkang20@gmail.com>
2024-10-02 20:48:09 +0900

    solved: [백준] 4659. 비밀번호 발음하기 (Java)

--- 02 백준/10 문자열/Java/src/Prob4659.java
+++ 02 백준/10 문자열/Java/src/Prob4659.java
@@ -1,14 +1,15 @@
 
 /**
 * title  : 4659. 비밀번호 발음하기 (Silver 5)
-* time   : ms
-* memory : KB
+* time   : 88ms
+* memory : 14008KB
 */
 
 import java.io.*;
-import java.util.*;
 
 public class Prob4659 {
+
+    private static String vowels = "aeiou";
 
     public static void main(String[] args) throws IOException {
 
@@ -20,13 +21,13 @@ public class Prob4659 {
             String word = br.readLine();
             char[] wordArray = word.toCharArray();
 
-            if (word == "end") {
+            if (word.equals("end")) {
                 break;
             }
 
             sb.append("<").append(word).append("> is ");
 
-            if (checkOne(wordArray) && checkTwo(word) && checkThree(word)) {
+            if (checkOne(wordArray) && checkTwo(wordArray) && checkThree(wordArray)) {
                 sb.append("acceptable.").append("\n");
             } else {
                 sb.append("not acceptable.").append("\n");
@@ -38,10 +39,8 @@ public class Prob4659 {
 
     public static boolean checkOne(char[] word) {
 
-        String check = "aeiou";
-
         for (int i = 0; i < word.length; i++) {
-            if (check.contains(String.valueOf(word[i]))) {
+            if (vowels.contains(String.valueOf(word[i]))) {
                 return true;
             } 
         }
@@ -49,11 +48,41 @@ public class Prob4659 {
         return false;
     }
 
-    public static boolean checkTwo(String word) {
-        return false;
+    public static boolean checkTwo(char[] word) {
+
+        int vCount = 0;
+        int eCount = 0;
+
+        for (int i = 0; i < word.length; i++) {
+
+            if (vowels.contains(String.valueOf(word[i]))) {
+                vCount++;
+                eCount = 0;
+            } else {
+                eCount++;
+                vCount = 0;
+            }
+
+            if (vCount == 3 || eCount == 3) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
-    public static boolean checkThree(String word) {
-        return false;
+    public static boolean checkThree(char[] word) {
+
+        for (int i = 1; i < word.length; i++) {
+
+            if (word[i-1] == word[i]) {
+                if (word[i] == 'e' || word[i] == 'o') {
+                    continue;
+                }
+                return false;
+            }
+        }
+
+        return true;
     }
 }
