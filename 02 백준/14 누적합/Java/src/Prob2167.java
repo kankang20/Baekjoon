01cb5d6f0bf5e4148bdf896a6e1226b02ef19c94
yu <yurimkang20@gmail.com>
2024-10-10 22:20:49 +0900

    solved: [백준] 2167. 2차원 배열의 합 (Java)

--- 02 백준/14 누적합/Java/src/Prob2167.java
+++ 02 백준/14 누적합/Java/src/Prob2167.java
@@ -1,8 +1,8 @@
 
 /**
 * title  : 2167. 2차원 배열의 합 (Silver 5)
-* time   : ms
-* memory : KB
+* time   : 276ms
+* memory : 27572KB
 */
 
 import java.io.*;
@@ -27,39 +27,22 @@ public class Prob2167 {
             }
         }
 
-        int[][] answer = new int[N][M];
-        
-        answer[0][0] = board[0][0];
+        int[][] answer = new int[N+1][M+1];
 
-        for(int i = 1; i < N; i++) {
-            answer[i][0] = board[i][0] + answer[i-1][0];
-        }
-
-        for(int j = 1; j < M; j++) {
-            answer[0][j] = board[0][j] + answer[0][j-1];
-        }
-
-        for (int i = 1; i < N; i++) {
-            for (int j = 1; j < M; j++) {
-                answer[i][j] = board[i][j] + answer[i-1][j] + answer[i][j-1] - answer[i-1][j-1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                answer[i][j] = answer[i-1][j] + answer[i][j-1] - answer[i-1][j-1] + board[i-1][j-1];
             }
-        }
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                System.out.print(answer[i][j] + " ");
-            }
-            System.out.println();
         }
 
         int K = Integer.parseInt(br.readLine());
         for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
-            int r1 = Integer.parseInt(st.nextToken())-1;
-            int c1 = Integer.parseInt(st.nextToken())-1;
+            int r1 = Integer.parseInt(st.nextToken()) - 1;
+            int c1 = Integer.parseInt(st.nextToken()) - 1;
             int r2 = Integer.parseInt(st.nextToken());
             int c2 = Integer.parseInt(st.nextToken()); 
-            sb.append(answer[r2][c2] - answer[r1][c1]).append("\n");
+            sb.append(answer[r2][c2] - answer[r1][c2] - answer[r2][c1] + answer[r1][c1]).append("\n");
         }
 
         System.out.println(sb);
